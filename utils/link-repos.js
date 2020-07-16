b51068802f660558a259e6d2f6d332b0291ea3a6
Alisher <alisher.amonulloyev@devexpress.com>
2020-07-16 21:55:19 +0300

    fixed scripts

--- utils/link-repos.js
+++ utils/link-repos.js
@@ -1,42 +1,25 @@
 const path = require('path');
-const prompts = require('prompts');
-const systemSync = require('./cp_utils').systemSync;
-const fs = require('fs');
-
-const baseDemosDir = 'Demos';
-const reposFilePath = './_reposPaths'; 
-const node_modulesDir = path.join(process.cwd(), 'node_modules');
-
-let reposPaths = {
-    devextreme: "",
-    devextreme_angular: "",
-    devextreme_react: "",
-    devextreme_vue: ""
-}
+const pq = require('./prompts_questions');
+const reposHelper = require('./repos_helper');
+const fs_utils = require('./fs_utils');
+const node_modulesDir = path.join(__dirname, '..', 'JSDemos', 'node_modules');
 
 const mainRoutine = async () => {
-    if(!fs.existsSync(reposFilePath)){
-        fs.writeFile(reposFilePath, JSON.stringify(reposPaths, null, 2), (err) => {
-            if(err) throw err;
-            console.log('File `' + reposFilePath + '` successfully created. Please fill it with paths to repos and run this script again');
-        });
-    } else {
-        reposPaths = JSON.parse(fs.readFileSync(reposFilePath));
-
-        console.log("Linking devextreme");
-        systemSync('cd ' + node_modulesDir + ' && npm link ' + path.join(reposPaths['devextreme'], 'artifacts', 'npm', 'devextreme'));
-
-        console.log('Linking devextreme-angular');
-        systemSync('cd ' + node_modulesDir + ' && npm link ' + reposPaths['devextreme-angular']);
-
-        console.log('Linking devextreme-react');
-        systemSync('cd ' + node_modulesDir + ' && npm link ' + reposPaths['devextreme-react']);
-
-        console.log('Linking devextreme-vue');
-        systemSync('cd ' + node_modulesDir + ' && npm link ' + reposPaths['devextreme-vue']);
-
-        console.log('Finished...');
-    }
+    response = await pq.askLinkRepositories();
+    let repositories = [];
+    await reposHelper.processRepositoriesAsync(response.repositories, async (repositoryName) => {
+        const repositoryPath = await reposHelper.getRepositoryPath(repositoryName);
+        repositories.push({ name: repositoryName, path: repositoryPath });
+    });
+    repositories.forEach(repository => {
+        console.log('Processing the `' + repository.name + '` repository...');
+        if(repository.name === 'devextreme') {
+            repository.path = path.join(repository.path, 'artifacts', 'npm', 'devextreme'); 
+        }
+        reposHelper.processRepository(response.command, repository.name, repository.path, node_modulesDir);
+        console.log("Processed: " + repository.path);
+    });
+    console.log('Finished ' + response.command + 'ing repositories...');
 }
 
 (async () => await mainRoutine())();
