61b1b2e98542a1a6d2a5ba461bd537abbc72b259
groshenkovamarina <magroshenkova@gmail.com>
2020-07-31 15:51:01 +0300

    Fix prepare mvc and netcore demos

--- utils/prepare-common.js
+++ utils/prepare-common.js
@@ -1,22 +1,17 @@
 const path = require('path');
 const fs = require('fs');
-const pq = require('./prompts_questions');
 const reposHelper = require('./repos_helper');
 const fs_utils = require('./fs_utils');
+const cp_utils = require('./cp_utils');
 
-const scripts = [
-
-]
-
-const prepareWorkspace = async () => {
-    const hgPath = await reposHelper.getRepositoryPath('HG');
-
+const prepareProjects = (hgPath) => {
     const rootDir = process.cwd();
     const jsDemos = path.join(rootDir, 'JSDemos')
     const netCoreDemos = path.join(rootDir, 'NetCoreDemos');
     const mvcDemos = path.join(rootDir, 'MVCDemos');
     const mvcDemosAppData = path.normalize(path.join(mvcDemos + '/App_Data'));
     const hgJSDemos = path.join(hgPath, 'Demos/WidgetsGallery/WidgetsGallery');
+    const hgNetCoreDemoShell = path.join(hgPath, 'Demos/WidgetsGallery/AspNetCoreDemos.DemoShell');
     const dxStyles = path.join(jsDemos, 'node_modules/devextreme/dist/css');
     const dxIcons = path.join(jsDemos, 'node_modules/devextreme/dist/css/icons');
     const dxScripts = path.join(jsDemos, 'node_modules/devextreme/dist/js');
@@ -26,9 +21,6 @@ const prepareWorkspace = async () => {
     const jQuery = path.join(jsDemos, 'node_modules/jquery/dist');
     const jszip = path.join(jsDemos, 'node_modules/jszip/dist');
 
-    fs_utils.recreateLink(path.join(hgPath, 'SampleImages'), path.join(mvcDemos, 'Content/SampleData/SampleImages'));
-    fs_utils.recreateLink(path.join(hgPath, 'SampleImages'), path.join(netCoreDemos, 'wwwroot/SampleData/SampleImages'));
- 
     fs.readdirSync(mvcDemosAppData)
         .filter(file => /.*\.ldf$/.test(file))
         .map(file => fs.unlinkSync(mvcDemosAppData, file));
@@ -51,11 +43,26 @@ const prepareWorkspace = async () => {
 
     fs.copyFileSync(path.join(hgPath, 'SampleDatabases/Northwind.mdf'), path.join(mvcDemosAppData, 'Northwind.mdf'));
 
+    fs_utils.recreateLink(path.join(hgPath, 'SampleImages'), path.join(mvcDemos, 'Content/SampleData/SampleImages'));
+    fs_utils.recreateLink(path.join(hgPath, 'SampleImages'), path.join(netCoreDemos, 'wwwroot/SampleData/SampleImages'));
+
     fs_utils.recreateLink(path.join(hgJSDemos, 'build/demo-template.css'), path.join(mvcDemos, 'Content/demo-template.css'));
     fs_utils.recreateLink(path.join(hgJSDemos, 'build/demo-template.css'), path.join(netCoreDemos, 'wwwroot/css/demo-template.css'));
 
     fs_utils.recreateLink(path.join(jsDemos, 'menuMeta.json'), path.join(mvcDemosAppData, 'menuMeta.json'));
     fs_utils.recreateLink(path.join(jsDemos, 'menuMeta.json'), path.join(netCoreDemos, 'menuMeta.json'));
+
+    fs_utils.recreateLink(path.join(hgNetCoreDemoShell, 'wwwroot/DemoShell'), path.join(netCoreDemos, 'wwwroot/DemoShell'));
+    fs_utils.recreateLink(path.join(hgNetCoreDemoShell, 'DemoShell'), path.join(netCoreDemos, 'DemoShell'));
+    fs.copyFileSync(path.join(hgNetCoreDemoShell, '.editorconfig'), path.join(netCoreDemos, '.editorconfig'));
+}
+
+const prepareWorkspace = async () => {
+    const hgPath = await reposHelper.getRepositoryPath('HG');
+    const hgWG = path.join(hgPath, 'Demos/WidgetsGallery');
+
+    cp_utils.systemSync('call ' + hgWG + '/prepareWG.cmd');
+    prepareProjects(hgPath);
 
     return hgPath;
 }
--- utils/prepare-netcore.js
+++ utils/prepare-netcore.js
@@ -1,68 +1,41 @@
 const path = require('path');
 const fs = require('fs');
-const systemSync = require('./cp_utils').systemSync;
+const cp_utils = require('./cp_utils');
 const common = require('./prepare-common');
 const fs_utils = require('./fs_utils');
+const util = require('util');
 
-const createNugetPackage = (netCorePath, hgPath) => {
-    const rootDir = process.cwd();
-    const netCoreDemos = path.join(rootDir, 'NetCoreDemos');
-    const netCoreProjPath = path.join(netCorePath, 'DevExtreme.AspNet.Core.csproj');
-    const netCoreReleasePath = path.join(hgPath, 'DevExtreme.AspNet.Mvc/AspNetCore/DevExtreme.AspNet.Core/bin/Release');
-    const nugetPath = path.join(hgPath, 'DevExtreme.AspNet.Mvc/AspNetCore/NuGet');
-    const demosNugetPath = path.join(netCoreDemos, 'NuGet');
-    const version = require(path.join(rootDir, 'JSDemos', 'node_modules/devextreme/package.json')).version;
+const modifyNetCoreProject = (netCoreDemos, version) => {
+    const netCoreDemosProj = path.join(netCoreDemos, 'DevExtreme.NETCore.Demos.csproj');
 
-    fs_utils.recreateLink(nugetPath);
+    let content = fs.readFileSync(netCoreDemosProj, 'utf-8');
 
-    if(fs.existsSync(demosNugetPath)) {
-        fs.rmdirSync(demosNugetPath, {recursive: true});
-    }
-
-    fs_utils.recreateLink(nugetPath, demosNugetPath);
-
-    systemSync('dotnet restore ' + netCoreProjPath);
-    systemSync('dotnet msbuild ' + netCoreProjPath + ' -p:Configuration=Release');
-    systemSync('dotnet pack ' + netCorePath + ' --no-build --configuration=Release');
-
-    fs.readdirSync(netCoreReleasePath)
-        .filter(file => /.*\.symbols.nupkg$/.test(file))
-        .map(file => fs.unlinkSync(path.join(netCoreReleasePath, file)));
-
-    const nugetFiles = fs.readdirSync(netCoreReleasePath)
-                            .filter(file => /.*\.nupkg$/.test(file));
-
-    fs.copyFile(path.join(netCoreReleasePath, nugetFiles[0]), path.join(nugetPath, nugetFiles[0]), (error) => {
-        if(error) {
-            console.log(error);
-            return;
-        } else {
-            console.log('Nuget Package has been created');
-
-            const netCoreDemosProj = path.join(netCoreDemos, 'DevExtreme.NETCore.Demos.csproj');
-            let content = fs.readFileSync(netCoreDemosProj, 'utf-8');
-
-            content = content.replace(/(\"DevExtreme.AspNet.Core\"\s*Version=")(\d+\.\d+\.\d+).*?(?=")/, '$1' + version);
+    content = content.replace(/(\"DevExtreme.AspNet.Core\"\s*Version=")(\d+\.\d+\.\d+).*?(?=")/, '$1' + version);
             
-            fs.writeFileSync(netCoreDemosProj, content, 'utf-8');
+    fs.writeFileSync(netCoreDemosProj, content, 'utf-8');
 
-            console.log('NetCore Demos project has been modified');
+    console.log('NetCore Demos project has been modified');
 
-            systemSync('dotnet restore ' + netCoreDemos);
-        }
-    });
-};
+    cp_utils.systemSync('dotnet restore ' + netCoreDemos);
+}
 
 const prepareNetCoreDemos = async () => {
-    const response = await common.prepareWorkspace();
+    const rootDir = process.cwd();
+    const hgPath = await common.prepareWorkspace();
+    const netCoreDemos = path.join(rootDir, 'NetCoreDemos');
+    const nugetPath = path.join(hgPath, 'DevExtreme.AspNet.Mvc/AspNetCore/NuGet');
+    const demosNugetPath = path.join(netCoreDemos, 'NuGet');
+    const version = require(path.join(hgPath, 'GitHub/artifacts/npm/devextreme/package.json')).version;
 
-    const netCorePath = path.join(response.hgPath, 'DevExtreme.AspNet.Mvc/AspNetCore/DevExtreme.AspNet.Core');
+    if(fs.existsSync(demosNugetPath)) {
+        fs.unlinkSync(demosNugetPath);
+    }
 
-    fs_utils.recreateLink(
-        path.join(response.hgPath, 'EULA_DXTREME.txt'),
-        path.join(netCorePath, 'LICENSE.txt'),
-        createNugetPackage(netCorePath, response.hgPath
-    ));
+    const recreateNugetLink = util.promisify(fs.symlink);
+    
+    recreateNugetLink(nugetPath, demosNugetPath).then(() => {
+        modifyNetCoreProject(netCoreDemos, version);
+    });
 };
 
 (async () => await prepareNetCoreDemos())();
