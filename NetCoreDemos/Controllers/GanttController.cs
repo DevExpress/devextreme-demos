f272fe0cad6c5b4e4285309b8d9ae4d1de980e2c
CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>
2020-08-31 13:26:53 +0300

    dxGantt: Add and update Demos for MVC, NET Core and JS

--- NetCoreDemos/Controllers/GanttController.cs
+++ NetCoreDemos/Controllers/GanttController.cs
@@ -3,7 +3,27 @@ using DevExtreme.NETCore.Demos.Models.SampleData;
 
 namespace DevExtreme.NETCore.Demos.Controllers {
     public class GanttController : Controller {
+        public IActionResult ChartAppearance() {
+            return View();
+        }
+
+        public IActionResult DataBinding() {
+            return View();
+        }
+
         public IActionResult Overview() {
+            return View();
+        }
+
+        public IActionResult StripLines() {
+            return View();
+        }
+
+        public IActionResult Toolbar() {
+            return View();
+        }
+
+        public IActionResult Validation() {
             return View();
         }
     }
