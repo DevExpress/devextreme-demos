4b4f984024cd7c60c64225f931ddf297aada4cda
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2020-09-24 20:25:11 +0300

    add "HtmlEditor - Tables" demo

---
+++ JSDemos/Demos/HtmlEditor/Tables/React/data.js
@@ -0,0 +1,28 @@
+export const markup = `
+    <h2>
+        Simple table
+    </h2>
+    <br>
+    <table>
+    <tr style="text-align: center; font-weight: bold;">
+        <td>Task</td>
+        <td>Status</td>
+        <td>Progress</td>
+    </tr>
+    <tr style="text-align: center;">
+        <td>Task 1</td>
+        <td>In Progress</td>
+        <td>45%</td>
+    </tr>
+    <tr style="text-align: center;">
+        <td>Task 2</td>
+        <td style="color: red;">Paused</td>
+        <td>80%</td>
+    </tr>
+    <tr style="text-align: center;">
+        <td>Task 3</td>
+        <td style="color: lime;">Finished</td>
+        <td>100%</td>
+    </tr>
+    </table>
+`;
