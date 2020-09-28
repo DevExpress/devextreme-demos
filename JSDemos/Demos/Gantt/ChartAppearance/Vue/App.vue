84838ae5a02cf4974df1c57564bfe46a5ab33517
Sergey Novikov <57402891+novsstation@users.noreply.github.com>
2020-09-28 12:42:10 +0300

    Update App.vue
    fix linters

--- JSDemos/Demos/Gantt/ChartAppearance/Vue/App.vue
+++ JSDemos/Demos/Gantt/ChartAppearance/Vue/App.vue
@@ -113,12 +113,12 @@ export default {
       var timeEstimate = Math.abs(model.start - model.end) / 36e5;
       var timeLeft = Math.floor((100 - model.progress) / 100 * timeEstimate);
 
-      return `<div style='font-size: 14px'>${model.title}</div>
-        <p style='font-size: 10px'><span> Estimate: </span>${timeEstimate}<span> hours </span></p>
-        <p style='font-size: 10px'><span> Left: </span>${timeLeft}<span> hours </span></p>`;
+      return `<div style='font-size:${'14px'}'>${model.title}</div>
+        <p style='font-size:${'10px'}'><span> Estimate: </span>${timeEstimate}<span> hours </span></p>
+        <p style='font-size:${'10px'}'><span> Left: </span>${timeLeft}<span> hours </span></p>`;
     },
     onShowCustomTaskTooltip(e) {
-      e.value ? this.taskTooltipContentTemplate = this.getTaskTooltipContentTemplate : this.taskTooltipContentTemplate = "";
+      e.value ? this.taskTooltipContentTemplate = this.getTaskTooltipContentTemplate : this.taskTooltipContentTemplate = '';
     }
   }
 };
