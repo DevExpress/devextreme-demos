43524e5535114c2c5cbf9d2f7857be1ba0e5ff73
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-09-28 20:19:04 +0300

    Scheduler - Virtual Scrolling demo (JQuery)

---
+++ JSDemos/Demos/Scheduler/VirtualScrolling/jQuery/data.js
@@ -0,0 +1,61 @@
+var resourcesAmount = 100;
+
+function getRandomInt(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+function generateResources() {
+  var colors = [
+    'rgba(63, 81, 181, 0.7)',
+    'rgba(234, 128, 252, 0.7)',
+    'rgba(223, 82, 134, 0.7)',
+    'rgba(254, 194, 0, 0.7)'
+  ];
+  var resources = [];
+
+  for(var i = 0; i < resourcesAmount; ++i) {
+    var color = colors[i % colors.length];
+
+    resources.push({
+      id: i,
+      text: `Resource ${i}`,
+      color: color
+    });
+  }
+
+  return resources;
+};
+
+function generateAppointments() {
+  var startDay = 6;
+  var endDay = 14;
+  var startDayHour = 9;
+  var endDayHour = 18;
+
+  var appointments = [];
+
+  for (var resourceId = 0; resourceId < resourcesAmount; ++resourceId) {
+    for (var dayIndex = startDay; dayIndex <= endDay; ++dayIndex) {
+      for (var count = 0; count < 10; ++count) {
+        var hour = getRandomInt(startDayHour, endDayHour);
+        var minutes = getRandomInt(15, 44);
+        var minutesBeforeHour = 60 - minutes;
+        var minMinutes = Math.min(minutes, minutesBeforeHour);
+        var maxMinutes = Math.max(minutes, minutesBeforeHour);
+        var apointmentTime = getRandomInt(minMinutes, maxMinutes) * 60 * 1000;
+        var startDate = new Date(2021, 8, dayIndex, hour, minutes);
+        var endDate = new Date(startDate.getTime() + apointmentTime);
+
+        var item = {
+          text: `item ${resourceId}_${dayIndex}`,
+          resourceId: resourceId,
+          startDate: startDate,
+          endDate: endDate,
+        };
+        appointments.push(item);
+      }
+    }
+  }
+
+  return appointments;
+}
