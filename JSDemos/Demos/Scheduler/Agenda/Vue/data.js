2dbc4c2e9dcd78c6680a97437f842bf4db338b6d
Andrew Makarov <andrey.makarov@devexpress.com>
2020-09-30 17:05:15 +0300

    corrected JSON structure

--- JSDemos/Demos/Scheduler/Agenda/Vue/data.js
+++ JSDemos/Demos/Scheduler/Agenda/Vue/data.js
@@ -32,344 +32,344 @@ export const priorityData = [
 
 export const data = [{
   text: 'Google AdWords Strategy',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-03T06:00:00.000Z'),
   endDate: new Date('2021-05-03T07:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'New Brochures',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-03T08:30:00.000Z'),
   endDate: new Date('2021-05-03T11:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Brochure Design Review',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-03T10:15:00.000Z'),
   endDate: new Date('2021-05-03T13:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Website Re-Design Plan',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-03T13:45:00.000Z'),
   endDate: new Date('2021-05-04T08:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Rollout of New Website and Marketing Brochures',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-04T05:15:00.000Z'),
   endDate: new Date('2021-05-04T07:45:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Update Sales Strategy Documents',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-04T09:00:00.000Z'),
   endDate: new Date('2021-05-04T10:45:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Non-Compete Agreements',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-05T05:15:00.000Z'),
   endDate: new Date('2021-05-05T06:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Approve Hiring of John Jeffers',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-05T07:00:00.000Z'),
   endDate: new Date('2021-05-05T08:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Update NDA Agreement',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-05T08:45:00.000Z'),
   endDate: new Date('2021-05-05T10:45:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Update Employee Files with New NDA',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-05T11:00:00.000Z'),
   endDate: new Date('2021-05-05T13:45:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Submit Questions Regarding New NDA',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-06T05:00:00.000Z'),
   endDate: new Date('2021-05-06T06:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Submit Signed NDA',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-06T09:45:00.000Z'),
   endDate: new Date('2021-05-06T11:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Review Revenue Projections',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-06T14:15:00.000Z'),
   endDate: new Date('2021-05-06T15:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Comment on Revenue Projections',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-07T06:15:00.000Z'),
   endDate: new Date('2021-05-07T08:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Provide New Health Insurance Docs',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-07T09:45:00.000Z'),
   endDate: new Date('2021-05-07T11:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Review Changes to Health Insurance Coverage',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-07T11:15:00.000Z'),
   endDate: new Date('2021-05-07T12:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Review Training Course for any Ommissions',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-10T11:00:00.000Z'),
   endDate: new Date('2021-05-11T09:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Recall Rebate Form',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-10T09:45:00.000Z'),
   endDate: new Date('2021-05-10T10:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Create Report on Customer Feedback',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-11T12:15:00.000Z'),
   endDate: new Date('2021-05-11T14:30:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Review Customer Feedback Report',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-11T13:15:00.000Z'),
   endDate: new Date('2021-05-11T15:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Customer Feedback Report Analysis',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-12T06:30:00.000Z'),
   endDate: new Date('2021-05-12T07:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Prepare Shipping Cost Analysis Report',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-12T09:30:00.000Z'),
   endDate: new Date('2021-05-12T10:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Provide Feedback on Shippers',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-12T11:15:00.000Z'),
   endDate: new Date('2021-05-12T13:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Select Preferred Shipper',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-12T14:30:00.000Z'),
   endDate: new Date('2021-05-12T17:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Complete Shipper Selection Form',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-13T05:30:00.000Z'),
   endDate: new Date('2021-05-13T07:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Upgrade Server Hardware',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-13T09:00:00.000Z'),
   endDate: new Date('2021-05-13T11:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Upgrade Personal Computers',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-13T11:45:00.000Z'),
   endDate: new Date('2021-05-13T13:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Upgrade Apps to Windows RT or stay with WinForms',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-14T07:30:00.000Z'),
   endDate: new Date('2021-05-14T10:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Estimate Time Required to Touch-Enable Apps',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-14T11:45:00.000Z'),
   endDate: new Date('2021-05-14T13:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Report on Tranistion to Touch-Based Apps',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-14T15:30:00.000Z'),
   endDate: new Date('2021-05-14T16:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Submit New Website Design',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-17T05:00:00.000Z'),
   endDate: new Date('2021-05-17T07:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Create Icons for Website',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-17T08:30:00.000Z'),
   endDate: new Date('2021-05-17T10:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Create New Product Pages',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-18T06:45:00.000Z'),
   endDate: new Date('2021-05-18T08:45:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Approve Website Launch',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-18T09:00:00.000Z'),
   endDate: new Date('2021-05-18T12:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Update Customer Shipping Profiles',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-19T06:30:00.000Z'),
   endDate: new Date('2021-05-19T08:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Create New Shipping Return Labels',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-19T09:45:00.000Z'),
   endDate: new Date('2021-05-19T11:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Get Design for Shipping Return Labels',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-19T12:00:00.000Z'),
   endDate: new Date('2021-05-19T13:30:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'PSD needed for Shipping Return Labels',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-20T05:30:00.000Z'),
   endDate: new Date('2021-05-20T06:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Contact ISP and Discuss Payment Options',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-20T08:30:00.000Z'),
   endDate: new Date('2021-05-20T13:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Prepare Year-End Support Summary Report',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-20T14:00:00.000Z'),
   endDate: new Date('2021-05-20T17:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Review New Training Material',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-21T05:00:00.000Z'),
   endDate: new Date('2021-05-21T06:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Distribute Training Material to Support Staff',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-21T09:45:00.000Z'),
   endDate: new Date('2021-05-21T11:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Training Material Distribution Schedule',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-21T11:15:00.000Z'),
   endDate: new Date('2021-05-21T13:15:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Approval on Converting to New HDMI Specification',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-24T06:30:00.000Z'),
   endDate: new Date('2021-05-24T07:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Create New Spike for Automation Server',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-24T07:00:00.000Z'),
   endDate: new Date('2021-05-24T09:30:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Code Review - New Automation Server',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-24T10:00:00.000Z'),
   endDate: new Date('2021-05-24T12:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Confirm Availability for Sales Meeting',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-25T07:15:00.000Z'),
   endDate: new Date('2021-05-25T12:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Reschedule Sales Team Meeting',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-25T13:15:00.000Z'),
   endDate: new Date('2021-05-25T15:00:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Send 2 Remotes for Giveaways',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-26T06:30:00.000Z'),
   endDate: new Date('2021-05-26T08:45:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Discuss Product Giveaways with Management',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-26T09:15:00.000Z'),
   endDate: new Date('2021-05-26T13:45:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Replace Desktops on the 3rd Floor',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-27T06:30:00.000Z'),
   endDate: new Date('2021-05-27T07:45:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Update Database with New Leads',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-27T09:00:00.000Z'),
   endDate: new Date('2021-05-27T11:15:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Mail New Leads for Follow Up',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-27T11:45:00.000Z'),
   endDate: new Date('2021-05-27T12:30:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Send Territory Sales Breakdown',
-  'ownerId': [2],
+  ownerId: [2],
   startDate: new Date('2021-05-27T15:00:00.000Z'),
   endDate: new Date('2021-05-27T17:00:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Territory Sales Breakdown Report',
-  'ownerId': [1],
+  ownerId: [1],
   startDate: new Date('2021-05-28T05:45:00.000Z'),
   endDate: new Date('2021-05-28T06:45:00.000Z'),
-  'priority': 1
+  priority: 1
 }, {
   text: 'Report on the State of Engineering Dept',
-  'ownerId': [3],
+  ownerId: [3],
   startDate: new Date('2021-05-28T11:45:00.000Z'),
   endDate: new Date('2021-05-28T12:30:00.000Z'),
-  'priority': 2
+  priority: 2
 }, {
   text: 'Staff Productivity Report',
-  'ownerId': [4],
+  ownerId: [4],
   startDate: new Date('2021-05-28T13:15:00.000Z'),
   endDate: new Date('2021-05-28T16:30:00.000Z'),
-  'priority': 2
+  priority: 2
 }];
