627a05428aaa722f890e5f267ffca528f3fb166f
Artem Kobilinskii <a.kobilinsky@gmail.com>
2020-10-02 17:11:10 +0300

    dxGantt: Add ContextMenu demo (#191)
    * dxGantt: Fix Linters
    
    * dxGantt: Fix linter issue
    
    * dxGantt: Fix linter issue by moving styles to classes
    
    * dxGantt: Fix linter issues with Vue and Angular
    
    * dxGantt: Add ContextMenu demo
    
    Co-authored-by: CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>

--- NetCoreDemos/Controllers/GanttController.cs
+++ NetCoreDemos/Controllers/GanttController.cs
@@ -3,6 +3,10 @@ using DevExtreme.NETCore.Demos.Models.SampleData;
 
 namespace DevExtreme.NETCore.Demos.Controllers {
     public class GanttController : Controller {
+        public IActionResult ContextMenu() {
+            return View();
+        }
+
         public IActionResult ChartAppearance() {
             return View();
         }
