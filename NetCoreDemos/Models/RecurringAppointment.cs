1602039e307971bd49de0f50c3518d16ab35d377
Andrew Makarov <andrey.makarov@devexpress.com>
2020-10-07 19:07:00 +0300

    dxScheduler - update dates in .netcore demos (#212)
    * Update dates in demos
    
    * Refactoring demos
    
    * Correct code formatting

--- NetCoreDemos/Models/RecurringAppointment.cs
+++ NetCoreDemos/Models/RecurringAppointment.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 
 namespace DevExtreme.NETCore.Demos.Models {
-    public class RecurringAppointment {
-        public string Text { get; set; }
+    public class RecurringAppointment : Appointment {
         public int RoomId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string RecurrenceRule { get; set; }
-        public string RecurrenceException { get; set; }
     }
 }
--- NetCoreDemos/Models/Workout.cs
+++ NetCoreDemos/Models/Workout.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 
 namespace DevExtreme.NETCore.Demos.Models {
-    public class Workout {
-        public string Text { get; set; }
+    public class WorkoutAppointment : Appointment {
         public int EmployeeID { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
     }
 }
