8647f377c8fcf82c1deee15782b421f13f2e02d6
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2020-10-07 15:48:40 +0300

    dxPieChart - Add annotations demo (#208)

---
+++ NetCoreDemos/Models/MedalistData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DevExtreme.NETCore.Demos.Models {
+    public class MedalistData {
+        public string Country { get; set; }
+        public string OldCountryName { get; set; }
+        public int Gold { get; set; }
+        public int Silver { get; set; }
+        public int Bronze { get; set; }
+    }
+}
