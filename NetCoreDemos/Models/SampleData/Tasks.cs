72326c1bf63846c6ca8df3101106f1f429efbe72
Andrew Makarov <andrey.makarov@devexpress.com>
2020-10-09 17:11:49 +0300

    Fix linters

--- NetCoreDemos/Models/SampleData/Tasks.cs
+++ NetCoreDemos/Models/SampleData/Tasks.cs
@@ -10,163 +10,163 @@ namespace DevExtreme.NETCore.Demos.Models.SampleData {
                 StartDate = "2021-05-17T06:30:00.000Z",
                 EndDate = "2021-05-17T08:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Book Flights to San Fran for Sales Trip",
                 Priority = Priority.Low,
                 StartDate = "2021-05-20T07:00:00.000Z",
                 EndDate = "2021-05-20T09:00:00.000Z",
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Install New Router in Dev Room",
                 Priority = Priority.Low,
                 StartDate = "2021-05-17T10:00:00.000Z",
                 EndDate = "2021-05-17T12:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Approve Personal Computer Upgrade Plan",
                 Priority = Priority.High,
                 StartDate = "2021-05-18T07:00:00.000Z",
                 EndDate = "2021-05-18T08:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Final Budget Review",
                 Priority = Priority.High,
                 StartDate = "2021-05-18T09:00:00.000Z",
                 EndDate = "2021-05-18T10:35:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "New Brochures",
                 Priority = Priority.High,
                 StartDate = "2021-05-17T10:00:00.000Z",
                 EndDate = "2021-05-17T12:15:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Install New Database",
                 Priority = Priority.Low,
                 StartDate = "2021-05-18T06:00:00.000Z",
                 EndDate = "2021-05-18T09:15:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Approve New Online Marketing Strategy",
                 Priority = Priority.High,
                 StartDate = "2021-05-19T09:00:00.000Z",
                 EndDate = "2021-05-19T11:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Upgrade Personal Computers",
                 Priority = Priority.Low,
                 StartDate = "2021-05-17T06:00:00.000Z",
                 EndDate = "2021-05-17T08:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Prepare 2021 Marketing Plan",
                 Priority = Priority.High,
                 StartDate = "2021-05-20T08:00:00.000Z",
                 EndDate = "2021-05-20T10:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Brochure Design Review",
                 Priority = Priority.Low,
                 StartDate = "2021-05-19T08:00:00.000Z",
                 EndDate = "2021-05-19T10:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Create Icons for Website",
                 Priority = Priority.High,
                 StartDate = "2021-05-21T07:00:00.000Z",
                 EndDate = "2021-05-21T08:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Upgrade Server Hardware",
                 Priority = Priority.Low,
                 StartDate = "2021-05-21T06:00:00.000Z",
                 EndDate = "2021-05-21T12:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Submit New Website Design",
                 Priority = Priority.High,
                 StartDate = "2021-05-21T13:30:00.000Z",
                 EndDate = "2021-05-21T15:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Launch New Website",
                 Priority = Priority.High,
                 StartDate = "2021-05-21T09:20:00.000Z",
                 EndDate = "2021-05-21T11:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Google AdWords Strategy",
                 Priority = Priority.Low,
                 StartDate = "2021-05-24T06:00:00.000Z",
                 EndDate = "2021-05-24T09:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Rollout of New Website and Marketing Brochures",
                 Priority = Priority.Low,
                 StartDate = "2021-05-24T10:00:00.000Z",
                 EndDate = "2021-05-24T12:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Non-Compete Agreements",
                 Priority = Priority.High,
                 StartDate = "2021-05-25T10:00:00.000Z",
                 EndDate = "2021-05-25T12:45:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Approve Hiring of John Jeffers",
                 Priority = Priority.High,
                 StartDate = "2021-05-25T06:00:00.000Z",
                 EndDate = "2021-05-25T09:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Update NDA Agreement",
                 Priority = Priority.Low,
                 StartDate = "2021-05-25T08:00:00.000Z",
                 EndDate = "2021-05-25T11:15:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Update Employee Files with New NDA",
                 Priority = Priority.Low,
                 StartDate = "2021-05-28T06:00:00.000Z",
                 EndDate = "2021-05-28T08:45:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Submit Questions Regarding New NDA",
                 Priority = Priority.Low,
                 StartDate = "2021-05-26T07:00:00.000Z",
                 EndDate = "2021-05-26T08:30:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Submit Signed NDA",
                 Priority = Priority.Low,
                 StartDate = "2021-05-26T10:00:00.000Z",
                 EndDate = "2021-05-26T12:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Review Revenue Projections",
                 Priority = Priority.High,
                 StartDate = "2021-05-26T08:00:00.000Z",
                 EndDate = "2021-05-26T11:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Comment on Revenue Projections",
                 Priority = Priority.High,
                 StartDate = "2021-05-24T07:00:00.000Z",
                 EndDate = "2021-05-24T10:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Provide New Health Insurance Docs",
                 Priority = Priority.High,
                 StartDate = "2021-05-28T09:00:00.000Z",
                 EndDate = "2021-05-28T12:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Review Changes to Health Insurance Coverage",
                 Priority = Priority.High,
                 StartDate = "2021-05-27T06:00:00.000Z",
                 EndDate = "2021-05-27T10:00:00.000Z"
             },
-			new EmployeeAppointment {
+            new EmployeeAppointment {
                 Text = "Review Training Course for any Ommissions",
                 Priority = Priority.Low,
                 StartDate = "2021-05-27T08:00:00.000Z",
