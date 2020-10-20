78efff462d14bf62e62f52a51e28085f8626dee9
polosatov.alexander <alexander.polosatov@devexpress.com>
2020-10-20 13:51:44 +0300

    Fix html support demo

--- JSDemos/Demos/Charts/TooltipHTMLSupport/jQuery/index.js
+++ JSDemos/Demos/Charts/TooltipHTMLSupport/jQuery/index.js
@@ -13,18 +13,31 @@ $(function() {
         tooltip: {
             enabled: true,
             contentTemplate: function(info, container) {
-                $("<div class='state-tooltip'><img src='../../../../images/flags/" +
-                    info.point.data.name.replace(/\s/, "") + ".svg' /><h4>" +
-                    info.argument + "</h4><div><span class='caption'>Capital</span>: " +
-                    info.point.data.capital +
-                    "</div><div><span class='caption'>Population</span>: " +
-                    Globalize.formatNumber(info.value, { maximumFractionDigits: 0 }) +
-                    " people</div>" + "<div><span class='caption'>Area</span>: " +
-                    Globalize.formatNumber(info.point.data.area, { maximumFractionDigits: 0 }) +
-                    " km<sup>2</sup> (" +
-                    Globalize.formatNumber(0.3861 * info.point.data.area, { maximumFractionDigits: 0 }) +
-                    " mi<sup>2</sup>)" + "</div>" + "</div>").appendTo(container);
+                var rootWrapper = $("<div>").attr({ class: "state-tooltip" }).appendTo(container);
+
+                $("<img src='../../../../images/flags/" +
+                    info.point.data.name.replace(/\s/, "") + ".svg' />").appendTo(rootWrapper);
+                $("<h4>").appendTo(rootWrapper).text(info.argument);
+
+                var capitalWrapper = $("<div>").appendTo(rootWrapper);
+                capitalWrapper.append($("<span class='caption'>Capital</span>"));
+
+                $("<span>").appendTo(capitalWrapper).text(": " + info.point.data.capital)
+
+                var populationWrapper = $("<div>").appendTo(rootWrapper);
+                populationWrapper.append($("<span class='caption'>Population</span>"));
+                $("<span>").appendTo(populationWrapper).text(": " + formatNumber(info.value) + " people")
+
+                var areaWrapper = $("<div>").appendTo(rootWrapper);
+                areaWrapper.append($("<span class='caption'>Area</span>"));
+                $("<span>").appendTo(areaWrapper).text(": " + formatNumber(info.point.data.area) + " km")
+                $("<sup>").appendTo(areaWrapper).text("2");
+                $("<span>").appendTo(areaWrapper).text(" (" + formatNumber(0.3861 * info.point.data.area) + " mi")
+                $("<sup>2</sup>").appendTo(areaWrapper)
+                $("<span>").appendTo(areaWrapper).text(")");
             }
         }
     });
 });
+
+var formatNumber = new Intl.NumberFormat("en-US", { maximumFractionDigits: 0 }).format;
