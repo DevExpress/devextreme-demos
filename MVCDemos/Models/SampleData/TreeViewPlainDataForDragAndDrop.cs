c5ca69bb72bd63d42cca178843c1431c7d595282
Sergey Novikov <novsstation@yandex.ru>
2020-10-27 16:16:22 +0300

    Fix icon alignment for MVC demos

--- MVCDemos/Models/SampleData/TreeViewPlainDataForDragAndDrop.cs
+++ MVCDemos/Models/SampleData/TreeViewPlainDataForDragAndDrop.cs
@@ -7,76 +7,91 @@ namespace DevExtreme.MVC.Demos.Models.SampleData {
             new FileSystemItem {
                 Id = "1",
                 Name =  "Documents",
+                Icon = "activefolder",
                 IsDirectory= true,
                 IsExpanded = true
             }, new FileSystemItem {
                 Id = "2",
                 ParentId = "1",
                 Name =  "Projects",
+                Icon = "activefolder",
                 IsDirectory= true,
                 IsExpanded = true
             }, new FileSystemItem {
                 Id = "3",
                 ParentId = "2",
                 Name =  "About.rtf",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "4",
                 ParentId = "2",
                 Name =  "Passwords.rtf",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "5",
                 ParentId = "2",
                 Name =  "About.xml",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "6",
                 ParentId = "2",
                 Name =  "Managers.rtf",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "7",
                 ParentId = "2",
                 Name =  "ToDo.txt",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "8",
                 Name =  "Images",
+                Icon = "activefolder",
                 IsDirectory= true,
                 IsExpanded = true
             }, new FileSystemItem {
                 Id = "9",
                 ParentId = "8",
                 Name =  "logo.png",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "10",
                 ParentId = "8",
                 Name =  "banner.gif",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "11",
                 Name =  "System",
+                Icon = "activefolder",
                 IsDirectory= true,
                 IsExpanded = true
             }, new FileSystemItem {
                 Id = "12",
                 ParentId = "11",
                 Name =  "Employees.txt",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "13",
                 ParentId = "11",
                 Name =  "PasswordList.txt",
+                Icon = "file",
                 IsDirectory= false,
             }, new FileSystemItem {
                 Id = "14",
                 Name =  "Description.rtf",
+                Icon = "file",
                 IsDirectory= false
             }, new FileSystemItem {
                 Id = "15",
                 Name =  "Description.txt",
+                Icon = "file",
                 IsDirectory= false
             }
         };
