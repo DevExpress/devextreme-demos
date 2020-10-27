4aec5ba70fcb757c45d101809b73a1eec7d83e54
Sergey Novikov <57402891+novsstation@users.noreply.github.com>
2020-10-27 17:42:55 +0300

    Treeview - Fix icons alignment in drag and drop demos (#308)
    * Fix icons alignment in drag and drop demos
    
    * Fix icon alignment for MVC demos

--- NetCoreDemos/Models/TreeView/FileSystemItem.cs
+++ NetCoreDemos/Models/TreeView/FileSystemItem.cs
@@ -5,6 +5,7 @@ namespace DevExtreme.NETCore.Demos.Models.TreeView {
         public string Id { get; set; }
         public string ParentId { get; set; }
         public string Name { get; set; }
+        public string Icon { get; set; }
         public bool IsDirectory { get; set; }
         public bool IsExpanded { get; set; }
         public IEnumerable<FileSystemItem> Items { get; set; }
