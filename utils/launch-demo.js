53464023cda86e0f0fd945c911213cbb492635bf
Alexey Babich <alexey.babych@gmail.com>
2020-11-13 15:00:36 +0300

    Add linter for utils folder (#358)

--- utils/launch-demo.js
+++ utils/launch-demo.js
@@ -1,28 +1,24 @@
+'use strict';
+
 const { spawn } = require('child_process');
 const path = require('path');
-const fs = require('fs');
-const fs_utils = require('./fs_utils');
-const pq = require('./prompts_questions');
+const fileSystemUtils = require('./fs_utils');
+const promptsQuestions = require('./prompts_questions');
 const menuMetaData = require('../JSDemos/menuMeta.json');
 
 const baseDemosDir = 'JSDemos/Demos';
-    
-const mainRoutine = async (menuMetaData) => {
-    const demo = await pq.askDemoToUpdate(menuMetaData);
-    const demoPath = fs_utils.getDemoPathByMeta(demo.category, demo.group, demo.demo, baseDemosDir, menuMetaData);
-    const approaches = fs_utils.getApproachesList(demoPath);
-    const demoApproach = await pq.askApproachesFolder(approaches);
-    const command = /^win/.test(process.platform) ? 'http-server.cmd' : 'http-server';
+
+const mainRoutine = async(menuMetaData) => {
+    const demo = await promptsQuestions.askDemoToUpdate(menuMetaData);
+    const demoPath = fileSystemUtils.getDemoPathByMeta(demo.category, demo.group, demo.demo, baseDemosDir, menuMetaData);
+    const approaches = fileSystemUtils.getApproachesList(demoPath);
+    const demoApproach = await promptsQuestions.askApproachesFolder(approaches);
     const openUrl = path.join(demoPath, demoApproach.approach).replace(/\\/g, '/');
-    const arguments = ['-a', 'localhost', '-p', '3000', '-c-1', '-d', '--cors', '--o', openUrl];
-    const server = spawn(command, arguments);
+    const args = ['-a', 'localhost', '-p', '3000', '-c-1', '-d', '--cors', '--o', openUrl];
+    const server = spawn('http-server', args, { shell: true });
 
-    server.stdout.on('data', function(data) {
-        console.log(data.toString());
-    });
-    server.stderr.on('data', function(data) {
-        console.log(data.toString());
-    });
-}
+    server.stdout.on('data', (data) => console.log(data.toString()));
+    server.stderr.on('data', (data) => console.log(data.toString()));
+};
 
-(async () => await mainRoutine(menuMetaData))();
+(async() => await mainRoutine(menuMetaData))();
