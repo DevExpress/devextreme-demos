1cf045a17459989a3552f7762db4a557b253d464
groshenkovamarina <magroshenkova@gmail.com>
2020-12-03 16:15:14 +0300

    Fix lint

--- utils/shared/fs-utils.js
+++ utils/shared/fs-utils.js
@@ -53,7 +53,7 @@ const copyRecursiveSync = (src, dest) => {
             const subSrc = path.join(src, childItemName);
             const subDest = path.join(dest, childItemName);
 
-            // remove only subdirectories before copying (e.g. main directory include commited files) 
+            // remove only subdirectories before copying (e.g. main directory include commited files)
             if(fs.statSync(subSrc).isDirectory()) {
                 if(fs.existsSync(subDest)) {
                     fs.rmdirSync(subDest, { recursive: true });
