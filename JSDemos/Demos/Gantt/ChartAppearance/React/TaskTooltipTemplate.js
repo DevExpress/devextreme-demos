65bc9f66da8e09e60d610efaca81bd81852c49a7
CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>
2020-12-04 10:45:52 +0300

    dxGantt: Update ChartAppearance demo

---
+++ JSDemos/Demos/Gantt/ChartAppearance/React/TaskTooltipTemplate.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function getTimeEstimate(task) {
+  return Math.abs(task.start - task.end) / 36e5;
+}
+
+function getTimeLeft(task) {
+  const timeEstimate = Math.abs(task.start - task.end) / 36e5;
+  return Math.floor((100 - task.progress) / 100 * timeEstimate);
+}
+
+function setCustomTooltipStyles() {
+  const parentElement = document.getElementsByClassName('dx-gantt-task-edit-tooltip')[0];
+  parentElement.className = 'dx-gantt-task-edit-tooltip custom-task-edit-tooltip';
+}
+
+export default function TaskTooltipTemplate(task) {
+  return (
+    <div>
+      {setCustomTooltipStyles()}
+      <div className="custom-tooltip-title">{task.title}</div>
+      <div className="custom-tooltip-row"><span> Estimate: </span>{getTimeEstimate(task)}<span> hours </span></div>
+      <div className="custom-tooltip-row"><span> Left: </span>{getTimeLeft(task)}<span> hours </span></div>
+    </div>
+  );
+}
