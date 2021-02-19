83d22a860d567bd6788b4cd70482bc2f005cab01
Alexey Babich <alexey.babych@gmail.com>
2021-02-19 18:28:17 +0300

    Do not use symlinks (#614)

--- gulpfile.js/copy-mercurial-asp-files.js
+++ gulpfile.js/copy-mercurial-asp-files.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const { src, dest, symlink, parallel, series } = require('gulp');
+const { src, dest, parallel, series } = require('gulp');
 const { existsSync, readFileSync } = require('fs');
 const { join } = require('path');
 const clean = require('gulp-clean');
@@ -70,8 +70,8 @@ exports.copyMercurialAspFiles = series(
             .pipe(dest('NetCoreDemos/wwwroot/SampleData/SampleImages')),
 
         () => src('Demos/WidgetsGallery/WidgetsGallery/build/demo-template.css', { cwd: mercurialPath })
-            .pipe(symlink('MVCDemos/Content'))
-            .pipe(symlink('NetCoreDemos/wwwroot/css')),
+            .pipe(dest('MVCDemos/Content'))
+            .pipe(dest('NetCoreDemos/wwwroot/css')),
 
         () => src('Demos/WidgetsGallery/AspNetCoreDemos.DemoShell/wwwroot/DemoShell/**/*', { cwd: mercurialPath, removeBOM: false })
             .pipe(dest('NetCoreDemos/wwwroot/DemoShell')),
