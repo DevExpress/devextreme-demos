c48edf6b72c6a18d7db83caf7670c02f0a8855f6
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-03-19 14:39:20 +0300

    Scheduler - Horizontal virtual scrolling demo (JQuery)

---
+++ JSDemos/Demos/Scheduler/HorizontalVirtualScrolling/jQuery/index.js
@@ -0,0 +1,31 @@
+$(function () {
+    var appointments = generateAppointments();
+
+    $('#scheduler').dxScheduler({
+        height: 600,
+        maxWidth: 800,
+        currentDate: new Date(2021, 1, 2),
+        dataSource: appointments,
+        views: [{
+            type: 'workWeek'
+        }, {
+            type: 'timelineWorkWeek'
+        }],
+        currentView: "timelineWorkWeek",
+        firstDayOfWeek: 0,
+        startDayHour: 8,
+        endDayHour: 20,
+        cellDuration: 60,
+        scrolling: {
+            mode: 'virtual',
+            orientation: 'both'
+        },
+        showAllDayPanel: false,
+        groups: ["humanId"],
+        resources: [{
+            fieldExpr: "humanId",
+            allowMultiple: false,
+            dataSource: resources
+        }]
+    });
+});
