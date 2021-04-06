4700c1595cebcaafa8ac172140c8c75ac079f636
Mike Vitik <vitikm@gmail.com>
2021-04-06 10:18:37 +0300

    implement demo for all platforms

--- JSDemos/Demos/DataGrid/RecordPaging/Vue/App.vue
+++ JSDemos/Demos/DataGrid/RecordPaging/Vue/App.vue
@@ -1,39 +1,91 @@
 <template>
-  <DxDataGrid
-    :data-source="dataSource"
-    :show-borders="true"
-  >
-    <DxPaging :page-size="10"/>
+  <DxDataGrid 
+    id="gridContainer" 
+    :data-source="dataSource" 
+    :show-borders="true">
+    <DxScrolling row-rendering-mode="virtual" />
+    <DxPaging :page-size="10" />
     <DxPager
-      :show-page-size-selector="true"
+      :visible="true"
       :allowed-page-sizes="pageSizes"
-      :show-info="true"
+      :display-mode="dispalyMode"
+      :show-page-size-selector="showPageSizeSelector"
+      :show-info="showInfo"
+      :show-navigation-buttons="showNavButtons"
     />
-
-    <DxColumn data-field="CompanyName"/>
-    <DxColumn data-field="City"/>
-    <DxColumn data-field="State"/>
-    <DxColumn data-field="Phone"/>
-    <DxColumn data-field="Fax"/>
   </DxDataGrid>
+  <div class="options">
+    <div class="caption">Options</div>
+    <div class="option-container">
+      <div class="option">
+        <span class="option-caption">Display Mode</span>
+        <DxSelectBox
+        id="dispalyModes" 
+        :items="dispalyModes"
+        v-model:value="dispalyMode">
+        </DxSelectBox>
+      </div>
+      <div class="option">
+        <DxCheckBox
+          id="showPageSizes"
+          text="Show Page Sizes Selector"
+          v-model:value="showPageSizeSelector"
+        ></DxCheckBox>
+      </div>
+      <div class="option">
+        <DxCheckBox
+          id="showInfo"
+          text="Show Info Text"
+          :disabled="isCompactMode"
+          v-model:value="showInfo"
+        ></DxCheckBox>
+      </div>
+      <div class="option">
+        <DxCheckBox
+          id="showNavButtons"
+          text="Show Navigation Buttons"
+          :disabled="isCompactMode"
+          v-model:value="showNavButtons"
+        ></DxCheckBox>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-
-import { DxDataGrid, DxColumn, DxPager, DxPaging } from 'devextreme-vue/data-grid';
-import { customers } from './data.js';
+import {
+  DxDataGrid,
+  DxScrolling,
+  DxPager,
+  DxPaging,
+} from "devextreme-vue/data-grid";
+import DxSelectBox from 'devextreme-vue/select-box';
+import DxCheckBox from 'devextreme-vue/check-box';
+import { generateData } from "./data.js";
 
 export default {
   components: {
     DxDataGrid,
-    DxColumn,
+    DxScrolling,
     DxPager,
-    DxPaging
+    DxPaging,
+    DxCheckBox,
+    DxSelectBox,
   },
   data() {
     return {
-      dataSource: customers,
-      pageSizes: [5, 10, 20]
+      dataSource: generateData(100000),
+      dispalyModes: ["full", "compact"],
+      dispalyMode: "full",
+      pageSizes: [5, 10, "all"],
+      showPageSizeSelector: true,
+      showInfo: true,
+      showNavButtons: true,
     };
-  }
+  },
+  computed: {
+    isCompactMode: function() {
+      return this.dispalyMode === "compact";
+    },
+  },
 };
 </script>
