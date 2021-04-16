340706f80357f19edcf40517530af44ec119c4b6
polosatov.alexander <alexander.polosatov@devexpress.com>
2021-04-16 12:19:25 +0300

    ClientSideDataProcessing

--- MVCDemos/Models/SampleData/MonthWeather.cs
+++ MVCDemos/Models/SampleData/MonthWeather.cs
@@ -4,130 +4,37 @@ using System.Collections.Generic;
 namespace DevExtreme.MVC.Demos.Models.SampleData {
     public partial class SampleData {
         public static readonly IEnumerable<Weather> MonthWeather = new[] {
-            new Weather {
-                Day = 1,
-                Temperature = 13.1
-            },
-            new Weather {
-                Day = 2,
-                Temperature = 13.2
-            },
-            new Weather {
-                Day = 3,
-                Temperature = 10.5
-            },
-            new Weather {
-                Day = 4,
-                Temperature = 10.6
-            },
-            new Weather {
-                Day = 5,
-                Temperature = 11.9
-            },
-            new Weather {
-                Day = 6,
-                Temperature = 14.3
-            },
-            new Weather {
-                Day = 7,
-                Temperature = 9.6
-            },
-            new Weather {
-                Day = 8,
-                Temperature = 9.5
-            },
-            new Weather {
-                Day = 9,
-                Temperature = 9.4
-            },
-            new Weather {
-                Day = 10,
-                Temperature = 8.8
-            },
-            new Weather {
-                Day = 11,
-                Temperature = 8.7
-            },
-            new Weather {
-                Day = 12,
-                Temperature = 8.8
-            },
-            new Weather {
-                Day = 13,
-                Temperature = 7.5
-            },
-            new Weather {
-                Day = 14,
-                Temperature = 6.4
-            },
-            new Weather {
-                Day = 15,
-                Temperature = 8.8
-            },
-            new Weather {
-                Day = 16,
-                Temperature = 9.6
-            },
-            new Weather {
-                Day = 17,
-                Temperature = 11.6
-            },
-            new Weather {
-                Day = 18,
-                Temperature = 8.9
-            },
-            new Weather {
-                Day = 19,
-                Temperature = 8.8
-            },
-            new Weather {
-                Day = 20,
-                Temperature = 10.8
-            },
-            new Weather {
-                Day = 21,
-                Temperature = 11.4
-            },
-            new Weather {
-                Day = 22,
-                Temperature = 11.6
-            },
-            new Weather {
-                Day = 23,
-                Temperature = 11.4
-            },
-            new Weather {
-                Day = 24,
-                Temperature = 10.6
-            },
-            new Weather {
-                Day = 25,
-                Temperature = 9.6
-            },
-            new Weather {
-                Day = 26,
-                Temperature = 9.6
-            },
-            new Weather {
-                Day = 27,
-                Temperature = 10.9
-            },
-            new Weather {
-                Day = 28,
-                Temperature = 9.8
-            },
-            new Weather {
-                Day = 29,
-                Temperature = 7.2
-            },
-            new Weather {
-                Day = 30,
-                Temperature = 7.2
-            },
-            new Weather {
-                Day = 31,
-                Temperature = 7.6
-            }
+            new Weather { Day= 1, Temperature= 11 },
+            new Weather { Day= 2, Temperature= 7 },
+            new Weather { Day= 3, Temperature= 6 },
+            new Weather { Day= 4, Temperature= 8 },
+            new Weather { Day= 5, Temperature= 7 },
+            new Weather { Day= 6, Temperature= 7 },
+            new Weather { Day= 7, Temperature= 11 },
+            new Weather { Day= 8, Temperature= 9 },
+            new Weather { Day= 9, Temperature= 5 },
+            new Weather { Day= 10, Temperature= 8 },
+            new Weather { Day= 11, Temperature= 6 },
+            new Weather { Day= 12, Temperature= 9 },
+            new Weather { Day= 13, Temperature= 8 },
+            new Weather { Day= 14, Temperature= 6 },
+            new Weather { Day= 15, Temperature= 6 },
+            new Weather { Day= 16, Temperature= 6 },
+            new Weather { Day= 17, Temperature= 10 },
+            new Weather { Day= 18, Temperature= 9 },
+            new Weather { Day= 19, Temperature= 12 },
+            new Weather { Day= 20, Temperature= 9 },
+            new Weather { Day= 21, Temperature= 8 },
+            new Weather { Day= 22, Temperature= 13 },
+            new Weather { Day= 23, Temperature= 9 },
+            new Weather { Day= 24, Temperature= 7 },
+            new Weather { Day= 25, Temperature= 6 },
+            new Weather { Day= 26, Temperature= 11 },
+            new Weather { Day= 27, Temperature= 8 },
+            new Weather { Day= 28, Temperature= 7 },
+            new Weather { Day= 29, Temperature= 9 },
+            new Weather { Day= 30, Temperature= 7 },
+            new Weather { Day= 31, Temperature= 3 }
         };
     }
 }
