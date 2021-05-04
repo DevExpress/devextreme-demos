c8b82cfea57694b5266d66ae4fee993615ee3bd1
Alexey Babich <alexey.babych@gmail.com>
2021-05-04 16:38:57 +0300

    Add possibility to run demos with bundles (#816) (#830)

--- gulpfile.js/js.js
+++ gulpfile.js/js.js
@@ -1,10 +1,14 @@
 'use strict';
-
+const { join } = require('path');
 const { series, src, dest } = require('gulp');
+const { init } = require('../utils/shared/config-helper');
+const createConfig = require('../utils/internal/create-config.js');
+const demosDir = join(__dirname, '..', 'JSDemos/Demos');
 
 function prepareJs(callback) {
-    require('../utils/shared/config-helper').init();
-    require('../utils/internal/create-config.js');
+    init();
+    createConfig.useBundles = false;
+    createConfig.run(demosDir);
     callback();
 }
 
@@ -13,3 +17,9 @@ exports.js = series(
     () => src('utils/.eslintrc.js')
         .pipe(dest('gulpfile.js'))
 );
+
+exports.bundles = function(callback) {
+    createConfig.useBundles = true;
+    createConfig.run(demosDir);
+    callback();
+};
