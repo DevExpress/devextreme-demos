8a145b1c0aae163886df423eca9112070b2474b5
Denis Gradoboev <idenis.gradoboev@yandex.com>
2021-05-19 18:24:54 +0300

    dxScheduler - Change dates in demos for screenshot tests (#881)
    (cherry picked from commit 189684783845a075fd1d54ff450275baf54e2578)

--- MVCDemos/Models/SampleData/AppointmentsWorkHours.cs
+++ MVCDemos/Models/SampleData/AppointmentsWorkHours.cs
@@ -6,73 +6,73 @@ namespace DevExtreme.MVC.Demos.Models.SampleData {
         public static readonly IEnumerable<DisableDatesAppointment> AppointmentsWorkHours = new[] {
             new DisableDatesAppointment {
                 Text = "Website Re-Design Plan",
-                StartDate = new DateTime(2021, 5, 24, 9, 30, 0),
-                EndDate = new DateTime(2021, 5, 24, 11, 30, 0)
+                StartDate = new DateTime(2021, 4, 26, 9, 30, 0),
+                EndDate = new DateTime(2021, 4, 26, 11, 30, 0)
             },
             new DisableDatesAppointment {
                 Text = "Install New Router in Dev Room",
-                StartDate = new DateTime(2021, 5, 24, 13, 0, 0),
-                EndDate = new DateTime(2021, 5, 24, 14, 0, 0)
+                StartDate = new DateTime(2021, 4, 26, 13, 0, 0),
+                EndDate = new DateTime(2021, 4, 26, 14, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Approve Personal Computer Upgrade Plan",
-                StartDate = new DateTime(2021, 5, 25, 10, 0, 0),
-                EndDate = new DateTime(2021, 5, 25, 11, 0, 0)
+                StartDate = new DateTime(2021, 4, 27, 10, 0, 0),
+                EndDate = new DateTime(2021, 4, 27, 11, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Final Budget Review",
-                StartDate = new DateTime(2021, 5, 25, 13, 30, 0),
-                EndDate = new DateTime(2021, 5, 25, 15, 0, 0)
+                StartDate = new DateTime(2021, 4, 27, 13, 30, 0),
+                EndDate = new DateTime(2021, 4, 27, 15, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "New Brochures",
-                StartDate = new DateTime(2021, 5, 24, 15, 0, 0),
-                EndDate = new DateTime(2021, 5, 24, 16, 15, 0)
+                StartDate = new DateTime(2021, 4, 26, 15, 0, 0),
+                EndDate = new DateTime(2021, 4, 26, 16, 15, 0)
             },
             new DisableDatesAppointment {
                 Text = "Install New Database",
-                StartDate = new DateTime(2021, 5, 26, 9, 45, 0),
-                EndDate = new DateTime(2021, 5, 26, 12, 0, 0)
+                StartDate = new DateTime(2021, 4, 28, 9, 45, 0),
+                EndDate = new DateTime(2021, 4, 28, 12, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Approve New Online Marketing Strategy",
-                StartDate = new DateTime(2021, 5, 26, 14, 30, 0),
-                EndDate = new DateTime(2021, 5, 26, 16, 30, 0)
+                StartDate = new DateTime(2021, 4, 28, 14, 30, 0),
+                EndDate = new DateTime(2021, 4, 28, 16, 30, 0)
             },
             new DisableDatesAppointment {
                 Text = "Upgrade Personal Computers",
-                StartDate = new DateTime(2021, 5, 25, 15, 30, 0),
-                EndDate = new DateTime(2021, 5, 25, 16, 45, 0)
+                StartDate = new DateTime(2021, 4, 27, 15, 30, 0),
+                EndDate = new DateTime(2021, 4, 27, 16, 45, 0)
             },
             new DisableDatesAppointment {
                 Text = "Prepare 2021 Marketing Plan",
-                StartDate = new DateTime(2021, 5, 31, 13, 0, 0),
-                EndDate = new DateTime(2021, 5, 31, 15, 0, 0)
+                StartDate = new DateTime(2021, 5, 3, 13, 0, 0),
+                EndDate = new DateTime(2021, 5, 3, 15, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Brochure Design Review",
-                StartDate = new DateTime(2021, 6, 1, 15, 30, 0),
-                EndDate = new DateTime(2021, 6, 2, 0, 0, 0)
+                StartDate = new DateTime(2021, 5, 4, 15, 30, 0),
+                EndDate = new DateTime(2021, 5, 5, 0, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Create Icons for Website",
-                StartDate = new DateTime(2021, 5, 28, 10, 0, 0),
-                EndDate = new DateTime(2021, 5, 28, 12, 0, 0)
+                StartDate = new DateTime(2021, 4, 30, 10, 0, 0),
+                EndDate = new DateTime(2021, 4, 30, 12, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Upgrade Server Hardware",
-                StartDate = new DateTime(2021, 5, 28, 16, 30, 0),
-                EndDate = new DateTime(2021, 5, 28, 18, 0, 0)
+                StartDate = new DateTime(2021, 4, 30, 16, 30, 0),
+                EndDate = new DateTime(2021, 4, 30, 18, 0, 0)
             },
             new DisableDatesAppointment {
                 Text = "Submit New Website Design",
-                StartDate = new DateTime(2021, 6, 2, 10, 0, 0),
-                EndDate = new DateTime(2021, 6, 2, 11, 30, 0)
+                StartDate = new DateTime(2021, 5, 5, 10, 0, 0),
+                EndDate = new DateTime(2021, 5, 5, 11, 30, 0)
             },
             new DisableDatesAppointment {
                 Text = "Launch New Website",
-                StartDate = new DateTime(2021, 5, 28, 14, 30, 0),
-                EndDate = new DateTime(2021, 5, 28, 16, 10, 0)
+                StartDate = new DateTime(2021, 4, 30, 14, 30, 0),
+                EndDate = new DateTime(2021, 4, 30, 16, 10, 0)
             }
         };
     }
