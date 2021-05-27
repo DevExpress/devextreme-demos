3738397ab4cf3a2c6616b06a071cc7ea6dcce1fb
volnyagin <vconst@users.noreply.github.com>
2021-05-27 18:18:28 +0300

    simplify eslint config for testing dir

--- testing/.eslintrc.js
+++ testing/.eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  extends: ['devextreme/javascript'],
-  globals: {
-    test: true,
-    fixture: true,
-  },
-  rules: {
-    // Allow using any types of dependencies
-    'import/no-extraneous-dependencies': 'off',
-
-    // Allow modifying parameters in event handlers to test these use cases
-    'no-param-reassign': ['error', { props: false }],
-
-    // Allow `await` inside of loops. This is a common case for TestCafe tests
-    'no-await-in-loop': 'off',
-  },
+  extends: ['devextreme/javascript', 'devextreme/testcafe'],
 };
