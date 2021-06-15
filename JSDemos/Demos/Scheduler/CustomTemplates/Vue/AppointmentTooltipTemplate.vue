f86dd2d8680bd182de8297142e49dfa075ecc6b7
Макаров Андрей <>
2021-06-15 14:19:44 +0300

    Fix linter

--- JSDemos/Demos/Scheduler/CustomTemplates/Vue/AppointmentTooltipTemplate.vue
+++ JSDemos/Demos/Scheduler/CustomTemplates/Vue/AppointmentTooltipTemplate.vue
@@ -42,7 +42,6 @@ export default {
   data() {
     return {
       dayOfWeekNames: dayOfWeekNames,
-      Globalize: Globalize,
       movieData: getMovieById(this.templateTooltipModel.appointmentData.movieId)
     };
   },
