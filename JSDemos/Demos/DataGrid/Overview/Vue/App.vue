2e2e5a50dc67c4ca43481508a8c0deabb77f1a11
Roman Semenov <roman.semenov@devexpress.com>
2021-06-28 11:00:34 +0300

    fix lint

--- JSDemos/Demos/DataGrid/Overview/Vue/App.vue
+++ JSDemos/Demos/DataGrid/Overview/Vue/App.vue
@@ -102,7 +102,7 @@ export default {
         store: {
           type: 'odata',
           url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
-          key: "Id",
+          key: 'Id',
           beforeSend: function(request) {
             request.params.startDate = '2020-05-10';
             request.params.endDate = '2020-05-15';
