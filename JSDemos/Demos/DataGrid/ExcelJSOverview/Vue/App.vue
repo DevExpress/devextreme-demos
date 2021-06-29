0395fc2471524d1bd792d233382bf4ae79805dad
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-06-29 10:42:19 +0300

    exceljs.export: try update versions
    (cherry picked from commit 99111220af5a4cea5c519420deb0a9df83048b3a)

--- JSDemos/Demos/DataGrid/ExcelJSOverview/Vue/App.vue
+++ JSDemos/Demos/DataGrid/ExcelJSOverview/Vue/App.vue
@@ -40,13 +40,9 @@
 </template>
 <script>
 import { DxDataGrid, DxColumn, DxExport, DxSelection, DxGroupPanel, DxGrouping } from 'devextreme-vue/data-grid';
+import { Workbook } from 'exceljs';
+import { saveAs } from 'file-saver';
 import { exportDataGrid } from 'devextreme/excel_exporter';
-import ExcelJS from 'exceljs';
-import saveAs from 'file-saver';
-/*
-  // Use this import for codeSandBox
-  import FileSaver from 'file-saver';
-*/
 import service from './data.js';
 
 export default {
