b1bcdb4a9847b0930f16e434b65822f2a7aebaef
Alexey Serov <alexey.serov@devexpress.com>
2021-06-30 14:38:51 +0300

    fix Scheduler-WebAPIService

--- JSDemos/Demos/Scheduler/WebAPIService/test-code.js
+++ JSDemos/Demos/Scheduler/WebAPIService/test-code.js
@@ -1,2 +1,1 @@
-﻿testUtils.postponeUntilFound('.dx-scheduler-appointment-title', 100, 10000)
-.then(testUtils.postponeUntilNotFound('.dx-scheduler-appointment-title', 100, 20000))
+﻿testUtils.postponeUntilFound('.dx-scheduler-appointment-title', 100, 20000).then(testUtils.postpone(200));
