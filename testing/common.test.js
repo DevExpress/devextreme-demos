6a32b2dba81931e6238ec7e6f32f4c948845afab
eugen_zha <eugen.zha@gmail.com>
2021-07-13 13:09:38 +0300

    Try to fix path

--- testing/common.test.js
+++ testing/common.test.js
@@ -39,9 +39,9 @@ fixture`Getting Started`
     t.ctx.watchDogHandle = setTimeout(() => { throw new Error('test timeout exceeded'); }, 3 * 60 * 1000);
     await t.resizeWindow(1000, 800);
   })
-  .clientScripts({ module: 'mockdate' }, 'testing/helpers/test-utils.js')
   // eslint-disable-next-line spellcheck/spell-checker
-  .afterEach((t) => clearTimeout(t.ctx.watchDogHandle));
+  .afterEach((t) => clearTimeout(t.ctx.watchDogHandle))
+  .clientScripts({ module: 'mockdate' }, './helpers/test-utils.js');
 
 const getDemoPaths = (platform) => glob.sync(`JSDemos/Demos/**/${platform}`);
 
