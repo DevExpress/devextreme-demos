d17694a3ffc26cb0a719bef6c1ee4fdc9a1e9a1c
polosatov.alexander <alexander.polosatov@devexpress.com>
2021-07-15 11:22:21 +0300

    fix lint. part1

--- JSDemos/Demos/Charts/SideBySideBar/React/data.js
+++ JSDemos/Demos/Charts/SideBySideBar/React/data.js
@@ -1,25 +1,25 @@
 export const grossProductData = [{
-  state: "Illinois",
+  state: 'Illinois',
   year2016: 803,
   year2017: 823,
   year2018: 863
 }, {
-  state: "Indiana",
+  state: 'Indiana',
   year2016: 316,
   year2017: 332,
   year2018: 332
 }, {
-  state: "Michigan",
+  state: 'Michigan',
   year2016: 452,
   year2017: 459,
   year2018: 470
 }, {
-  state: "Ohio",
+  state: 'Ohio',
   year2016: 621,
   year2017: 642,
   year2018: 675
 }, {
-  state: "Wisconsin",
+  state: 'Wisconsin',
   year2016: 290,
   year2017: 294,
   year2018: 301
