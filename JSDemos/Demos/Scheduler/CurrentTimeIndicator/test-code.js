08fa2b24a41762eb29a275d997f47d77ba5ff39d
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-07-27 18:31:27 +0300

    fix style errors

--- JSDemos/Demos/Scheduler/CurrentTimeIndicator/test-code.js
+++ JSDemos/Demos/Scheduler/CurrentTimeIndicator/test-code.js
@@ -1,10 +1,11 @@
 testUtils.importAnd(() => 'devextreme/ui/scheduler', () => DevExpress.ui.dxScheduler, function (dxScheduler) {
-    var today = new Date();
+    const today = new Date();
     today.setHours(11, 35, 0, 0);
+    
     const indicatorTime = new Date(today.setDate(today.getDate() - today.getDay() + 3));
 
     return testUtils.postponeUntilFound('.dx-scheduler').then(() => {
-        var instance = dxScheduler.getInstance(document.querySelector('.dx-scheduler'));
+        const instance = dxScheduler.getInstance(document.querySelector('.dx-scheduler'));
         instance.option('indicatorTime', indicatorTime);
         testUtils.postpone(2000).then(() => {
             instance.scrollTo(indicatorTime);
