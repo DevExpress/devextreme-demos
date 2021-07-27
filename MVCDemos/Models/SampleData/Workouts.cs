6c91835727b24ef85b53da524a1b58913e4e439d
Denis Gradoboev <idenis.gradoboev@yandex.com>
2021-07-27 11:39:52 +0300

    Fix dates for overwiev demo

--- MVCDemos/Models/SampleData/Workouts.cs
+++ MVCDemos/Models/SampleData/Workouts.cs
@@ -7,80 +7,80 @@ namespace DevExtreme.MVC.Demos.Models.SampleData {
             new WorkoutAppointment {
                 Text = "Helen",
                 EmployeeID = 2,
-                StartDate = "2021-07-27T16:30:00.000Z",
-                EndDate = "2021-07-27T18:30:00.000Z"
+                StartDate = "2021-06-01T16:30:00.000Z",
+                EndDate = "2021-06-01T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Helen",
                 EmployeeID = 2,
-                StartDate = "2021-08-05T16:30:00.000Z",
-                EndDate = "2021-08-06T18:30:00.000Z"
+                StartDate = "2021-06-10T16:30:00.000Z",
+                EndDate = "2021-06-11T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Alex",
                 EmployeeID = 1,
-                StartDate = "2021-07-28T16:30:00.000Z",
-                EndDate = "2021-07-28T18:30:00.000Z"
+                StartDate = "2021-06-02T16:30:00.000Z",
+                EndDate = "2021-06-02T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Alex",
                 EmployeeID = 1,
-                StartDate = "2021-08-06T19:00:00.000Z",
-                EndDate = "2021-08-06T20:00:00.000Z"
+                StartDate = "2021-06-11T19:00:00.000Z",
+                EndDate = "2021-06-11T20:00:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Alex",
                 EmployeeID = 2,
-                StartDate = "2021-08-11T16:30:00.000Z",
-                EndDate = "2021-08-11T18:30:00.000Z"
+                StartDate = "2021-06-16T16:30:00.000Z",
+                EndDate = "2021-06-16T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Stan",
                 EmployeeID = 1,
-                StartDate = "2021-08-02T16:30:00.000Z",
-                EndDate = "2021-08-02T18:30:00.000Z"
+                StartDate = "2021-06-07T16:30:00.000Z",
+                EndDate = "2021-06-07T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Stan",
                 EmployeeID = 1,
-                StartDate = "2021-08-23T16:30:00.000Z",
-                EndDate = "2021-08-23T18:30:00.000Z"
+                StartDate = "2021-06-28T16:30:00.000Z",
+                EndDate = "2021-06-28T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Stan",
                 EmployeeID = 1,
-                StartDate = "2021-08-25T16:30:00.000Z",
-                EndDate = "2021-08-25T18:30:00.000Z"
+                StartDate = "2021-06-30T16:30:00.000Z",
+                EndDate = "2021-06-30T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Rachel",
                 EmployeeID = 2,
-                StartDate = "2021-07-30T16:30:00.000Z",
-                EndDate = "2021-07-30T18:30:00.000Z"
+                StartDate = "2021-06-04T16:30:00.000Z",
+                EndDate = "2021-06-04T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Rachel",
                 EmployeeID = 2,
-                StartDate = "2021-08-02T16:30:00.000Z",
-                EndDate = "2021-08-02T18:30:00.000Z",
+                StartDate = "2021-06-07T16:30:00.000Z",
+                EndDate = "2021-06-07T18:30:00.000Z",
             },
             new WorkoutAppointment {
                 Text = "Rachel",
                 EmployeeID = 1,
-                StartDate = "2021-08-16T16:30:00.000Z",
-                EndDate = "2021-08-16T16:30:00.000Z"
+                StartDate = "2021-06-21T16:30:00.000Z",
+                EndDate = "2021-06-21T16:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Kelly",
                 EmployeeID = 2,
-                StartDate = "2021-08-10T16:30:00.000Z",
-                EndDate = "2021-08-10T18:30:00.000Z"
+                StartDate = "2021-06-15T16:30:00.000Z",
+                EndDate = "2021-06-15T18:30:00.000Z"
             },
             new WorkoutAppointment {
                 Text = "Kelly",
                 EmployeeID = 2,
-                StartDate = "2021-08-24T16:30:00.000Z",
-                EndDate = "2021-08-24T18:30:00.000Z"
+                StartDate = "2021-06-29T16:30:00.000Z",
+                EndDate = "2021-06-29T18:30:00.000Z"
             }
         };
     }
