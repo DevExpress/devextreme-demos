0ec00c74c5229c55372ca39e16fd9090fa33e9ad
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-07-27 18:05:34 +0300

    remove extra 'then' methods

--- JSDemos/Demos/Scheduler/CurrentTimeIndicator/test-code.js
+++ JSDemos/Demos/Scheduler/CurrentTimeIndicator/test-code.js
@@ -1,16 +1,13 @@
 testUtils.importAnd(() => 'devextreme/ui/scheduler', () => DevExpress.ui.dxScheduler, function (dxScheduler) {
     var today = new Date();
     today.setHours(11, 35, 0, 0);
-
     const indicatorTime = new Date(today.setDate(today.getDate() - today.getDay() + 3));
 
-    return testUtils.postponeUntilFound('.dx-scheduler')
-    .then(() => {
+    return testUtils.postponeUntilFound('.dx-scheduler').then(() => {
         var instance = dxScheduler.getInstance(document.querySelector('.dx-scheduler'));
         instance.option('indicatorTime', indicatorTime);
+        testUtils.postpone(2000).then(() => {
+            instance.scrollTo(indicatorTime);
+        })
     })
-    .then(testUtils.postpone(2000).then(() => {
-        dxScheduler.getInstance(document.querySelector('.dx-scheduler')).scrollTo(indicatorTime);
-    }))
-    .then(testUtils.postpone(2000));
 });
