b65efeff08f29b0c9bdf2c08ee4e99b2c0e5a0e7
DendyGrobovshik <idenis.gradoboev@yandex.com>
2021-08-13 14:49:13 +0300

    fix setting wednesday of current week

--- JSDemos/Demos/Scheduler/CurrentTimeIndicator/client-script.js
+++ JSDemos/Demos/Scheduler/CurrentTimeIndicator/client-script.js
@@ -1,3 +1,15 @@
-const date = new Date();
-date.setHours(11, 35, 0, 0);
-MockDate.set(date);
+const getWednesdayThisWeek = () => {
+    WEDNDESDAY_INDEX = 3;
+
+    const date = new Date();
+
+    var currentDay = date.getDay();
+    var distance = WEDNDESDAY_INDEX - currentDay;
+    date.setDate(date.getDate() + distance);
+
+    date.setHours(11, 35, 0, 0);
+
+    return date;
+}
+
+MockDate.set(getWednesdayThisWeek());
