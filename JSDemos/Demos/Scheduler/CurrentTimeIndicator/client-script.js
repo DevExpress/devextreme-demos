ab2bd3503a5a4c2ec2eef16e7aea273bf1ae841e
DendyGrobovshik <idenis.gradoboev@yandex.com>
2021-08-16 12:04:30 +0300

    Fix

--- JSDemos/Demos/Scheduler/CurrentTimeIndicator/client-script.js
+++ JSDemos/Demos/Scheduler/CurrentTimeIndicator/client-script.js
@@ -1,10 +1,10 @@
 const getWednesdayThisWeek = () => {
-    WEDNDESDAY_INDEX = 3;
+    const WEDNESDAY_INDEX = 3;
 
     const date = new Date();
 
     const currentDay = date.getDay();
-    const distance = WEDNDESDAY_INDEX - currentDay;
+    const distance = WEDNESDAY_INDEX - currentDay;
     date.setDate(date.getDate() + distance);
 
     date.setHours(11, 35, 0, 0);
