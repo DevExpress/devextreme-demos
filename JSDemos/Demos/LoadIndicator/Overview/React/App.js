6f3bbf472373556ff75a205267ffec61d6e1f8fe
CORP/zaborshchikov.e <jacksonrr3@yandex.ru>
2021-08-23 11:21:01 +0300

    add themes.initialized to ensure that all styles are applied

--- JSDemos/Demos/LoadIndicator/Overview/React/App.js
+++ JSDemos/Demos/LoadIndicator/Overview/React/App.js
@@ -20,8 +20,8 @@ class App extends React.Component {
       <div className="form">
         <div className="label">Custom size</div>
 
-        <LoadIndicator id="small-indicator" height={20} width={20} />
-        <LoadIndicator id="medium-indicator" height={40} width={40} />
+        <LoadIndicator id="small-indicator" height={20} width={20} />&nbsp;
+        <LoadIndicator id="medium-indicator" height={40} width={40} />&nbsp;
         <LoadIndicator id="large-indicator" height={60} width={60} />
 
         <div className="label">Custom image</div>
