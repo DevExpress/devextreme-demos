5722b27b6984780e8d76f79960952a5ece88a854
e.zaborshchikov <jacksonrr3@yandex.ru>
2021-09-03 11:42:45 +0300

    add test-code.js

---
+++ JSDemos/Demos/DataGrid/RealTimeUpdates/test-code.js
@@ -0,0 +1,3 @@
+testUtils.importAnd(() => ['devextreme/ui/data_grid', 'devextreme/data/data_source'], () => [DevExpress.ui.dxDataGrid, DevExpress.data.DataSource], function(DataGrid, DataSource){
+    
+});
