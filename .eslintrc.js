6d7b1c3c13cfcf66e5f695c48d15c73e8c086db7
babich-a <alexey.babych@gmail.com>
2021-09-07 13:37:55 +0300

    Enable 'no-useless-concat' eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -305,7 +305,6 @@ module.exports = {
     'no-restricted-syntax': 0, // TODO error (was error)
     'no-restricted-properties': 0,
     'guard-for-in': 0, // TODO error (was error)
-    'no-useless-concat': 0,
     'no-underscore-dangle': 0,
     'import/no-webpack-loader-syntax': 0,
     'import/no-unresolved': 0,
