6cfc6cb5a82f938752e59168b36e0f0a69fb114c
babich-a <alexey.babych@gmail.com>
2021-09-08 19:53:30 +0300

    Enable 'no-mixed-operators' eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -292,7 +292,6 @@ module.exports = {
     'consistent-return': 0, // TODO error (was error)
     'no-throw-literal': 0, // TODO error (was error)
     'prefer-const': 0, // TODO error (was error)
-    'no-mixed-operators': 0, // TODO error (was error)
     'radix': 0, // TODO error (was error)
     'no-void': 0, // TODO error (was error)
     'prefer-rest-params': 0, // TODO error (was error)
