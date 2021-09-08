378edd9792124a1f18c04f8c6bdee5b283031058
e.zaborshchikov <jacksonrr3@yandex.ru>
2021-09-08 22:42:48 +0300

    delete test-code.js

--- JSDemos/Demos/DataGrid/RealTimeUpdates/test-code.js
+++
@@ -1,3 +0,0 @@
-testUtils.importAnd(() => ['devextreme/ui/data_grid', 'devextreme/data/data_source'], () => [DevExpress.ui.dxDataGrid, DevExpress.data.DataSource], function(DataGrid, DataSource){
-    
-});
