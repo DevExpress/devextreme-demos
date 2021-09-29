1aecf06d5afe4708564d7b0ab10d74fea09d3926
Roman Semenov <roman.semenov@devexpress.com>
2021-09-29 10:12:31 +0300

    use webapi

--- JSDemos/Demos/DataGrid/AddNewRowPosition/jQuery/index.js
+++ JSDemos/Demos/DataGrid/AddNewRowPosition/jQuery/index.js
@@ -3,7 +3,7 @@ $(() => {
   const scrollingModeOptions = ['standard', 'virtual'];
 
   const dataGrid = $('#gridContainer').dxDataGrid({
-    dataSource: generateData(100000),
+    dataSource,
     showBorders: true,
     columnAutoWidth: true,
     editing: {
@@ -15,34 +15,41 @@ $(() => {
       useIcons: true,
       newRowPosition: 'viewportTop',
     },
-    columns: ['id', 'firstName', 'lastName', 'gender', 'birthDate', {
-      type: 'buttons',
-      buttons: [{
-        icon: 'add',
-        onClick(e) {
-          const changes = dataGrid.option('editing.changes');
-          const key = new DevExpress.data.Guid().toString();
-          changes.push({
-            key,
-            type: 'insert',
-            insertAfterKey: e.row.key,
-          });
-          dataGrid.option('editing.changes', changes);
-          dataGrid.option('editing', {
-            editColumnName: 'id',
-            editRowKey: key,
-          });
-        },
-        visible({ row }) {
-          return !row.isEditing;
-        },
-      }, 'delete'],
-    }],
+    columns: [
+      'OrderID', {
+        dataField: 'OrderDate',
+        dataType: 'date',
+      }, 'Freight', 'ShipName', 'ShipAddress', 'ShipCity', 'ShipPostalCode', 'ShipCountry', {
+        type: 'buttons',
+        buttons: [{
+          icon: 'add',
+          onClick(e) {
+            const changes = dataGrid.option('editing.changes');
+            const key = new DevExpress.data.Guid().toString();
+            changes.push({
+              key,
+              type: 'insert',
+              insertAfterKey: e.row.key,
+            });
+            dataGrid.option('editing.changes', changes);
+            dataGrid.option('editing', {
+              editColumnName: 'OrderID',
+              editRowKey: key,
+            });
+          },
+          visible({ row }) {
+            return !row.isEditing;
+          },
+        }, 'delete'],
+      }],
     toolbar: {
       items: [{
         name: 'addRowButton',
         showText: 'always',
       }],
+    },
+    onRowInserted(e) {
+      e.component.navigateToRow(e.key);
     },
   }).dxDataGrid('instance');
 
