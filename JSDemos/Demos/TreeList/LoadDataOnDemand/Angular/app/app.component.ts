b9b40950773e43d052a848ddf460c2f9758126ad
Roman Semenov <roman.semenov@devexpress.com>
2021-10-11 10:48:53 +0300

    fix
    (cherry picked from commit 6b1eb9fc8f5ad2401dfc39c9e3db47b57c100364)

--- JSDemos/Demos/TreeList/LoadDataOnDemand/Angular/app/app.component.ts
+++ JSDemos/Demos/TreeList/LoadDataOnDemand/Angular/app/app.component.ts
@@ -1,7 +1,7 @@
 import { NgModule, Component, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 
 import { DxTreeListModule } from 'devextreme-angular';
 
@@ -19,11 +19,18 @@ export class AppComponent {
 
     constructor(http: HttpClient) {
         this.dataSource = {
-            load: function(loadOptions) {
-                return http.get("https://js.devexpress.com/Demos/Mvc/api/treeListData?parentIds=" + loadOptions.parentIds)
-                           .toPromise();
+            load: function (loadOptions) {
+                let params = new HttpParams();
+                if (loadOptions.parentIds) {
+                    loadOptions.parentIds.forEach((id) => {
+                        params = params.append("parentIds", id);
+                    });
+                }
+                return http
+                    .get("https://js.devexpress.com/Demos/Mvc/api/treeListData", { params })
+                    .toPromise();
             }
-        }
+        };
     }
 
     customizeSizeText(e) {
