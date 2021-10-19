7eee69713c6742af7f7e6efd2ef5db0b2e1181b2
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2021-10-19 15:27:28 +0300

    initial changes

--- JSDemos/Demos/HtmlEditor/Tables/jQuery/index.js
+++ JSDemos/Demos/HtmlEditor/Tables/jQuery/index.js
@@ -1,7 +1,9 @@
 $(() => {
   $('#html-editor').dxHtmlEditor({
     value: markup,
-    height: 350,
+    height: 750,
+    tableContextMenu: { enabled: true },
+    tableResizing: { enabled: true },
     toolbar: {
       items: [
         {
@@ -11,9 +13,26 @@ $(() => {
         'separator', 'bold', 'color', 'separator',
         'alignLeft', 'alignCenter', 'alignRight', 'separator',
         'insertTable', 'deleteTable',
+        'tableProperties', 'cellProperties',
         'insertRowAbove', 'insertRowBelow', 'deleteRow',
         'insertColumnLeft', 'insertColumnRight', 'deleteColumn',
       ],
+    },
+  });
+
+  $('#resizing').dxCheckBox({
+    text: 'Allow table resizing',
+    value: true,
+    onValueChanged(e) {
+      editor.option('tableResizing.enabled', e.value);
+    },
+  });
+  
+  $('#contextMenu').dxCheckBox({
+    text: 'Allow table context menu',
+    value: true,
+    onValueChanged(e) {
+      editor.option('tableContextMenu.enabled', e.value);
     },
   });
 });
