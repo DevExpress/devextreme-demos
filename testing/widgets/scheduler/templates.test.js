83fbb135dc64d06a9a3b1b1738c5a7eee187b64a
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-11-11 15:01:29 +0300

    Only react test

--- testing/widgets/scheduler/templates.test.js
+++ testing/widgets/scheduler/templates.test.js
@@ -8,32 +8,16 @@ fixture('Scheduler.Templates')
       .resizeWindow(900, 600);
   });
 
-['jQuery', 'React', 'Vue', 'Angular'].forEach((framework) => {
-  runManualTest(test, 'Scheduler', 'Overview', framework, (test) => {
-    test(`Overview cell selection (T1041269) in ${framework}`, async (t) => {
-      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+runManualTest(test, 'Scheduler', 'Overview', 'React', (test) => {
+  test('Overview cell selection (T1041269)', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-      await t.click('.dx-scheduler-date-table-cell', { speed: 0.1 });
+    await t.click('.dx-scheduler-date-table-cell', { speed: 0.1 });
 
-      await takeScreenshot('scheduler_overview_selection.png');
+    await takeScreenshot('scheduler_overview_selection.png');
 
-      await t
-        .expect(compareResults.isValid())
-        .ok(compareResults.errorMessages());
-    });
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
   });
-
-//   runManualTest(test, 'Scheduler', 'CellTemplates', framework, (test) => {
-//     test(`CellTemplates cell selection (T1041269) in ${framework}`, async (t) => {
-//       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-//       await t.click('.dx-scheduler-date-table-cell');
-
-//       await takeScreenshot('scheduler_CellTemplates_selection.png');
-
-//       await t
-//         .expect(compareResults.isValid())
-//         .ok(compareResults.errorMessages());
-//     });
-//   });
 });
