770104ead8ad9c8c43794e927c8cab68470edc2e
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-11-11 15:02:26 +0300

    Refactor

--- testing/widgets/scheduler/templates.test.js
+++ testing/widgets/scheduler/templates.test.js
@@ -12,7 +12,7 @@ runManualTest(test, 'Scheduler', 'Overview', 'React', (test) => {
   test('Overview cell selection (T1041269)', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t.click('.dx-scheduler-date-table-cell', { speed: 0.1 });
+    await t.click('.dx-scheduler-date-table-cell');
 
     await takeScreenshot('scheduler_overview_selection.png');
 
