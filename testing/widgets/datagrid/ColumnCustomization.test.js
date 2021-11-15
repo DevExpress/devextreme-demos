812fd753eb14709bbcafd4e9b9f554aee8d6e485
Roman Semenov <roman.semenov@devexpress.com>
2021-11-15 14:00:18 +0300

    DataGrid - move testcafe tests from farm (#1490)

--- testing/widgets/datagrid/ColumnCustomization.test.js
+++ testing/widgets/datagrid/ColumnCustomization.test.js
@@ -1,7 +1,8 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector as $ } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
-fixture('DataGrid.Columns')
+fixture('DataGrid.ColumnCustomization')
   .page('http://localhost:8080/')
   .beforeEach(async (t) => {
     await t
@@ -12,23 +13,17 @@ runManualTest(test, 'DataGrid', 'ColumnCustomization', 'jQuery', (test) => {
   test('ColumnCustomization', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await takeScreenshot('datagrid_column_customization_1.png');
-
-    await t
-      .click('.dx-icon-column-chooser');
-
+    await t.click('.dx-icon-column-chooser');
     await takeScreenshot('datagrid_column_customization_2.png');
 
-    await t
-      .drag('td[aria-label=\'Column Birth Date\']',
-        500, 200,
-        { offsetX: 5, offsetY: 5 });
-
+    await t.drag(
+      $('td').withAttribute('aria-label', 'Column Birth Date'),
+      500, 200,
+      { offsetX: 5, offsetY: 5 },
+    );
     await takeScreenshot('datagrid_column_customization_3.png');
 
-    await t
-      .click('.dx-closebutton');
-
+    await t.click('.dx-closebutton');
     await takeScreenshot('datagrid_column_customization_4.png');
 
     await t
---
+++ testing/widgets/datagrid/MasterDetailView.test.js
@@ -0,0 +1,28 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector as $ } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+fixture('DataGrid.MasterDetailView')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t
+      .resizeWindow(900, 600);
+  });
+
+runManualTest(test, 'DataGrid', 'MasterDetailView', 'jQuery', (test) => {
+  test('MasterDetailView', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t.click(
+      $('.dx-datagrid-rowsview')
+        .find('.dx-row:not(.dx-master-detail-row)')
+        .nth(1),
+    );
+
+    await takeScreenshot('datagrid_master_detail_api_2_desktop.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
