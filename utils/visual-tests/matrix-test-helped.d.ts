b6f3e81be9fcba14aa6a5c0d3ebb4057f8be11a5
Roman Semenov <roman.semenov@devexpress.com>
2021-12-22 14:01:46 +0300

    remove runManualTestCore from d.ts

--- utils/visual-tests/matrix-test-helped.d.ts
+++ utils/visual-tests/matrix-test-helped.d.ts
@@ -3,4 +3,3 @@ import 'testcafe';
 type FrameworkType = 'jQuery' | 'React' | 'Angular' | 'Vue';
 
 export function runManualTest(product: string, demo: string, framework: FrameworkType | ArrayLike<FrameworkType>, callback: (test: TestFn) => void): void;
-export function runManualTestCore(product: string, demo: string, framework: FrameworkType, callback: (test: TestFn) => void): void;
