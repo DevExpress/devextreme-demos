b702680b207ae6e09bd82f27f45f04d5ef1cfa9d
Roman Semenov <roman.semenov@devexpress.com>
2022-01-12 17:38:22 +0300

    fix localization

---
+++ JSDemos/Demos/Localization/UsingGlobalize/Angular/app/cldr.d.ts
@@ -0,0 +1,14 @@
+declare module 'npm:devextreme-cldr-data/de.json!json' {
+  var json:any;
+  export = json;
+}
+
+declare module 'npm:devextreme-cldr-data/ru.json!json' {
+  var json:any;
+  export = json;
+}
+
+declare module 'npm:devextreme-cldr-data/supplemental.json!json' {
+  var json:any;
+  export = json;
+}
