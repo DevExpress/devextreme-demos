02e6958b332aedf26e23688b95b136d2e891d356
Roman Semenov <roman.semenov@devexpress.com>
2022-01-12 17:38:22 +0300

    fix localization
    (cherry picked from commit b702680b207ae6e09bd82f27f45f04d5ef1cfa9d)

---
+++ JSDemos/Demos/Localization/UsingIntl/Angular/app/messages.d.ts
@@ -0,0 +1,9 @@
+declare module 'npm:devextreme/localization/messages/de.json!json'{
+  var json:any;
+  export = json;
+}
+
+declare module 'npm:devextreme/localization/messages/ru.json!json'{
+  var json:any;
+  export = json;
+}
