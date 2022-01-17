ff042a76b0367a3a14c37a6600aa8d8c4aed1a1e
Roman Semenov <roman.semenov@devexpress.com>
2022-01-17 11:38:00 +0300

    fix localization

--- JSDemos/Demos/Localization/UsingIntl/Angular/app/messages.d.ts
+++ JSDemos/Demos/Localization/UsingIntl/Angular/app/messages.d.ts
@@ -1,9 +1,9 @@
 declare module 'npm:devextreme/localization/messages/de.json!json'{
-  var json:any;
+  const json: object;
   export = json;
 }
 
 declare module 'npm:devextreme/localization/messages/ru.json!json'{
-  var json:any;
+  const json: object;
   export = json;
 }
