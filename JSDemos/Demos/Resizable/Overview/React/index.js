3b5f055e9c1a4fbce158ce9fb8160933fb1741cc
ksercs <ksercs0@gmail.com>
2022-03-22 11:40:38 +0400

    implement React demo

---
+++ JSDemos/Demos/Resizable/Overview/React/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.js';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
