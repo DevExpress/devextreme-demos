a22d913c148c1f908e10c1dbe19dbb2d1a15d0ec
Anton Kuznetsov <ksercs0@gmail.com>
2022-03-23 16:53:43 +0400

    Resizable: add new demo (#1759)

---
+++ MVCDemos/Controllers/ResizableController.cs
@@ -0,0 +1,13 @@
+﻿using DevExtreme.MVC.Demos.Models;
+using DevExtreme.MVC.Demos.Models.SampleData;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DevExtreme.MVC.Demos.Controllers {
+    public class DataGridController : Controller {
+        public ActionResult ResizableOverview() {
+            return View(SampleData.Orders);
+        }
+    }
+}
