390d681bca4ced4f1d14c34506f861b3cd00d4cf
Stanislav Klesarev <stanislav.klesarev@devexpress.com>
2022-04-06 15:35:17 +0400

    Support upload control client uploading

--- MVCDemos/Controllers/ApiControllers/FileUploaderAzureAccessApiController.cs
+++ MVCDemos/Controllers/ApiControllers/FileUploaderAzureAccessApiController.cs
@@ -1,97 +1,78 @@
 ﻿using DevExtreme.MVC.Demos.Models.FileManagement;
-//using Microsoft.Azure.Storage;
-//using Microsoft.Azure.Storage.Auth;
-//using Microsoft.Azure.Storage.Blob;
-using Azure;
-using Azure.Core;
-using Azure.Storage;
-//using Azure.Storage.Auth;
-using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
-using Azure.Storage.Blobs.Specialized;
 using System;
 using System.Net.Http;
 using System.Web.Http;
+using Azure.Storage;
+using Azure.Storage.Sas;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
 
 namespace DevExtreme.MVC.Demos.Controllers.ApiControllers {
     public class FileUploaderAzureAccessApiController : ApiController {
-        const long MaxBlobSize = 1048576;
+        const string ServiceUri = "https://{0}.blob.core.windows.net";
+        const long MaxBlobSize = 104857600;
 
-        //CloudBlobClient _client;
-        //CloudBlobClient Client {
-        //    get {
-        //        if(this._client == null) {
-        //            AzureStorageAccount accountModel = AzureStorageAccount.FileUploader.Value;
-        //            var credentials = new StorageCredentials(accountModel.AccountName, accountModel.AccessKey);
-        //            var account = new CloudStorageAccount(credentials, true);
-        //            this._client = account.CreateCloudBlobClient();
-        //        }
-        //        return this._client;
-        //    }
-        //}
-
-        //CloudBlobContainer _container;
-        //CloudBlobContainer Container {
-        //    get {
-        //        if(this._container == null) {
-        //            AzureStorageAccount accountModel = AzureStorageAccount.FileUploader.Value;
-        //            this._container = Client.GetContainerReference(accountModel.ContainerName);
-        //        }
-        //        return this._container;
-        //    }
-        //}
-
+        BlobServiceClient _client;
+        BlobServiceClient Client {
+            get {
+                if(_client == null) {
+                    AzureStorageAccount accountModel = AzureStorageAccount.FileManager.Value;
+                    StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountModel.AccountName, accountModel.AccessKey);
+                    _client = new BlobServiceClient(new Uri(string.Format(ServiceUri, accountModel.AccountName)), credential);
+                }
+                return _client;
+            }
+        }
+        BlobContainerClient _container;
+        BlobContainerClient Container {
+            get {
+                if(_container == null) {
+                    AzureStorageAccount accountModel = AzureStorageAccount.FileManager.Value;
+                    _container = Client.GetBlobContainerClient(accountModel.ContainerName);
+                }
+                return _container;
+            }
+        }
         [HttpGet]
         [Route("api/file-uploader-azure-access", Name = "FileUploaderAzureAccessApi")]
-        public HttpResponseMessage Process(string command, string blobName) {
-            object result;
-            //try {
-            //    result = UploadBlob(blobName);
-            //} catch {
-            //    result = CreateErrorResult();
-            //}
-            result = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.OK, Content = new StringContent("Test response") };
-            return Request.CreateResponse(result);
+        public object Process(string command, string blobName) {
+            try {
+                return UploadBlob(blobName);
+            } catch {
+                return CreateErrorResult();
+            }
         }
+        object UploadBlob(string blobName) {
+            if(blobName.Contains("/"))
+                return CreateErrorResult("Invalid blob name.");
 
-        //    object UploadBlob(string blobName) {
-        //        if(blobName.Contains("/"))
-        //            return CreateErrorResult("Invalid blob name.");
+            string prefix = Guid.NewGuid().ToString("N");
+            string fullBlobName = $"{prefix}_{blobName}";
+            var blob = Container.GetBlockBlobClient(fullBlobName);
 
-        //        string prefix = Guid.NewGuid().ToString("N");
-        //        string fullBlobName = $"{prefix}_{blobName}";
-        //        CloudBlockBlob blob = Container.GetBlockBlobReference(fullBlobName);
+            if(blob.CanGenerateSasUri) {
+                var sasUri =  blob.GenerateSasUri(BlobSasPermissions.Write, DateTimeOffset.UtcNow.AddHours(1));
+                return CreateSuccessResult(sasUri.AbsoluteUri);
+            } else {
+                return CreateErrorResult("BlobClient cannot generate SasUri");
+            }
 
-        //        if(blob.Exists() && blob.Properties.Length > MaxBlobSize) {
-        //            blob.Delete();
-        //            return CreateErrorResult();
-        //        }
+        }
+        object CreateSuccessResult(string url, string url2 = null) {
+            return new {
+                success = true,
+                accessUrl = url,
+                accessUrl2 = url2
+            };
+        }
+        object CreateErrorResult(string error = null) {
+            if(string.IsNullOrEmpty(error))
+                error = "Unspecified error.";
 
-        //        var policy = new SharedAccessBlobPolicy {
-        //            SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(1),
-        //            Permissions = SharedAccessBlobPermissions.Write
-        //        };
-        //        string url = blob.Uri + blob.GetSharedAccessSignature(policy, null, null, SharedAccessProtocol.HttpsOnly, null);
-
-        //        return CreateSuccessResult(url);
-        //    }
-
-        //    object CreateSuccessResult(string url, string url2 = null) {
-        //        return new {
-        //            success = true,
-        //            accessUrl = url,
-        //            accessUrl2 = url2
-        //        };
-        //    }
-
-        //    object CreateErrorResult(string error = null) {
-        //        if(string.IsNullOrEmpty(error))
-        //            error = "Unspecified error.";
-
-        //        return new {
-        //            success = false,
-        //            error = error
-        //        };
-        //    }
+            return new {
+                success = false,
+                error = error
+            };
+        }
     }
 }
