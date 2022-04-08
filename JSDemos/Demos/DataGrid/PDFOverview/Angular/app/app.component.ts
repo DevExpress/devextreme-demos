ee9b41be309e8053d297b3a97d9755a94732a2e8
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-04-08 10:26:25 +0400

    rename export method

--- JSDemos/Demos/DataGrid/PDFOverview/Angular/app/app.component.ts
+++ JSDemos/Demos/DataGrid/PDFOverview/Angular/app/app.component.ts
@@ -4,7 +4,7 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DxDataGridModule, DxButtonModule, DxDataGridComponent } from 'devextreme-angular';
-import { exportDataGrid as exportDataGridToPdf } from 'devextreme/pdf_exporter';
+import { exportDataGrid } from 'devextreme/pdf_exporter';
 import { jsPDF } from 'jspdf';
 import { Service, Customer } from './app.service';
 
@@ -29,7 +29,7 @@ export class AppComponent {
 
   exportGrid() {
     const doc = new jsPDF();
-    exportDataGridToPdf({
+    exportDataGrid({
       jsPDFDocument: doc,
       component: this.dataGrid.instance,
       indent: 5,
--- JSDemos/Demos/DataGrid/PDFOverview/React/App.js
+++ JSDemos/Demos/DataGrid/PDFOverview/React/App.js
@@ -13,7 +13,7 @@ import Button from 'devextreme-react/button';
 
 import { jsPDF } from 'jspdf';
 
-import { exportDataGrid as exportDataGridToPdf } from 'devextreme/pdf_exporter';
+import { exportDataGrid } from 'devextreme/pdf_exporter';
 
 import { customers } from './data.js';
 
@@ -25,7 +25,7 @@ export default function App() {
     const doc = new jsPDF();
     const dataGrid = dataGridRef.current.instance;
 
-    exportDataGridToPdf({
+    exportDataGrid({
       jsPDFDocument: doc,
       component: dataGrid,
       indent: 5,
