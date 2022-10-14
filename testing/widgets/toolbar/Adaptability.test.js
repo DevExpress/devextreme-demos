ea2a1c5bfeb56378f6b33da045669bca1442d824
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2022-10-14 13:38:36 +0400

    Update tests && Update Screenshots && Fix Angular Demo

--- testing/widgets/toolbar/Adaptability.test.js
+++ testing/widgets/toolbar/Adaptability.test.js
@@ -3,7 +3,7 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
 const RESIZABLE_HANDLE_RIGHT_CLASS = 'dx-resizable-handle-right';
-const RADIO_BUTTON_CLASS = 'dx-radiobutton';
+const CHECKBOX_CLASS = 'dx-checkbox';
 const OPTIONS_CONTAINER_CLASS = 'options-container';
 const TOOLBAR_CLASS = 'dx-toolbar';
 const DROP_DOWN_MENU_BUTTON_CLASS = 'dx-dropdownmenu-button';
@@ -22,7 +22,7 @@ runManualTest('Toolbar', 'Adaptability', ['jQuery', 'React', 'Vue', 'Angular'], 
     await t.drag($(`.${RESIZABLE_HANDLE_RIGHT_CLASS}`), -400, 0);
     await takeScreenshot('toolbar_multiline_mode_minimize.png');
 
-    await t.click($(`.${OPTIONS_CONTAINER_CLASS} .${RADIO_BUTTON_CLASS}`).nth(1));
+    await t.click($(`.${OPTIONS_CONTAINER_CLASS} .${CHECKBOX_CLASS}`).nth(1));
     await takeScreenshot('toolbar_singleline_mode_init.png');
 
     await t.click($(`.${TOOLBAR_CLASS} .${DROP_DOWN_MENU_BUTTON_CLASS}`));
