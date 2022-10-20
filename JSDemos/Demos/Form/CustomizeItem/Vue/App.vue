75730ce4998005c89086d611d31f15a71a53408d
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-10-20 13:41:08 +0300

    Vue

--- JSDemos/Demos/Form/CustomizeItem/Vue/App.vue
+++ JSDemos/Demos/Form/CustomizeItem/Vue/App.vue
@@ -10,56 +10,123 @@
       <DxItem
         :editor-options="nameEditorOptions"
         data-field="FirstName"
-      />
+      >
+        <DxLabel template="nameLabel"/>
+      </DxItem>
       <DxItem
         :editor-options="positionEditorOptions"
         :validation-rules="validationRules.position"
         data-field="Position"
         editor-type="dxSelectBox"
-      />
+      >
+        <DxLabel template="positionLabel"/>
+      </DxItem>
       <DxItem
         :editor-options="nameEditorOptions"
         data-field="LastName"
-      />
+      >
+        <DxLabel template="nameLabel"/>
+      </DxItem>
       <DxItem
         :editor-options="hireDateEditorOptions"
         :validation-rules="validationRules.hireDate"
         data-field="HireDate"
         editor-type="dxDateBox"
-      />
+      >
+        <DxLabel template="dateLabel"/>
+      </DxItem>
       <DxItem
         :editor-options="birthDateEditorOptions"
         data-field="BirthDate"
         editor-type="dxDateBox"
-      />
+      >
+        <DxLabel template="dateLabel"/>
+      </DxItem>
       <DxItem
         data-field="Address"
-      />
+      >
+        <DxLabel template="addressLabel"/>
+      </DxItem>
       <DxItem
         :col-span="2"
         :editor-options="notesEditorOptions"
         data-field="Notes"
         editor-type="dxTextArea"
-      />
+      >
+        <DxLabel template="notesLabel"/>
+      </DxItem>
       <DxItem
         :editor-options="phonesEditorOptions"
         data-field="Phone"
-      />
+      >
+        <DxLabel template="phoneLabel"/>
+      </DxItem>
       <DxItem
         data-field="Email"
-      />
+      >
+        <DxLabel template="emailLabel"/>
+      </DxItem>
+      <template #nameLabel="{ data }">
+        <LabelTemplate
+          :data="data"
+          icon="user"
+        />
+      </template>
+      <template #positionLabel="{ data }">
+        <LabelTemplate
+          :data="data"
+          icon="info"
+        />
+      </template>
+      <template #dateLabel="{ data }">
+        <LabelTemplate
+          :data="data"
+          icon="event"
+        />
+      </template>
+      <template #addressLabel="{ data }">
+        <LabelTemplate
+          :data="data"
+          icon="home"
+        />
+      </template>
+      <template #notesLabel="{ data }">
+        <LabelNotesTemplate
+          :data="data"
+        />
+      </template>
+      <template #phoneLabel="{ data }">
+        <LabelTemplate
+          :data="data"
+          icon="phone"
+        />
+      </template>
+      <template #emailLabel="{ data }">
+        <LabelTemplate
+          :data="data"
+          icon="email"
+        />
+      </template>
     </DxGroupItem>
   </DxForm>
 </template>
 <script>
-import { DxForm, DxItem } from 'devextreme-vue/form';
+import { DxForm, DxItem, DxLabel } from 'devextreme-vue/form';
+import { DxTooltip } from 'devextreme-vue/tooltip';
 import service from './data.js';
 import 'devextreme-vue/text-area';
+
+import LabelTemplate from './LabelTemplate.vue';
+import LabelNotesTemplate from './LabelNotesTemplate.vue';
 
 export default {
   components: {
     DxForm,
     DxItem,
+    DxLabel,
+    DxTooltip,
+    LabelTemplate,
+    LabelNotesTemplate,
   },
   data() {
     const employee = service.getEmployee();
---
+++ JSDemos/Demos/Form/CustomizeItem/Vue/LabelNotesTemplate.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span>
+      <i class="dx-icon dx-icon-comment"/>
+      Additional
+      <br>
+      <i
+        id="helpedInfo"
+        class="dx-icon dx-icon-info"
+      />
+      { data.text }
+    </span>
+
+    <DxTooltip
+      target="#helpedInfo"
+      show-event="mouseenter"
+      hide-event="mouseleave"
+    >
+      <b>This field must not exceed 200 characters</b>
+    </DxTooltip>
+  </div>
+</template>
+<script>
+
+import { DxTooltip } from 'devextreme-vue/tooltip';
+
+export default {
+  components: {
+    DxTooltip,
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
