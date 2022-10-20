b43031360f4e8776b54a7fe8d7a29a0101ec572c
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-10-20 07:35:08 +0300

    refactor

--- JSDemos/Demos/Form/CustomizeItem/jQuery/index.js
+++ JSDemos/Demos/Form/CustomizeItem/jQuery/index.js
@@ -10,6 +10,9 @@ $(() => {
         editorOptions: {
           disabled: true,
         },
+        label: {
+          template: labelTemplate('user'),
+        },
       }, {
         dataField: 'Position',
         editorType: 'dxSelectBox',
@@ -22,10 +25,16 @@ $(() => {
           type: 'required',
           message: 'Position is required',
         }],
+        label: {
+          template: labelTemplate('info'),
+        },
       }, {
         dataField: 'LastName',
         editorOptions: {
           disabled: true,
+        },
+        label: {
+          template: labelTemplate('user'),
         },
       }, {
         dataField: 'HireDate',
@@ -38,6 +47,9 @@ $(() => {
           type: 'required',
           message: 'Hire date is required',
         }],
+        label: {
+          template: labelTemplate('event'),
+        },
       }, {
         dataField: 'BirthDate',
         editorType: 'dxDateBox',
@@ -45,7 +57,15 @@ $(() => {
           disabled: true,
           width: '100%',
         },
-      }, 'Address', {
+        label: {
+          template: labelTemplate('event'),
+        },
+      }, {
+        dataField: 'Address',
+        label: {
+          template: labelTemplate('home'),
+        },
+      }, {
         colSpan: 2,
         dataField: 'Notes',
         editorType: 'dxTextArea',
@@ -53,15 +73,41 @@ $(() => {
           height: 90,
           maxLength: 200,
         },
+        label: {
+          template: (data, element) => {
+            element.append(`<span><i class='dx-icon dx-icon-comment'></i>Additional<br><i id="helpedInfo" class='dx-icon dx-icon-info'></i>${data.text}</span>`);
+
+            $('<div>').dxTooltip({
+              target: '#helpedInfo',
+              showEvent: 'mouseenter',
+              hideEvent: 'mouseleave',
+              contentTemplate(args) {
+                args.html('<b>This field must not exceed 200 characters</b>');
+              },
+            }).appendTo(element);
+          },
+        },
       }, {
         dataField: 'Phone',
         editorOptions: {
           mask: '+1 (X00) 000-0000',
           maskRules: { X: /[02-9]/ },
         },
-      }, 'EmailRule'],
+        label: {
+          template: labelTemplate('tel'),
+        },
+      }, {
+        dataField: 'Email',
+        label: {
+          template: labelTemplate('email'),
+        },
+      }],
     }],
   });
 
   $('#form').dxForm('instance').validate();
+
+  function labelTemplate(iconName) {
+    return (data) => $(`<span><i class='dx-icon dx-icon-${iconName}'></i>${data.text}</span>`);
+  }
 });
