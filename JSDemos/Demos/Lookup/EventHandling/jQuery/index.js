4058de3b831fd709c35cccc7cd6207d9269a9cdb
Marc Bambalan <marcbambi@gmail.com>
2022-11-08 14:18:37 +0800

    Lookup "Handle Value Change" demo 22.1: Add the options area

--- JSDemos/Demos/Lookup/EventHandling/jQuery/index.js
+++ JSDemos/Demos/Lookup/EventHandling/jQuery/index.js
@@ -1,5 +1,5 @@
 $(() => {
-  $('#lookup').dxLookup({
+  const lookup = $('#lookup').dxLookup({
     items: employees,
     displayExpr(item) {
       if (!item) {
@@ -8,6 +8,7 @@ $(() => {
 
       return `${item.FirstName} ${item.LastName}`;
     },
+    applyValueMode: 'instantly',
     dropDownOptions: {
       showTitle: false,
     },
@@ -17,5 +18,13 @@ $(() => {
       $('#selected-employee-img').attr('src', e.value.Picture);
       $('#selected-employee-notes').text(e.value.Notes);
     },
-  });
+  }).dxLookup('instance');
+  
+  $('#applyValueMode').dxSelectBox({
+    items: ['instantly', 'useButtons'],
+    value: applyValueMode,
+    onValueChanged(e) {
+      lookup.option('applyValueMode', e.value);
+    }
+  })
 });
