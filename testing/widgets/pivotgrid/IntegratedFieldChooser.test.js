94ce547586e562a467be3dd4bf8e27c7f69971fd
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-11-15 11:06:04 +0400

    PivotGrid: Hierarchy icons overlap in fieldChooser in the IntegratedField Chooser demo (T1126747)

---
+++ testing/widgets/pivotgrid/IntegratedFieldChooser.test.js
@@ -0,0 +1,34 @@
+import { Selector } from 'testcafe';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+const PIVOTGRID_FIELD_CHOOSER_BUTTON = 'dx-pivotgrid-field-chooser-button';
+const TREEVIEW_SEARCH_FIELD = 'dx-treeview-search';
+const TEXTEDITOR_INPUT = 'dx-texteditor-input';
+
+fixture('PivotGrid.IntegratedFieldChooser')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t
+      .resizeWindow(900, 600);
+  });
+
+runManualTest('PivotGrid', 'IntegratedFieldChooser', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('IntegratedFieldChooser', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t.click(Selector(`.${PIVOTGRID_FIELD_CHOOSER_BUTTON}`));
+
+    await takeScreenshot('Integrated field chooser.png');
+
+    const textEditorInput = Selector(`.${TREEVIEW_SEARCH_FIELD} .${TEXTEDITOR_INPUT}`);
+    await t
+      .typeText(textEditorInput, 'Product M');
+
+    await takeScreenshot('Integrated field chooser after search of hierarchy field.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
