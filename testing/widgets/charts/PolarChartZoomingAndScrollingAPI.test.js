03a8a4458dda2d9adc84c6526245be2310edef14
polosatov.alexander <alexander.polosatov@devexpress.com>
2022-11-28 15:47:06 +0400

    PolarChartZoomingAndScrollingAPI

---
+++ testing/widgets/charts/PolarChartZoomingAndScrollingAPI.test.js
@@ -0,0 +1,24 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector as $ } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+fixture('Charts.PolarChartZoomingAndScrollingAPI')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t
+      .resizeWindow(900, 600);
+  });
+
+runManualTest('Charts', 'PolarChartZoomingAndScrollingAPI', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('PolarChartZoomingAndScrollingAPI. RangeSelector drag', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t.drag($('.slider-marker').nth(1), 500, 0);
+    await t.wait(1000);
+    await takeScreenshot('zoom_polar_chart.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
