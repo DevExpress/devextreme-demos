cd550ffdc9b78a8a4b16292fc31f04868ac1e754
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2022-12-19 10:51:23 +0400

    fixed date display issue and added testcafe test (#2321)

---
+++ testing/widgets/scheduler/cellTemplates.test.js
@@ -0,0 +1,25 @@
+import { Selector as $ } from 'testcafe';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+fixture('Scheduler.CellTemplates')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t
+      .resizeWindow(900, 600);
+  });
+
+runManualTest('Scheduler', 'CellTemplates', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('CellTemplates', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t
+      .click($('.dx-widget').withAttribute('aria-label', 'Month'))
+      .expect(
+        await takeScreenshot('scheduler_CellTemplates_month_view.png'),
+      ).ok();
+
+    await t.expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
