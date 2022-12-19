b6f6be9b1c8f0ad693d3e9f943534f82b368ca05
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2022-12-19 19:32:28 +0700

    fix(@Tests): Edit img selector

--- testing/widgets/common/DialogsAndNotificationsOverview.test.js
+++ testing/widgets/common/DialogsAndNotificationsOverview.test.js
@@ -2,20 +2,20 @@ import { Selector as $ } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
-const IMAGE_ID = 'house-image1';
+const IMAGES_CONTAINER_CLASS = 'images';
+const ITEM_CONTENT_CLASS = 'item-content';
 
 fixture('Common.DialogsAndNotificationsOverview')
   .page('http://localhost:8080/')
   .beforeEach(async (t) => {
-    await t
-      .resizeWindow(900, 600);
+    await t.resizeWindow(900, 600);
   });
 
 runManualTest('Common', 'DialogsAndNotificationsOverview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
   test('DialogsAndNotificationsOverview', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t.click($(`#${IMAGE_ID}`));
+    await t.click($(`.${IMAGES_CONTAINER_CLASS} .${ITEM_CONTENT_CLASS} img`));
     await takeScreenshot('common_dialogs_and_notifications_overview_popup.png');
 
     await t
