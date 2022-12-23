d802ca365a4a71a5233dc043d1565fd37ac02793
Mark Allen Ramirez <markallenramirez@gmail.com>
2022-12-23 17:27:38 +0800

    add mvc
    (cherry picked from commit ec5ee80b1ef17d874283426781944d6423a97fa9)

---
+++ MVCDemos/Models/TileViewItem.cs
@@ -0,0 +1,18 @@
+using Newtonsoft.Json;
+
+namespace DevExtreme.MVC.Demos.Models {
+    public class TileViewItem {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Price { get; set; }
+        public string ImageSrc { get; set; }
+
+        [JsonProperty("heightRatio")]
+        public int? HeightRatio { get; set; }
+
+        [JsonProperty("widthRatio")]
+        public int? WidthRatio { get; set; }
+    }
+}
