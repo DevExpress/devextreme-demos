fa8c6eb57c3df18041bae73d371763c4fcf8729b
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-01-18 14:10:03 +0800

    TileView: Add ASP.NET demos (Basics) (#2338)

---
+++ NetCoreDemos/Controllers/TileViewController.cs
@@ -0,0 +1,10 @@
+using DevExtreme.NETCore.Demos.Models.SampleData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevExtreme.NETCore.Demos.Controllers {
+    public class TileViewController : Controller {
+        public ActionResult Basics() {
+            return View(SampleData.TileViewItems);
+        }
+    }
+}
