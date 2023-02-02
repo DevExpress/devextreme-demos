40c594f81e72d731a495833cb047761b35443ee9
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2023-02-02 14:12:18 +0200

    Shared azure.file.system.js source file across file manager and upload demos (#2430)
    * Shared azure.file.system.js source file across file manager and upload demos
    
    * Copy shared file without rebuild Mvc

--- gulpfile.js/mvc.js
+++ gulpfile.js/mvc.js
@@ -3,8 +3,10 @@ const { series } = require('gulp');
 
 const { copyCommonAspFiles } = require('./copy-common-asp-files');
 const { copyMercurialAspFiles } = require('./copy-mercurial-asp-files');
+const { copyMvcSharedResources } = require('../utils/copy-shared-resources/copy');
 
 exports.mvc = series(
   copyCommonAspFiles,
   copyMercurialAspFiles,
+  copyMvcSharedResources,
 );
