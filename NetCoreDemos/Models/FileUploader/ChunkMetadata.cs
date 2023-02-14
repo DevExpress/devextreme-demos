6a4f208d7c2b5945d0e502da5c2c726f1dd3a18c
OlegKipchatov <kipchatovbrend@yandex.ru>
2023-02-14 19:07:21 +0400

    Update security

---
+++ NetCoreDemos/Models/FileUploader/ChunkMetadata.cs
@@ -0,0 +1,10 @@
+﻿namespace DevExtreme.NETCore.Demos.Models.FileUploader {
+    public class ChunkMetadata {
+        public int Index { get; set; }
+        public int TotalCount { get; set; }
+        public int FileSize { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public string FileGuid { get; set; }
+    }
+}
