a3f7963967f8d94b83af85970cb167cf45237141
Marc Bambalan <marcbambi@gmail.com>
2023-02-28 15:40:45 +0800

    Add asyncForEach helper and update visual test

--- testing/widgets/common/EditorAppearanceVariants.test.js
+++ testing/widgets/common/EditorAppearanceVariants.test.js
@@ -1,12 +1,12 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { Selector as $ } from 'testcafe';
+import asyncForEach from '../../../utils/visual-tests/helpers/async-for-each-helper';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
 fixture('Common.EditorAppearanceVariants')
   .page('http://localhost:8080/')
   .beforeEach(async (t) => {
-    await t
-      .resizeWindow(900, 600);
+    await t.resizeWindow(900, 600);
   });
 
 runManualTest('Common', 'EditorAppearanceVariants', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
@@ -18,31 +18,27 @@ runManualTest('Common', 'EditorAppearanceVariants', ['jQuery', 'React', 'Vue', '
 
     const clickSaveButton = async () => {
       await t.click($('.validate.dx-button'));
-    }
+    };
 
     const changeLabelMode = async (labelMode) => {
       await t.click($('.dx-selectbox').nth(3));
       await t.click($('.dx-item').withText(labelMode));
-    }
+    };
 
     const changeStylingMode = async (stylingMode) => {
       await t.click($('.dx-selectbox').nth(2));
       await t.click($('.dx-item').withText(stylingMode)).wait(200);
-    }
+    };
 
-    for (let stylingMode of stylingModes) {
-      for (let labelMode of labelModes) {
+    await asyncForEach(stylingModes, async (stylingMode) => {
+      await asyncForEach(labelModes, async (labelMode) => {
         await changeStylingMode(stylingMode);
         await changeLabelMode(labelMode);
         await clickSaveButton();
         await takeScreenshot(`common_editor_appearance_variants_${stylingMode}_${labelMode}_desktop.png`);
-      }
-    }
+      });
+    });
 
-
-
-    await t
-      .expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
+    await t.expect(compareResults.isValid()).ok(compareResults.errorMessages());
   });
 });
