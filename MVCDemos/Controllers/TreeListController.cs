ffef16521e4c20e7c8d6318b2d9beb12b16278f7
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2023-04-13 18:15:08 +0300

    DataGrid / TreeList Demos: add missing crud configuration settings for existing ui (ref #895)

--- MVCDemos/Controllers/TreeListController.cs
+++ MVCDemos/Controllers/TreeListController.cs
@@ -40,7 +40,7 @@ namespace DevExtreme.MVC.Demos.Controllers {
             return View();
         }
         public ActionResult CustomizeKeyboardNavigation() {
-            return View(SampleData.TreeListEmployees);
+            return View();
         }
         public ActionResult ColumnChooser() {
             return View(SampleData.TreeListEmployees);
