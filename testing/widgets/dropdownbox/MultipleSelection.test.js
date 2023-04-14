0e72670b4d0cb8341b19bed77000ead3815acf56
Marc Bambalan <marcbambi@gmail.com>
2023-04-14 10:45:41 +0800

    DropDownBox Multiple Selection: Add visual test

---
+++ testing/widgets/dropdownbox/MultipleSelection.test.js
@@ -0,0 +1,32 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector as $ } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+fixture('DropDownBox.MultipleSelection')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t.resizeWindow(900, 600);
+  });
+
+runManualTest('DropDownBox', 'MultipleSelection', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test.only('MultipleSelection', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t.click($('.dx-dropdownbox').nth(0))
+        .wait(200)
+        
+    await takeScreenshot('dropdownbox_multiple_selection_treebox');
+
+    await t.click($('.dx-dropdownbox').nth(0))
+        .wait(200);
+
+    await t.click($('.dx-dropdownbox').nth(1))
+        .wait(200)
+        
+    await takeScreenshot('dropdownbox_multiple_selection_gridbox');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
