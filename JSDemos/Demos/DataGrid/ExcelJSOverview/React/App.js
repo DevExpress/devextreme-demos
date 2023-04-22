a67dd22ec81818191d1e02ae699aa5459e06dbb5
CORP\serhii.pylypchuk <serhii.pylypchuk@devexpress.com>
2023-04-22 14:14:49 +0400

    DataGrid: import regenerator-runtime (csp/exceljs)

--- JSDemos/Demos/DataGrid/ExcelJSOverview/React/App.js
+++ JSDemos/Demos/DataGrid/ExcelJSOverview/React/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DataGrid, {
   Column, Export, Selection, GroupPanel, Grouping,
 } from 'devextreme-react/data-grid';
+import 'regenerator-runtime/runtime';
 import { Workbook } from 'exceljs';
 import { saveAs } from 'file-saver-es';
 // Our demo infrastructure requires us to use 'file-saver-es'.
