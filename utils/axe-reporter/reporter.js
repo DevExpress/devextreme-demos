dca55a8760113ee1fd68a0f31cf4d16c7919294b
alexlavrov <alex.s.lavrov@gmail.com>
2023-05-17 18:39:20 +0400

    Fix reporter

--- utils/axe-reporter/reporter.js
+++ utils/axe-reporter/reporter.js
@@ -3,10 +3,13 @@ import path from 'path';
 import {
   appendFileSync,
   readFileSync,
-  writeFileSync,
   mkdirSync,
   existsSync,
 } from 'fs';
+
+const reportDir = './testing/artifacts/axe';
+const reportFileName = `${process.env.CONSTEL.replace(/[()/]/g, '_')}axe_report.md`;
+const template = readFileSync(path.join(__dirname, 'template.md'), { encoding: 'utf8' });
 
 function replaceBase64(html) {
   return html.replace(/data:image\/png;base64.+"/, '..."');
@@ -30,17 +33,13 @@ function simplifyResults(r) {
 }
 
 export function appendMdReport({ testName, results }) {
-  const template = readFileSync('./utils/axe-reporter/template.md', { encoding: 'utf8' });
   // eslint-disable-next-line max-len
   const mdString = `${mustache.render(template, { testName, results: simplifyResults(results) })} \n\n`;
-  const reportDir = './testing/artifacts/axe';
-  const reportFileName = 'axe_report.md';
   const reportPath = path.join(reportDir, reportFileName);
 
   if (!existsSync(reportPath)) {
     mkdirSync(reportDir, { recursive: true });
-    writeFileSync(reportPath, mdString);
-  } else {
-    appendFileSync(reportPath, mdString);
   }
+
+  appendFileSync(reportPath, mdString);
 }
