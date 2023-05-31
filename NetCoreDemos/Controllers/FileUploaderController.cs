c0ac829845d28ff3293b1a96ab035071b608f3c3
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-05-31 15:40:28 +0800

    FileUploader - Async Upload: Fix parameter issue (#2616)

--- NetCoreDemos/Controllers/FileUploaderController.cs
+++ NetCoreDemos/Controllers/FileUploaderController.cs
@@ -17,9 +17,9 @@ namespace DevExtreme.NETCore.Demos.Controllers {
 
         [HttpPost]
         [EnableCors("CorsPolicy")]
-        public ActionResult Upload(IFormFile file) {
+        public ActionResult Upload(IFormFile myFile) {
             try {
-                // Write code that saves the 'file' file.
+                // Write code that saves the 'myFile' file.
                 // Don't rely on or trust the FileName property without validation
             } catch {
                 return BadRequest();
