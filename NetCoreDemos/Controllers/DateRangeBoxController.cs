512866c7fa4ce64ccd7f8831661193e54f963900
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-05-31 15:38:44 +0400

    MVCCOre

--- NetCoreDemos/Controllers/DateRangeBoxController.cs
+++ NetCoreDemos/Controllers/DateRangeBoxController.cs
@@ -5,5 +5,8 @@ namespace DevExtreme.NETCore.Demos.Controllers {
         public ActionResult Overview() {
             return View();
         }
+        public ActionResult Formatting() {
+            return View();
+        }
     }
 }
