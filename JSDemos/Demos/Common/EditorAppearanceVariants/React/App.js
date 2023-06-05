cd747a513819823f682557a13ab02fba9048e448
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-06-05 11:01:33 +0400

    use hooks

--- JSDemos/Demos/Common/EditorAppearanceVariants/React/App.js
+++ JSDemos/Demos/Common/EditorAppearanceVariants/React/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import SelectBox from 'devextreme-react/select-box';
 import TextBox from 'devextreme-react/text-box';
@@ -8,19 +8,12 @@ import Button from 'devextreme-react/button';
 import Validator, { RequiredRule } from 'devextreme-react/validator';
 import notify from 'devextreme/ui/notify';
 
-import { states } from './data.js';
-
-const stylingModes = ['outlined', 'filled', 'underlined'];
-const labelModes = ['static', 'floating', 'hidden'];
-const notesLabel = { 'aria-label': 'Notes' };
-const birthDateLabel = { 'aria-label': 'Birth Date' };
-const hireDateLabel = { 'aria-label': 'Hire Date' };
-const nameLabel = { 'aria-label': 'Name' };
-const addressLabel = { 'aria-label': 'Address' };
-const phoneLabel = { 'aria-label': 'Phone' };
-const stateLabel = { 'aria-label': 'State' };
-const labelModeLabel = { 'aria-label': 'Label Mode' };
-const stylingModeLabel = { 'aria-label': 'Styling Mode' };
+import {
+  states, stylingModes, labelModes, notesLabel,
+  birthDateLabel, hireDateLabel,
+  nameLabel, addressLabel, phoneLabel,
+  stateLabel, labelModeLabel, stylingModeLabel,
+} from './data.js';
 
 const birthDate = new Date(1981, 5, 3);
 const phoneRules = {
@@ -35,167 +28,149 @@ function validateClick(e) {
     notify('The task was not saved. Please check if all fields are valid.', 'error');
   }
 }
-class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      stylingMode: 'filled',
-      labelMode: 'static',
-    };
+export function App() {
+  const [stylingMode, setStylingMode] = useState('filled');
+  const [labelMode, setLabelMode] = useState('static');
+  const changeStylingMode = useCallback(({ value }) => {
+    setStylingMode(value);
+  }, []);
+  const labelModeChange = useCallback(({ value }) => {
+    setLabelMode(value);
+  }, []);
 
-    this.stylingModeChange = this.stylingModeChange.bind(this);
-    this.labelModeChange = this.labelModeChange.bind(this);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="title">Edit Profile</div>
-        <div className="editors">
-          <div className="flex-container editors-padding-right">
-            <div className="left">
-              <TextBox
-                stylingMode={this.state.stylingMode}
-                defaultValue="Olivia Peyton"
-                width="100%"
-                inputAttr={nameLabel}
-                placeholder="Type..."
-                label="Name"
-                labelMode={this.state.labelMode}
-              >
-                <Validator>
-                  <RequiredRule />
-                </Validator>
-              </TextBox>
-              <TextBox
-                stylingMode={this.state.stylingMode}
-                width="100%"
-                placeholder="Type..."
-                inputAttr={addressLabel}
-                label="Address"
-                labelMode={this.state.labelMode}
-              >
-                <Validator>
-                  <RequiredRule />
-                </Validator>
-              </TextBox>
-              <DateBox
-                stylingMode={this.state.stylingMode}
-                width="100%"
-                placeholder="Select..."
-                label="Hire Date"
-                inputAttr={hireDateLabel}
-                labelMode={this.state.labelMode}
-              >
-                <Validator>
-                  <RequiredRule />
-                </Validator>
-              </DateBox>
-            </div>
-            <div className="right">
-              <DateBox
-                defaultValue={birthDate}
-                stylingMode={this.state.stylingMode}
-                width="100%"
-                placeholder="Birth Date"
-                label="Birth Date"
-                inputAttr={birthDateLabel}
-                labelMode={this.state.labelMode}
-              >
-                <Validator>
-                  <RequiredRule />
-                </Validator>
-              </DateBox>
-              <div className="flex-container">
-                <div className="state-select-box">
-                  <SelectBox
-                    items={states}
-                    stylingMode={this.state.stylingMode}
-                    inputAttr={stateLabel}
-                    width="100%"
-                    placeholder="Select..."
-                    label="State"
-                    labelMode={this.state.labelMode}
-                  >
-                    <Validator>
-                      <RequiredRule />
-                    </Validator>
-                  </SelectBox>
-                </div>
-                <TextBox
-                  stylingMode={this.state.stylingMode}
+  return (
+    <React.Fragment>
+      <div className="title">Edit Profile</div>
+      <div className="editors">
+        <div className="flex-container editors-padding-right">
+          <div className="left">
+            <TextBox
+              stylingMode={stylingMode}
+              defaultValue="Olivia Peyton"
+              width="100%"
+              inputAttr={nameLabel}
+              placeholder="Type..."
+              label="Name"
+              labelMode={labelMode}
+            >
+              <Validator>
+                <RequiredRule />
+              </Validator>
+            </TextBox>
+            <TextBox
+              stylingMode={stylingMode}
+              width="100%"
+              placeholder="Type..."
+              inputAttr={addressLabel}
+              label="Address"
+              labelMode={labelMode}
+            >
+              <Validator>
+                <RequiredRule />
+              </Validator>
+            </TextBox>
+            <DateBox
+              stylingMode={stylingMode}
+              width="100%"
+              placeholder="Select..."
+              label="Hire Date"
+              inputAttr={hireDateLabel}
+              labelMode={labelMode}
+            >
+              <Validator>
+                <RequiredRule />
+              </Validator>
+            </DateBox>
+          </div>
+          <div className="right">
+            <DateBox
+              defaultValue={birthDate}
+              stylingMode={stylingMode}
+              width="100%"
+              placeholder="Birth Date"
+              label="Birth Date"
+              inputAttr={birthDateLabel}
+              labelMode={labelMode}
+            >
+              <Validator>
+                <RequiredRule />
+              </Validator>
+            </DateBox>
+            <div className="flex-container">
+              <div className="state-select-box">
+                <SelectBox
+                  items={states}
+                  stylingMode={stylingMode}
+                  inputAttr={stateLabel}
                   width="100%"
-                  mask="+1 (000) 000-0000"
-                  inputAttr={phoneLabel}
-                  maskRules={phoneRules}
-                  label="Phone"
-                  labelMode={this.state.labelMode}
+                  placeholder="Select..."
+                  label="State"
+                  labelMode={labelMode}
                 >
                   <Validator>
                     <RequiredRule />
                   </Validator>
-                </TextBox>
+                </SelectBox>
               </div>
+              <TextBox
+                stylingMode={stylingMode}
+                width="100%"
+                mask="+1 (000) 000-0000"
+                inputAttr={phoneLabel}
+                maskRules={phoneRules}
+                label="Phone"
+                labelMode={labelMode}
+              >
+                <Validator>
+                  <RequiredRule />
+                </Validator>
+              </TextBox>
             </div>
           </div>
-          <div className="editors-padding-right center">
-            <TextArea
-              stylingMode={this.state.stylingMode}
-              defaultValue='Olivia loves to sell. She has been selling DevAV products since 2012.'
-              width="100%"
-              placeholder="Type..."
-              label="Notes"
-              labelMode={this.state.labelMode}
-              inputAttr={notesLabel}
-            />
-            <Button
-              onClick={validateClick}
-              text="Save"
-              icon="save"
-              type="default"
-              className="save-button"
-            />
-          </div>
         </div>
-        <div className="options">
-          <div className="caption">Options</div>
-          <div className="option">
-            <label>Styling Mode</label>
-            <SelectBox
-              stylingMode={this.state.stylingMode}
-              items={stylingModes}
-              inputAttr={stylingModeLabel}
-              value={this.state.stylingMode}
-              onValueChanged={this.stylingModeChange}
-            />
-          </div>
-          <div className="option">
-            <label>Label Mode</label>
-            <SelectBox
-              stylingMode={this.state.stylingMode}
-              items={labelModes}
-              inputAttr={labelModeLabel}
-              value={this.state.labelMode}
-              onValueChanged={this.labelModeChange}
-            />
-          </div>
+        <div className="editors-padding-right center">
+          <TextArea
+            stylingMode={stylingMode}
+            defaultValue='Olivia loves to sell. She has been selling DevAV products since 2012.'
+            width="100%"
+            placeholder="Type..."
+            label="Notes"
+            labelMode={labelMode}
+            inputAttr={notesLabel}
+          />
+          <Button
+            onClick={validateClick}
+            text="Save"
+            icon="save"
+            type="default"
+            className="save-button"
+          />
         </div>
-      </React.Fragment>
-    );
-  }
-
-  stylingModeChange({ value }) {
-    this.setState({
-      stylingMode: value,
-    });
-  }
-
-  labelModeChange({ value }) {
-    this.setState({
-      labelMode: value,
-    });
-  }
+      </div>
+      <div className="options">
+        <div className="caption">Options</div>
+        <div className="option">
+          <label>Styling Mode</label>
+          <SelectBox
+            stylingMode={stylingMode}
+            items={stylingModes}
+            inputAttr={stylingModeLabel}
+            value={stylingMode}
+            onValueChanged={changeStylingMode}
+          />
+        </div>
+        <div className="option">
+          <label>Label Mode</label>
+          <SelectBox
+            stylingMode={stylingMode}
+            items={labelModes}
+            inputAttr={labelModeLabel}
+            value={labelMode}
+            onValueChanged={labelModeChange}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
-
-export default App;
