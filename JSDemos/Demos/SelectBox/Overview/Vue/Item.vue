4c706d5fe1a55e6a0f68bb4d3359f7345740de9d
marker dao ® <mark_nikolsky@outlook.com>
2023-06-13 17:53:59 +0400

    feat(selectBox): Add img alt

--- JSDemos/Demos/SelectBox/Overview/Vue/Item.vue
+++ JSDemos/Demos/SelectBox/Overview/Vue/Item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="custom-item">
-    <img :src="itemData.ImageSrc">
+    <img alt='Product name' :src="itemData.ImageSrc">
     <div class="product-name">
       {{ itemData.Name }}
     </div>
