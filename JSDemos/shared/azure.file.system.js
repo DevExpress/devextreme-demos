e5fc2b979d5e7ced780b226d44f66fc48593e9e0
marker dao ® <mark_nikolsky@outlook.com>
2023-06-15 11:23:03 +0400

    fix(azure): Fix autofix

--- JSDemos/shared/azure.file.system.js
+++ JSDemos/shared/azure.file.system.js
@@ -216,7 +216,7 @@ export class AzureGateway {
         while (res.length < 10) {
             res = `0${res}`;
         }
-        return window.btoa(res);
+        return btoa(res);
     }
     getUploadAccessUrl(blobName) {
         return this.getAccessUrl('UploadBlob', blobName);
