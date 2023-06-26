20b5c96088b875e37416d496c28ae290f411b4fc
Marc Bambalan <marcbambi@gmail.com>
2023-06-26 11:02:02 +0800

    Add Vue

---
+++ JSDemos/Demos/TagBox/Overview/Vue/Tag.vue
@@ -0,0 +1,73 @@
+<template>
+  <div 
+    ref="target"
+    :class="{'dx-tag-content': true, 'disabled-tag': isDisabled}"
+    @mouseenter="show = true"
+    @mouseleave="show = false"
+  >
+    <img :src="tagData.ImageSrc" class="tag-img" />
+    <span>{{ tagData.Name }}</span>
+    <div v-if="!isDisabled" class="dx-tag-remove-button"></div>
+  </div>
+
+  <DxPopover :visible="show" :target="target">
+    <p>
+      <b>Name: </b><span>{{ tagData.Name }}</span>
+    </p>
+    <p>
+      <b>Price: </b><span>{{ tagData.Price }}</span>
+    </p>
+    <p>
+      <b>In-stock: </b><span>{{ tagData.Current_Inventory }}</span>
+    </p>
+    <p>
+      <b>Category: </b><span>{{ tagData.Category }}</span>
+    </p>
+  </DxPopover>
+</template>
+<script>
+import { ref } from 'vue';
+import DxPopover from 'devextreme-vue/popover';
+
+export default {
+  components: {
+    DxPopover
+  },
+  setup() {
+    const target = ref(null);
+
+    return {
+      target
+    }
+  }, 
+  data: function() {
+    return {
+      isDisabled: this.tagData.Name === 'SuperHD Video Player',
+      show: false,
+    }
+  },
+  props: {
+    tagData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+<style scoped>
+.dx-tag-content {
+  display: flex;
+  align-items: center;
+}
+
+.tag-img {
+  height: 30px;
+  margin-right: 5px;
+}
+
+.disabled-tag {
+  padding-right: 6px !important;
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+</style>
