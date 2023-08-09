7c74020849e87dbe6e871a8cf39964d78dac1caf
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2023-08-09 15:08:42 +0400

    Fix lint

--- JSDemos/Demos/FileManager/AzureClientBinding/React/App.js
+++ JSDemos/Demos/FileManager/AzureClientBinding/React/App.js
@@ -33,38 +33,38 @@ function App() {
   function getItems(parentDirectory) {
     return azure.getItems(parentDirectory.path);
   }
-  
+
   function createDirectory(parentDirectory, name) {
     return azure.createDirectory(parentDirectory.path, name);
   }
-  
+
   function renameItem(item, name) {
     return item.isDirectory
       ? azure.renameDirectory(item.path, name)
       : azure.renameFile(item.path, name);
   }
-  
+
   function deleteItem(item) {
     return item.isDirectory ? azure.deleteDirectory(item.path) : azure.deleteFile(item.path);
   }
-  
+
   function copyItem(item, destinationDirectory) {
     const destinationPath = destinationDirectory.path ? `${destinationDirectory.path}/${item.name}` : item.name;
     return item.isDirectory
       ? azure.copyDirectory(item.path, destinationPath)
       : azure.copyFile(item.path, destinationPath);
   }
-  
+
   function moveItem(item, destinationDirectory) {
     const destinationPath = destinationDirectory.path ? `${destinationDirectory.path}/${item.name}` : item.name;
     return item.isDirectory
       ? azure.moveDirectory(item.path, destinationPath)
       : azure.moveFile(item.path, destinationPath);
   }
-  
+
   function uploadFileChunk(fileData, uploadInfo, destinationDirectory) {
     let promise = null;
-  
+
     if (uploadInfo.chunkIndex === 0) {
       const filePath = destinationDirectory.path ? `${destinationDirectory.path}/${fileData.name}` : fileData.name;
       promise = gateway.getUploadAccessUrl(filePath).then((accessURLs) => {
@@ -73,23 +73,23 @@ function App() {
     } else {
       promise = Promise.resolve();
     }
-  
+
     promise = promise.then(() => gateway.putBlock(
       uploadInfo.customData.accessUrl,
       uploadInfo.chunkIndex,
       uploadInfo.chunkBlob,
     ));
-  
+
     if (uploadInfo.chunkIndex === uploadInfo.chunkCount - 1) {
       promise = promise.then(() => gateway.putBlockList(
         uploadInfo.customData.accessUrl,
         uploadInfo.chunkCount,
       ));
     }
-  
+
     return promise;
   }
-  
+
   function downloadItems(items) {
     azure.downloadFile(items[0].path);
   }
