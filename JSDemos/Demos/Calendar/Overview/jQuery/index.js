2362178fe57782fd43de9848318ab90eddc71154
Alexander Kozlovskiy <zedwag@gmail.com>
2023-08-22 09:38:52 +0400

    add jQuery demo

--- JSDemos/Demos/Calendar/Overview/jQuery/index.js
+++ JSDemos/Demos/Calendar/Overview/jQuery/index.js
@@ -1,6 +1,7 @@
 $(() => {
   const msInDay = 1000 * 60 * 60 * 24;
   const zoomLevels = ['month', 'year', 'decade', 'century'];
+  const selectionMode = ['single', 'multiple', 'range'];
   const weekDays = [
     { id: 0, text: 'Sunday' },
     { id: 1, text: 'Monday' },
@@ -12,17 +13,14 @@ $(() => {
   ];
   const weekNumberRules = ['auto', 'firstDay', 'firstFourDays', 'fullWeek'];
   const date = new Date().getTime();
+  let zoomLevel;
 
   const calendar = $('#calendar').dxCalendar({
-    value: new Date(),
     disabled: false,
     firstDayOfWeek: 0,
     showWeekNumbers: false,
     weekNumberRule: 'auto',
     zoomLevel: zoomLevels[0],
-    onValueChanged(data) {
-      selectedDate.option('value', data.value);
-    },
     onOptionChanged(data) {
       if (data.name === 'zoomLevel') {
         zoomLevel.option('value', data.value);
@@ -30,94 +28,27 @@ $(() => {
     },
   }).dxCalendar('instance');
 
-  $('#min-date').dxCheckBox({
-    text: 'Set minimum date',
-    onValueChanged(data) {
-      const minDate = new Date(date - msInDay * 3);
-
-      calendar.option('min', data.value ? minDate : null);
-      selectedDate.option('min', data.value ? minDate : null);
-    },
-  });
-
-  $('#max-date').dxCheckBox({
-    text: 'Set maximum date',
-    onValueChanged(data) {
-      const maxDate = new Date(date + msInDay * 3);
-
-      calendar.option('max', data.value ? maxDate : null);
-      selectedDate.option('max', data.value ? maxDate : null);
-    },
-  });
-
-  $('#disable-dates').dxCheckBox({
-    text: 'Disable weekends',
-    onValueChanged(data) {
-      if (data.value) {
-        calendar.option('disabledDates', (d) => d.view === 'month' && isWeekend(d.date));
-      } else {
-        calendar.option('disabledDates', null);
-      }
-    },
-  });
-
-  $('#week-numbers').dxCheckBox({
-    text: 'Show week numbers',
-    onValueChanged(data) {
-      calendar.option('showWeekNumbers', data.value);
-    },
-  });
-
-  $('#disabled').dxCheckBox({
-    text: 'Disable the calendar',
-    onValueChanged(data) {
-      calendar.option('disabled', data.value);
-    },
-  });
-
-  $('#custom-cell').dxCheckBox({
-    text: 'Use custom cell template',
-    value: false,
-    onValueChanged(data) {
-      calendar.option('cellTemplate', data.value ? getCellTemplate : 'cell');
-    },
-  });
-
-  const zoomLevel = $('#zoom-level').dxSelectBox({
-    dataSource: zoomLevels,
-    value: zoomLevels[0],
-    inputAttr: { 'aria-label': 'Zoom Level' },
-    onValueChanged(data) {
-      calendar.option('zoomLevel', data.value);
-    },
-  }).dxSelectBox('instance');
-
-  const selectedDate = $('#selected-date').dxDateBox({
-    value: new Date(),
-    inputAttr: { 'aria-label': 'Date' },
-    onValueChanged(data) {
-      calendar.option('value', data.value);
-    },
-  }).dxDateBox('instance');
-
-  $('#first-day-of-week').dxSelectBox({
-    dataSource: weekDays,
-    value: 0,
-    valueExpr: 'id',
-    inputAttr: { 'aria-label': 'First Day of Week' },
-    displayExpr: 'text',
-    onValueChanged(data) {
-      calendar.option('firstDayOfWeek', data.value);
-    },
-  });
-
-  $('#week-number-rule').dxSelectBox({
-    dataSource: weekNumberRules,
-    value: weekNumberRules[0],
-    inputAttr: { 'aria-label': 'Week Number Rule' },
-    onValueChanged(data) {
-      calendar.option('weekNumberRule', data.value);
-    },
+  $('#accordion').dxAccordion({
+    collapsible: true,
+    activeStateEnabled: false,
+    hoverStateEnabled: false,
+    focusStateEnabled: false,
+    multiple: false,
+    selectedIndex: 0,
+    items: [
+      {
+        title: 'General options',
+        template: getGeneralOptionsTemplate,
+      },
+      {
+        title: 'Week numeration',
+        template: getWeekNumerationTemplate,
+      },
+      {
+        title: 'Dates availability',
+        template: getDatesAvailabilityTemplate,
+      },
+    ],
   });
 
   const holidays = [[1, 0], [4, 6], [25, 11]];
@@ -148,5 +79,133 @@ $(() => {
     }
 
     return `<span class='${cssClass}'>${data.text}</span>`;
+  }
+
+  function getGeneralOptionsTemplate() {
+    const $zoomLevel = $('<div>').dxSelectBox({
+      dataSource: zoomLevels,
+      value: zoomLevels[0],
+      inputAttr: { 'aria-label': 'Zoom Level' },
+      onValueChanged(data) {
+        calendar.option('zoomLevel', data.value);
+      },
+    });
+    zoomLevel = $zoomLevel.dxSelectBox('instance');
+
+    const $selectionMode = $('<div>').dxSelectBox({
+      dataSource: selectionMode,
+      value: selectionMode[0],
+      inputAttr: { 'aria-label': 'Selection Mode' },
+      onValueChanged(data) {
+        calendar.option('selectionMode', data.value);
+      },
+    });
+
+    const $cellTemplate = $('<div>').dxCheckBox({
+      text: 'Use custom cell template',
+      onValueChanged(data) {
+        calendar.option('cellTemplate', data.value ? getCellTemplate : 'cell');
+      },
+    });
+
+    const $disableCalendar = $('<div>').dxCheckBox({
+      text: 'Disable the calendar',
+      onValueChanged(data) {
+        calendar.option('disabled', data.value);
+      },
+    });
+
+    return $('<div>').append(
+      $('<div class="option">').append(
+        $('<span>Zoom level</span>'),
+        $zoomLevel,
+      ),
+      $('<div class="option">').append(
+        $('<span>Selection mode</span>'),
+        $selectionMode,
+      ),
+      $('<div class="option">').append($cellTemplate),
+      $('<div class="option">').append($disableCalendar),
+    );
+  }
+
+  function getWeekNumerationTemplate() {
+    const $showWeekNumbers = $('<div>').dxCheckBox({
+      text: 'Show week numbers',
+      onValueChanged(data) {
+        calendar.option('showWeekNumbers', data.value);
+      },
+    });
+
+    const $firstDayOfWeek = $('<div>').dxSelectBox({
+      dataSource: weekDays,
+      value: 0,
+      valueExpr: 'id',
+      inputAttr: { 'aria-label': 'First Day of Week' },
+      displayExpr: 'text',
+      onValueChanged(data) {
+        calendar.option('firstDayOfWeek', data.value);
+      },
+    });
+
+    const $weekNumberRule = $('<div>').dxSelectBox({
+      dataSource: weekNumberRules,
+      value: weekNumberRules[0],
+      inputAttr: { 'aria-label': 'Week Number Rule' },
+      onValueChanged(data) {
+        calendar.option('weekNumberRule', data.value);
+      },
+    });
+
+    return $('<div>').append(
+      $('<div class="option">').append(
+        $showWeekNumbers,
+      ),
+      $('<div class="option">').append(
+        $('<span>First day of week</span>'),
+        $firstDayOfWeek,
+      ),
+      $('<div class="option">').append(
+        $('<span>Week number rule</span>'),
+        $weekNumberRule,
+      ),
+    );
+  }
+
+  function getDatesAvailabilityTemplate() {
+    const $minDate = $('<div>').dxCheckBox({
+      text: 'Set minimum date',
+      onValueChanged(data) {
+        const minDate = new Date(date - msInDay * 3);
+
+        calendar.option('min', data.value ? minDate : null);
+      },
+    });
+
+    const $maxDate = $('<div>').dxCheckBox({
+      text: 'Set maximum date',
+      onValueChanged(data) {
+        const maxDate = new Date(date + msInDay * 3);
+
+        calendar.option('max', data.value ? maxDate : null);
+      },
+    });
+
+    const $disableWeekends = $('<div>').dxCheckBox({
+      text: 'Disable weekends',
+      onValueChanged(data) {
+        if (data.value) {
+          calendar.option('disabledDates', (d) => d.view === 'month' && isWeekend(d.date));
+        } else {
+          calendar.option('disabledDates', null);
+        }
+      },
+    });
+
+    return $('<div>').append(
+      $('<div class="option">').append($minDate),
+      $('<div class="option">').append($maxDate),
+      $('<div class="option">').append($disableWeekends),
+    );
   }
 });
