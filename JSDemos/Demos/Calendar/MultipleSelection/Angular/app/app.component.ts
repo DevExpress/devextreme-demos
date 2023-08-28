872f3ac3281dbf464f4923abee4bf9c1bbce949a
Alexander Kozlovskiy <zedwag@gmail.com>
2023-08-28 18:54:30 +0200

    add multiselect angular demo

---
+++ JSDemos/Demos/Calendar/MultipleSelection/Angular/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component, NgModule, enableProdMode } from '@angular/core';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  DxSelectBoxModule,
+  DxCheckBoxModule,
+  DxCalendarModule,
+} from 'devextreme-angular';
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+})
+
+export class AppComponent {
+  now: Date = new Date();
+
+  initialValues: Date[] = [new Date(), new Date(new Date().getTime() + 1000 * 60 * 60 * 24)];
+
+  selectionModes: string[] = [
+    'single', 'multiple', 'range',
+  ];
+
+  selectionMode = 'multiple';
+
+  minDateValue: Date | null = null;
+
+  maxDateValue: Date | null = null;
+
+  disabledDates: Function | null = null;
+
+  isWeekend(date) {
+    const day = date.getDay();
+
+    return day === 0 || day === 6;
+  }
+
+  setMinDate({ value }) {
+    this.minDateValue = value
+      ? new Date(this.now.getTime() - 1000 * 60 * 60 * 24 * 3)
+      : null;
+  }
+
+  setMaxDate({ value }) {
+    this.maxDateValue = value
+      ? new Date(this.now.getTime() + 1000 * 60 * 60 * 24 * 3)
+      : null;
+  }
+
+  disableWeekend({ value }) {
+    this.disabledDates = value
+      ? (data) => data.view === 'month' && this.isWeekend(data.date)
+      : null;
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserTransferStateModule,
+    DxSelectBoxModule,
+    DxCalendarModule,
+    DxCheckBoxModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
