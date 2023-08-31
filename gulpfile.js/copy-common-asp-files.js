3a9329d0eb295fbe2387af0cee9ef123b2ef9e50
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2023-08-31 18:46:37 +0400

    Using local devextreme dist for running aspnet demos (#2809)

--- gulpfile.js/copy-common-asp-files.js
+++ gulpfile.js/copy-common-asp-files.js
@@ -1,40 +1,64 @@
 /* eslint-disable import/no-extraneous-dependencies */
-
-const { src, dest, parallel } = require('gulp');
+/* eslint-disable no-console */
+const {
+  src, dest, parallel, series,
+} = require('gulp');
+const { existsSync } = require('fs');
 const minify = require('gulp-minify');
 
-exports.copyCommonAspFiles = parallel(
-  () => src([
-    'node_modules/devextreme/dist/css/**/*',
-    'node_modules/devexpress-diagram/dist/**/*.css',
-    'node_modules/devexpress-gantt/dist/**/*.css',
-  ])
-    .pipe(dest('MVCDemos/Content/DevExtreme'))
-    .pipe(dest('NetCoreDemos/wwwroot/css')),
+const { init } = require('../utils/shared/config-helper');
+const { systemSync } = require('../utils/shared/child-process-utils');
 
-  () => src('node_modules/devextreme/dist/css/icons/*')
-    .pipe(dest('MVCDemos/Content/icons')),
+const devextremePath = init().devextreme;
 
-  () => src([
-    'node_modules/devextreme-dist/js/**/*',
-    'node_modules/globalize/dist/**/+(currency|date|message|number|globalize).js',
-    'node_modules/cldrjs/dist/**/!(node_main.js)',
-    'node_modules/devextreme-aspnet-data/js/**/**',
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/jszip/dist/jszip.js',
-    'node_modules/devexpress-diagram/dist/**/*.js',
-    'node_modules/devexpress-gantt/dist/**/*.js',
-  ])
-    .pipe(minify({
-      ext: {
-        min: '.min.js',
-      },
-      ignoreFiles: ['dx*', '.min.js', '.map'],
-    }))
-    .pipe(dest('MVCDemos/Scripts'))
-    .pipe(dest('NetCoreDemos/wwwroot/js')),
+let devextremeDistPath = 'node_modules/devextreme-dist';
 
-  () => src('JSDemos/menuMeta.json')
-    .pipe(dest('MVCDemos/App_Data'))
-    .pipe(dest('NetCoreDemos')),
+const prepareDevextreme = (callback) => {
+  if (devextremePath.length !== 0 && existsSync(devextremePath)) {
+    systemSync(`cd ${devextremePath}/packages/devextreme && npm run build-npm-devextreme`);
+    devextremeDistPath = `${devextremePath}/packages/devextreme/artifacts/npm/devextreme-dist`;
+  } else {
+    console.warn('Path to DevExtreme monorepo not specified in "repository.config.json", so uses DevExtreme dist from node_modules');
+  }
+
+  callback();
+};
+
+exports.copyCommonAspFiles = series(
+  prepareDevextreme,
+  parallel(
+    () => src([
+      `${devextremeDistPath}/css/**/*`,
+      'node_modules/devexpress-diagram/dist/**/*.css',
+      'node_modules/devexpress-gantt/dist/**/*.css',
+    ])
+      .pipe(dest('MVCDemos/Content/DevExtreme'))
+      .pipe(dest('NetCoreDemos/wwwroot/css')),
+
+    () => src(`${devextremeDistPath}/css/icons/*`)
+      .pipe(dest('MVCDemos/Content/icons')),
+
+    () => src([
+      `${devextremeDistPath}/js/**/*`,
+      'node_modules/globalize/dist/**/+(currency|date|message|number|globalize).js',
+      'node_modules/cldrjs/dist/**/!(node_main.js)',
+      'node_modules/devextreme-aspnet-data/js/**/**',
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/jszip/dist/jszip.js',
+      'node_modules/devexpress-diagram/dist/**/*.js',
+      'node_modules/devexpress-gantt/dist/**/*.js',
+    ])
+      .pipe(minify({
+        ext: {
+          min: '.min.js',
+        },
+        ignoreFiles: ['dx*', '.min.js', '.map'],
+      }))
+      .pipe(dest('MVCDemos/Scripts'))
+      .pipe(dest('NetCoreDemos/wwwroot/js')),
+
+    () => src('JSDemos/menuMeta.json')
+      .pipe(dest('MVCDemos/App_Data'))
+      .pipe(dest('NetCoreDemos')),
+  ),
 );
