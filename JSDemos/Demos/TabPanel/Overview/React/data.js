6cf40e86c6c93b9a38ab77b5893ecb46fa40a5fb
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-09-08 16:26:25 +0400

    TabPanel: Add demo for new features

--- JSDemos/Demos/TabPanel/Overview/React/data.js
+++ JSDemos/Demos/TabPanel/Overview/React/data.js
@@ -1,41 +1,296 @@
-export const multiViewItems = [{
-  ID: 1,
-  CompanyName: 'SuprMart',
-  Address: '702 SW 8th Street',
-  City: 'Bentonville',
-  State: 'Arkansas',
-  Zipcode: 72716,
-  Phone: '(800) 555-2797',
-  Fax: '(800) 555-2171',
-  Website: 'http://www.nowebsitesupermart.com',
-}, {
-  ID: 2,
-  CompanyName: "El'Depot",
-  Address: '2455 Paces Ferry Road NW',
-  City: 'Atlanta',
-  State: 'Georgia',
-  Zipcode: 30339,
-  Phone: '(800) 595-3232',
-  Fax: '(800) 595-3231',
-  Website: 'http://www.nowebsitedepot.com',
-}, {
-  ID: 3,
-  CompanyName: 'K&S Music',
-  Address: '1000 Nicllet Mall',
-  City: 'Minneapolis',
-  State: 'Minnesota',
-  Zipcode: 55403,
-  Phone: '(612) 304-6073',
-  Fax: '(612) 304-6074',
-  Website: 'http://www.nowebsitemusic.com',
-}, {
-  ID: 4,
-  CompanyName: 'Tom Club',
-  Address: '999 Lake Drive',
-  City: 'Issaquah',
-  State: 'Washington',
-  Zipcode: 98027,
-  Phone: '(800) 955-2292',
-  Fax: '(800) 955-2293',
-  Website: 'http://www.nowebsitetomsclub.com',
-}];
+export const tabsPositionsSelectBoxLabel = { 'aria-label': 'Tabs positions' };
+
+export const tabsPositions = [
+  'left',
+  'top',
+  'right',
+  'bottom',
+];
+
+export const stylingModesSelectBoxLabel = { 'aria-label': 'Tabs positions' };
+
+export const stylingModes = [
+  'secondary',
+  'primary',
+];
+
+export const iconPositionsSelectBoxLabel = { 'aria-label': 'Icon positions' };
+
+export const iconPositions = [
+  'top',
+  'start',
+  'end',
+  'bottom',
+];
+
+const tasks = [
+  {
+    status: 'Not Started',
+    priority: 'hight',
+    text: 'Revenue Projections',
+    date: '2023-09-16',
+    assignedBy: 'John Heart',
+  },
+  {
+    status: 'Not Started',
+    priority: 'hight',
+    text: 'New Brochures',
+    date: '2023-09-16',
+    assignedBy: 'Samantha Bright',
+  },
+  {
+    status: 'Not Started',
+    priority: 'normal',
+    text: 'Training',
+    date: '2023-09-16',
+    assignedBy: 'Arthur Miller',
+  },
+  {
+    status: 'Not Started',
+    priority: 'normal',
+    text: 'NDA',
+    date: '2023-09-16',
+    assignedBy: 'Robert Reagan',
+  },
+  {
+    status: 'Not Started',
+    priority: 'low',
+    text: 'Health Insurance',
+    date: '2023-09-16',
+    assignedBy: 'Greta Sims',
+  },
+
+  {
+    status: 'Help Needed',
+    priority: 'low',
+    text: 'TV Recall',
+    date: '2023-09-16',
+    assignedBy: 'Brett Wade',
+  },
+  {
+    status: 'Help Needed',
+    priority: 'low',
+    text: 'Recall and Refund Forms',
+    date: '2023-09-16',
+    assignedBy: 'Sandra Johnson',
+  },
+  {
+    status: 'Help Needed',
+    priority: 'hight',
+    text: 'Shippers',
+    date: '2023-09-16',
+    assignedBy: 'Ed Holmes',
+  },
+  {
+    status: 'Help Needed',
+    priority: 'normal',
+    text: 'Hardware Upgrade',
+    date: '2023-09-16',
+    assignedBy: 'Barb Banks',
+  },
+  {
+    status: 'Help Needed',
+    priority: 'normal',
+    text: 'Touch-Enabled Apps',
+    date: '2023-09-16',
+    assignedBy: 'Kevin Carter',
+  },
+  {
+    status: 'Help Needed',
+    priority: 'low',
+    text: 'Review HR Budget Company Wide',
+    date: '2023-09-16',
+    assignedBy: 'Ed Holmes',
+  },
+
+  {
+    status: 'In Progress',
+    priority: 'normal',
+    text: 'Online Sales',
+    date: '2023-09-16',
+    assignedBy: 'Cindy Stanwick',
+  },
+  {
+    status: 'In Progress',
+    priority: 'normal',
+    text: 'New Website Design',
+    date: '2023-09-16',
+    assignedBy: 'Sammy Hill',
+  },
+  {
+    status: 'In Progress',
+    priority: 'low',
+    text: 'Bandwidth Increase',
+    date: '2023-09-16',
+    assignedBy: 'Davey Jones',
+  },
+  {
+    status: 'In Progress',
+    priority: 'normal',
+    text: 'Support',
+    date: '2023-09-16',
+    assignedBy: 'Victor Norris',
+  },
+  {
+    status: 'In Progress',
+    priority: 'low',
+    text: 'Training Material',
+    date: '2023-09-16',
+    assignedBy: 'John Heart',
+  },
+
+  {
+    status: 'Deferred',
+    priority: 'normal',
+    text: 'New Database',
+    date: '2023-09-16',
+    assignedBy: 'Samantha Bright',
+  },
+  {
+    status: 'Deferred',
+    priority: 'hight',
+    text: 'Automation Server',
+    date: '2023-09-16',
+    assignedBy: 'Arthur Miller',
+  },
+  {
+    status: 'Deferred',
+    priority: 'normal',
+    text: 'Retail Sales',
+    date: '2023-09-16',
+    assignedBy: 'Robert Reagan',
+  },
+  {
+    status: 'Deferred',
+    priority: 'normal',
+    text: 'Shipping Labels',
+    date: '2023-09-16',
+    assignedBy: 'Greta Sims',
+  },
+  {
+    status: 'Deferred',
+    priority: 'low',
+    text: 'Customer Follow Up Plan',
+    date: '2023-09-16',
+    assignedBy: 'Brett Wade',
+  },
+  {
+    status: 'Deferred',
+    priority: 'normal',
+    text: 'Support Dept Budget Report',
+    date: '2023-09-16',
+    assignedBy: 'John Heart',
+  },
+
+  {
+    status: 'Rejected',
+    priority: 'hight',
+    text: 'Schedule Meeting with Sales Team',
+    date: '2023-09-16',
+    assignedBy: 'Sandra Johnson',
+  },
+  {
+    status: 'Rejected',
+    priority: 'normal',
+    text: 'Confirm Availability for Sales Meeting',
+    date: '2023-09-16',
+    assignedBy: 'Ed Holmes',
+  },
+  {
+    status: 'Rejected',
+    priority: 'normal',
+    text: 'Reschedule Sales Team Meeting',
+    date: '2023-09-16',
+    assignedBy: 'Barb Banks',
+  },
+  {
+    status: 'Rejected',
+    priority: 'hight',
+    text: 'Update Database with New Leads',
+    date: '2023-09-16',
+    assignedBy: 'Kevin Carter',
+  },
+  {
+    status: 'Rejected',
+    priority: 'low',
+    text: 'Send Territory Sales Breakdown',
+    date: '2023-09-16',
+    assignedBy: 'Cindy Stanwick',
+  },
+
+  {
+    status: 'Completed',
+    priority: 'normal',
+    text: 'Territory Sales Breakdown Report',
+    date: '2023-09-16',
+    assignedBy: 'Sammy Hill',
+  },
+  {
+    status: 'Completed',
+    priority: 'low',
+    text: 'Return Merchandise Report',
+    date: '2023-09-16',
+    assignedBy: 'Davey Jones',
+  },
+  {
+    status: 'Completed',
+    priority: 'hight',
+    text: 'Staff Productivity Report',
+    date: '2023-09-16',
+    assignedBy: 'Victor Norris',
+  },
+  {
+    status: 'Completed',
+    priority: 'normal',
+    text: 'Review HR Budget Company Wide',
+    date: '2023-09-16',
+    assignedBy: 'Mary Stern',
+  },
+  {
+    status: 'Completed',
+    priority: 'normal',
+    text: 'Support',
+    date: '2023-09-16',
+    assignedBy: 'Victor Norris',
+  },
+  {
+    status: 'Completed',
+    priority: 'low',
+    text: 'Support Dept Budget Report',
+    date: '2023-09-16',
+    assignedBy: 'Robin Cosworth',
+  },
+];
+
+export const dataSource = [
+  {
+    icon: 'description',
+    title: 'Not Started',
+    tasks: tasks.filter((item) => item.status === 'Not Started'),
+  },
+  {
+    icon: 'task-helpneeded',
+    title: 'Help Needed',
+    tasks: tasks.filter((item) => item.status === 'Help Needed'),
+  },
+  {
+    icon: 'task-inprogress',
+    title: 'In Progress',
+    tasks: tasks.filter((item) => item.status === 'In Progress'),
+  },
+  {
+    icon: 'task-stop',
+    title: 'Deferred',
+    tasks: tasks.filter((item) => item.status === 'Deferred'),
+  },
+  {
+    icon: 'task-rejected',
+    title: 'Rejected',
+    tasks: tasks.filter((item) => item.status === 'Rejected'),
+  },
+  {
+    icon: 'task-complete',
+    title: 'Completed',
+    tasks: tasks.filter((item) => item.status === 'Completed'),
+  },
+];
