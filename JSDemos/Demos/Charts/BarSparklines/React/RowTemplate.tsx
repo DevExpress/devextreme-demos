ff85b444fe0d5dbead79933bdffd647a5138bf0e
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-09-28 15:03:31 +0400

    Fix lint errors

--- JSDemos/Demos/Charts/BarSparklines/React/RowTemplate.tsx
+++ JSDemos/Demos/Charts/BarSparklines/React/RowTemplate.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Sparkline, {
   Tooltip,
 } from 'devextreme-react/sparkline';
+import DataSource from 'devextreme/data/data_source';
 
 interface RowTemplateProps {
   key: number;
