58659e3578e9486e6805d00ae956c187a9e91a14
Alexander Kozlovskiy <zedwag@gmail.com>
2023-09-29 11:52:37 -0500

    change test demo date for stable screenshots

--- JSDemos/Demos/Calendar/MultipleSelection/test-code.js
+++ JSDemos/Demos/Calendar/MultipleSelection/test-code.js
@@ -2,6 +2,6 @@ testUtils.importAnd(() => 'devextreme/ui/calendar', () => DevExpress.ui.dxCalend
   .postponeUntilFound('.dx-calendar', 100, 10000)
   .then(() => {
     const instance = dxCalendar.getInstance(document.querySelector('.dx-calendar'));
-    instance.option('value', [new Date('2023/09/19'), new Date('2023/09/20')]);
+    instance.option('value', [new Date('2023/08/19'), new Date('2023/08/20')]);
   })
   .then(() => testUtils.postpone(2000)));
