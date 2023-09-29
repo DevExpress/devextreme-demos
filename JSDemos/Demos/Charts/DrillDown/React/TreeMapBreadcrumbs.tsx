eebf1ae14d284d1135c271a82df972b6f9d2c9d2
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-09-29 11:47:06 +0400

    Temporarily add eslint-ingnore for 1 line (false positive no-unused-vars rule)

--- JSDemos/Demos/Charts/DrillDown/React/TreeMapBreadcrumbs.tsx
+++ JSDemos/Demos/Charts/DrillDown/React/TreeMapBreadcrumbs.tsx
@@ -4,6 +4,7 @@ import Breadcrumb from './Breadcrumb.tsx';
 
 interface TreeMapBreadcrumbsProps {
   className: string;
+  // eslint-disable-next-line no-unused-vars
   onItemClick: (node: any) => void;
   treeInfo: any;
 }
