042c40d9c39a97040ceb635c1c990f5be7f24b16
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-10-09 11:13:35 +0400

    Generate JS

--- JSDemos/Demos/TreeList/MultipleRowSelection/ReactJs/App.js
+++ JSDemos/Demos/TreeList/MultipleRowSelection/ReactJs/App.js
@@ -1,46 +1,41 @@
 import React from 'react';
-import {
-  TreeList, Selection, Column, TreeListTypes,
-} from 'devextreme-react/tree-list';
-import { CheckBox, CheckBoxTypes } from 'devextreme-react/check-box';
-import { SelectBox, SelectBoxTypes } from 'devextreme-react/select-box';
-import { employees, selectionModeLabel } from './data.ts';
+import { TreeList, Selection, Column } from 'devextreme-react/tree-list';
+import { CheckBox } from 'devextreme-react/check-box';
+import { SelectBox } from 'devextreme-react/select-box';
+import { employees, selectionModeLabel } from './data.js';
 
 const expandedRowKeys = [1, 2, 10];
 const emptySelectedText = 'Nobody has been selected';
 const selectionModes = ['all', 'excludeRecursive', 'leavesOnly'];
-
 const App = () => {
   const [selectedRowKeys, setSelectedRowKeys] = React.useState([]);
   const [recursive, setRecursive] = React.useState(false);
   const [selectionMode, setSelectionMode] = React.useState('all');
   const [selectedEmployeeNames, setSelectedEmployeeNames] = React.useState(emptySelectedText);
-
   const getEmployeeNames = React.useCallback((employeeList) => {
     if (employeeList.length > 0) {
       return employeeList.map((employee) => employee.Full_Name).join(', ');
     }
     return emptySelectedText;
   }, []);
-
-  const onSelectionChanged = React.useCallback((e: TreeListTypes.SelectionChangedEvent) => {
-    const selectedData = e.component.getSelectedRowsData(selectionMode);
-    setSelectedRowKeys(e.selectedRowKeys);
-    setSelectedEmployeeNames(getEmployeeNames(selectedData));
-  }, [selectionMode, getEmployeeNames]);
-
-  const onRecursiveChanged = React.useCallback((e: CheckBoxTypes.ValueChangedEvent) => {
+  const onSelectionChanged = React.useCallback(
+    (e) => {
+      const selectedData = e.component.getSelectedRowsData(selectionMode);
+      setSelectedRowKeys(e.selectedRowKeys);
+      setSelectedEmployeeNames(getEmployeeNames(selectedData));
+    },
+    [selectionMode, getEmployeeNames],
+  );
+  const onRecursiveChanged = React.useCallback((e) => {
     setRecursive(e.value);
     setSelectedRowKeys([]);
     setSelectedEmployeeNames(emptySelectedText);
   }, []);
-
-  const onSelectionModeChanged = React.useCallback((e: SelectBoxTypes.ValueChangedEvent) => {
+  const onSelectionModeChanged = React.useCallback((e) => {
     setSelectionMode(e.value);
     setSelectedRowKeys([]);
     setSelectedEmployeeNames(emptySelectedText);
   }, []);
-
   return (
     <div>
       <TreeList
@@ -55,12 +50,22 @@ const App = () => {
         parentIdExpr="Head_ID"
         onSelectionChanged={onSelectionChanged}
       >
-        <Selection recursive={recursive} mode="multiple" />
+        <Selection
+          recursive={recursive}
+          mode="multiple"
+        />
         <Column dataField="Full_Name" />
-        <Column dataField="Title" caption="Position" />
+        <Column
+          dataField="Title"
+          caption="Position"
+        />
         <Column dataField="City" />
         <Column dataField="State" />
-        <Column width={120} dataField="Hire_Date" dataType="date" />
+        <Column
+          width={120}
+          dataField="Hire_Date"
+          dataType="date"
+        />
       </TreeList>
       <div className="options">
         <div className="caption">Options</div>
@@ -82,14 +87,10 @@ const App = () => {
           />
         </div>
         <div className="selected-data">
-          <span className="caption">Selected Records:</span>{' '}
-          <span>
-            { selectedEmployeeNames }
-          </span>
+          <span className="caption">Selected Records:</span> <span>{selectedEmployeeNames}</span>
         </div>
       </div>
     </div>
   );
 };
-
 export default App;
