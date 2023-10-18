e8e342ef677413d6de1e5425316f7d356feae9d8
Andrew Makarov <andrewmakarov@inbox.ru>
2023-10-18 14:12:13 +0400

    Fix linter errors

--- JSDemos/Demos/Scheduler/Editing/React/data.ts
+++ JSDemos/Demos/Scheduler/Editing/React/data.ts
@@ -1,6 +1,6 @@
 import { SchedulerTypes } from 'devextreme-react/scheduler';
 
-export const data: SchedulerTypes.Appointment = [
+export const data: SchedulerTypes.Appointment[] = [
   {
     text: 'Website Re-Design Plan',
     startDate: new Date('2021-04-26T16:30:00.000Z'),
