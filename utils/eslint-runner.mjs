7c3ba6ce94d319fce619144d5cfc0bef8d3fd7fc
Lykoi18 <aiden.ghost666@gmail.com>
2023-10-20 15:45:58 +0400

    Rework starting eslint

---
+++ utils/eslint-runner.mjs
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import { ESLint } from 'eslint';
+
+const getPatterns = () => {
+  const CONSTEL = process.env.CONSTEL;
+
+  if (CONSTEL == null) {
+    return ['JSDemos/Demos/**/*.[tj]s?(x)'];
+  }
+
+  const [current, total] = CONSTEL.split('/');
+
+  const demos = fs.readdirSync(path.resolve(process.cwd(), 'JSDemos/Demos'));
+  const filteredDemos = demos.filter((_, index) => index % total === current - 1);
+
+  return filteredDemos.map((demoName) => `JSDemos/Demos/${demoName}/**/*.[tj]s?(x)`);
+}
+
+(async () => {
+  const eslint = new ESLint({
+    cwd: process.cwd()
+  });
+
+  const patterns = getPatterns();
+  const results = await eslint.lintFiles(patterns);
+
+  const formatter = await eslint.loadFormatter("stylish");
+  const resultText = formatter.format(results);
+
+  console.log(resultText);
+})().catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
