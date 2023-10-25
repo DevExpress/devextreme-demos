25a73d12e05080eb2d92b75eca9b0bf3420a8a3d
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2023-10-25 14:06:37 +0400

    Decrease lint task duration in frameworks tests (#2926) (#2940)

---
+++ utils/eslint-runner.ts
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import fs from 'fs';
+import path from 'path';
+import { ESLint } from 'eslint';
+
+const getPatterns = () => {
+  const CONSTEL = process.env.CONSTEL;
+
+  if (CONSTEL == null) {
+    return ['JSDemos/Demos/**/*.[tj]s?(x)'];
+  }
+
+  const [current, total] = CONSTEL.split('/').map(Number);
+
+  const demos = fs.readdirSync(path.resolve(process.cwd(), 'JSDemos/Demos'));
+  const filteredDemos = demos.filter((_, index) => index % total === current - 1);
+
+  return filteredDemos.map((demoName) => `JSDemos/Demos/${demoName}/**/*.[tj]s?(x)`);
+};
+
+(async () => {
+  const eslint = new ESLint({
+    cwd: process.cwd(),
+  });
+
+  const patterns = getPatterns();
+  const results = await eslint.lintFiles(patterns);
+
+  const formatter = await eslint.loadFormatter('stylish');
+  const resultText = formatter.format(results);
+
+  console.log(resultText);
+})().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
