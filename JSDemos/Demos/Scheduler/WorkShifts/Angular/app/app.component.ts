2fdf546d9448441a249a7b6067921a8d0003dcad
Andrew Makarov <andrewmakarov@inbox.ru>
2023-11-10 01:56:31 +0400

    Add angular demo template

--- JSDemos/Demos/Scheduler/WorkShifts/Angular/app/app.component.ts
+++ JSDemos/Demos/Scheduler/WorkShifts/Angular/app/app.component.ts
@@ -1,8 +1,8 @@
 import { NgModule, Component, enableProdMode } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { DxDataGridModule } from 'devextreme-angular';
-import { Customer, Service } from './app.service';
+import { DxSchedulerModule, DxRadioGroupModule } from 'devextreme-angular';
+import { Service, Appointment, Shift } from './app.service';
 
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
@@ -11,21 +11,34 @@ if (!/localhost/.test(document.location.host)) {
 @Component({
   selector: 'demo-app',
   templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
   providers: [Service],
 })
 export class AppComponent {
-  customers: Customer[];
+  appointments: Appointment[];
+
+  shifts: Shift[];
+
+  currentDate: Date = new Date(2021, 2, 30);
+
+  offset = 0;
 
   constructor(service: Service) {
-    this.customers = service.getCustomers();
+    this.appointments = service.getAppointments();
+    this.shifts = service.getShifts();
   }
+
+  onShiftChanged = (e) => {
+    // TODO this.offset = e.value;
+  };
 }
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserTransferStateModule,
-    DxDataGridModule,
+    DxSchedulerModule,
+    DxRadioGroupModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
