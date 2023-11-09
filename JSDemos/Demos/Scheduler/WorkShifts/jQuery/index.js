18be41e3a2be51b6ce904e2b3a40d6ca51678cd1
Andrew Makarov <andrewmakarov@inbox.ru>
2023-11-09 22:53:58 +0400

    Add jQuery demo tempalte

--- JSDemos/Demos/Scheduler/WorkShifts/jQuery/index.js
+++ JSDemos/Demos/Scheduler/WorkShifts/jQuery/index.js
@@ -1,9 +1,23 @@
 $(() => {
-  $('#button').dxButton({
-    text: 'Click me',
-    onClick() {
-      // eslint-disable-next-line no-alert
-      alert('Test!');
+  const scheduler = $('#scheduler').dxScheduler({
+    timeZone: 'America/Los_Angeles',
+    dataSource: data,
+    views: ['day', 'week'],
+    currentView: 'week',
+    currentDate: new Date(2021, 2, 30),
+    startDayHour: 0,
+    endDayHour: 8,
+    // offset: 0,
+    height: 600,
+    showAllDayPanel: false,
+  }).dxScheduler('instance');
+
+  $('#shifts').dxRadioGroup({
+    items: shifts,
+    value: shifts[0],
+    layout: 'horizontal',
+    onValueChanged: (e) => {
+      scheduler.option('offset', e.value.offset);
     },
   });
 });
