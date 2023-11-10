0f408fdf27765a7a1b44394e7b69fa662c806643
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-11-10 10:57:00 +0400

    Convert vue Chart Demos to Composition API part 1 + fix lint

--- JSDemos/Demos/Charts/Overview/Vue/App.vue
+++ JSDemos/Demos/Charts/Overview/Vue/App.vue
@@ -76,15 +76,15 @@ import { complaintsData } from './data.js';
 function customizeTooltip(pointInfo) {
   return {
     html: `<div><div class='tooltip-header'>${
-        pointInfo.argumentText
+      pointInfo.argumentText
     }</div><div class='tooltip-body'><div class='series-name'><span class='top-series-name'>${
-        pointInfo.points[0].seriesName
+      pointInfo.points[0].seriesName
     }</span>: </div><div class='value-text'><span class='top-series-value'>${
-        pointInfo.points[0].valueText
+      pointInfo.points[0].valueText
     }</span></div><div class='series-name'><span class='bottom-series-name'>${
-        pointInfo.points[1].seriesName
+      pointInfo.points[1].seriesName
     }</span>: </div><div class='value-text'><span class='bottom-series-value'>${
-        pointInfo.points[1].valueText
+      pointInfo.points[1].valueText
     }</span>% </div></div></div>`,
   };
 }
