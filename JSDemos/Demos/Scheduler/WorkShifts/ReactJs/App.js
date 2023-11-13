8e4b4f378c16e0ebf6bbfa2f1d9fd60c7a4c311d
Andrew Makarov <andrewmakarov@inbox.ru>
2023-11-13 20:17:45 +0400

    Generate reactjs version

--- JSDemos/Demos/Scheduler/WorkShifts/ReactJs/App.js
+++ JSDemos/Demos/Scheduler/WorkShifts/ReactJs/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import DataGrid from 'devextreme-react/data-grid';
-import { customers } from './data.js';
+import Scheduler from 'devextreme-react/scheduler';
+import RadioGroup from 'devextreme-react/radio-group';
+import { data, shifts } from './data.js';
 
-const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
-
-class App extends React.Component {
-  render() {
-    return (
-      <DataGrid
-        dataSource={customers}
-        defaultColumns={columns}
-        showBorders={true}
+const currentDate = new Date(2021, 2, 30);
+const views = ['day', 'week'];
+const App = () => {
+  const [offset, setOffset] = React.useState(0);
+  const onShiftChanged = React.useCallback((e) => {
+    setOffset(e.value);
+  }, []);
+  return (
+    <React.Fragment>
+      <div className="options">
+        <div className="option">
+          <div className="label">Work Hours</div>
+          <div className="value">
+            <RadioGroup
+              defaultValue={shifts[0]}
+              items={shifts}
+              layout="horizontal"
+              onValueChanged={onShiftChanged}
+            />
+          </div>
+        </div>
+      </div>
+      <br />
+      <Scheduler
+        timeZone="America/Los_Angeles"
+        dataSource={data}
+        views={views}
+        defaultCurrentView="week"
+        currentDate={currentDate}
+        startDayHour={0}
+        endDayHour={8}
+        // offset={offset}
+        height={600}
+        showAllDayPanel={false}
       />
-    );
-  }
-}
-
+    </React.Fragment>
+  );
+};
 export default App;
