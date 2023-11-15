457b4bae03162418399a25f3d828d47b5654ab30
Lykoi18 <aiden.ghost666@gmail.com>
2023-11-15 18:14:50 +0400

    Add release config for prepare-aspnet script / Use pre-built devextreme-aspnet packages during build task on farm

--- gulpfile.js/copy-asp-files.js
+++ gulpfile.js/copy-asp-files.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const {
-  src, dest, parallel, series,
+  src, dest, parallel, series, task,
 } = require('gulp');
-const { existsSync } = require('fs');
+const fs = require('fs');
+const path = require('path');
 const clean = require('gulp-clean');
 const { systemSync } = require('../utils/shared/child-process-utils');
 const { init } = require('../utils/shared/config-helper');
@@ -15,7 +16,7 @@ function checkAspnetPath(callback) {
     throw Error('Aspnet repo path is not specified. Look at repository.config.json');
   }
 
-  if (!existsSync(aspnetPath)) {
+  if (!fs.existsSync(aspnetPath)) {
     throw Error(`Specified aspnet repo path (${aspnetPath}) doesn't exists.`);
   }
 
@@ -33,29 +34,57 @@ function restorePackages(callback) {
   callback();
 }
 
-exports.copyAspFiles = series(
+task('prepareMisc', parallel(
+  () => src('MVCDemos/AppData/*.ldf', { read: false }).pipe(clean()),
+  () => src('NetCoreDemos/Northwind.mdf')
+    .pipe(dest('MVCDemos/App_Data')),
+
+  () => src('NetCoreDemos/project.lock.json', { read: false, allowEmpty: true }).pipe(clean()),
+
+  () => src('SampleImages/**/*')
+    .pipe(dest('MVCDemos/Content/SampleData/SampleImages'))
+    .pipe(dest('NetCoreDemos/wwwroot/SampleData/SampleImages')),
+));
+
+exports.buildAndCopyAspFiles = series(
   checkAspnetPath,
   runBuild,
   parallel(
-    () => src('MVCDemos/AppData/*.ldf', { read: false }).pipe(clean()),
-    () => src('NetCoreDemos/Northwind.mdf')
-      .pipe(dest('MVCDemos/App_Data')),
-
-    () => src('NetCoreDemos/project.lock.json', { read: false, allowEmpty: true }).pipe(clean()),
-
-    // TODO: back to the "demo-template.css" after migrating WidgetsGallery
-    // () => src('Demos/WidgetsGallery/WidgetsGallery/build/demo-template.css', { cwd: aspnetPath })
-    //   .pipe(dest('MVCDemos/Content'))
-    //   .pipe(dest('NetCoreDemos/wwwroot/css')),
-
-    () => src('SampleImages/**/*')
-      .pipe(dest('MVCDemos/Content/SampleData/SampleImages'))
-      .pipe(dest('NetCoreDemos/wwwroot/SampleData/SampleImages')),
+    'prepareMisc',
 
     () => src('artifacts/*.{dll,pdb,xml}', { cwd: aspnetPath })
       .pipe(dest('MVCDemos/lib')),
 
     () => src('artifacts/*.nupkg', { cwd: aspnetPath })
+      .pipe(dest('NetCoreDemos/Nuget')),
+
+    // TODO: back to the "demo-template.css" after migrating WidgetsGallery
+    // () => src('Demos/WidgetsGallery/WidgetsGallery/build/demo-template.css', { cwd: aspnetPath })
+    //   .pipe(dest('MVCDemos/Content'))
+    //   .pipe(dest('NetCoreDemos/wwwroot/css')),
+  ),
+  restorePackages,
+);
+
+const aspnetPackagesPath = path.resolve('aspnetPackages');
+
+function checkAspnetPackagesPath(callback) {
+  if (!fs.existsSync(aspnetPackagesPath)) {
+    throw new Error(`The folder is missing: "${aspnetPackagesPath}"! Did you forget to build devextreme-aspnet packages?`);
+  }
+
+  callback();
+}
+
+exports.copyAspFiles = series(
+  checkAspnetPackagesPath,
+  parallel(
+    'prepareMisc',
+
+    () => src(`${aspnetPackagesPath}/*.{dll,pdb,xml}`, { strict: true, allowEmpty: false })
+      .pipe(dest('MVCDemos/lib')),
+
+    () => src(`${aspnetPackagesPath}/*.nupkg`, { strict: true, allowEmpty: false })
       .pipe(dest('NetCoreDemos/Nuget')),
   ),
   restorePackages,
