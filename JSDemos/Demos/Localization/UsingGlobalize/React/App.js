217da4d68c4013307496d3714e29ca5c17ff54c4
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-11-16 10:48:18 +0400

    Convert from class to functional

--- JSDemos/Demos/Localization/UsingGlobalize/React/App.js
+++ JSDemos/Demos/Localization/UsingGlobalize/React/App.js
@@ -30,28 +30,12 @@ const amountEditorOptions = {
 };
 const selectBoxInputAttr = { id: 'selectInput' };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locale: this.getLocale(),
-    };
-    this.locales = service.getLocales();
-    this.payments = service.getPayments();
-    this.initGlobalize();
-    this.changeLocale = this.changeLocale.bind(this);
-  }
+const App = () => {
+  const [locale, setLocale] = React.useState(sessionStorage.getItem('locale') || 'en');
+  const locales = service.getLocales();
+  const payments = service.getPayments();
 
-  getLocale() {
-    const locale = sessionStorage.getItem('locale');
-    return locale != null ? locale : 'en';
-  }
-
-  setLocale(locale) {
-    sessionStorage.setItem('locale', locale);
-  }
-
-  initGlobalize() {
+  const initGlobalize = React.useCallback(() => {
     Globalize.load(
       deCldrData,
       ruCldrData,
@@ -60,60 +44,78 @@ class App extends React.Component {
     Globalize.loadMessages(deMessages);
     Globalize.loadMessages(ruMessages);
     Globalize.loadMessages(service.getDictionary());
-    Globalize.locale(this.state.locale);
-  }
+    Globalize.locale(locale);
+  }, [locale]);
 
-  changeLocale(e) {
-    this.setState({
-      locale: e.value,
-    });
-    this.setLocale(e.value);
+  const changeLocale = (e) => {
+    setLocale(e.value);
+    sessionStorage.setItem('locale', e.value);
     document.location.reload();
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <DataGrid dataSource={this.payments}
-          keyExpr="PaymentId">
-          <Editing mode="popup"
-            allowUpdating={true}
-            popup={editPopupOptions} />
-          <FilterRow visible={true}
-            applyFilter="auto" />
-          <Column dataField="PaymentId"
-            caption={Globalize.formatMessage('Number')}
-            allowEditing={false}
-            width={100} />
-          <Column dataField="ContactName"
-            caption={Globalize.formatMessage('Contact')} />
-          <Column dataField="CompanyName"
-            caption={Globalize.formatMessage('Company')} />
-          <Column dataField="Amount"
-            caption={Globalize.formatMessage('Amount')}
-            dataType="number"
-            format="currency"
-            editorOptions={amountEditorOptions} />
-          <Column dataField="PaymentDate"
-            caption={Globalize.formatMessage('PaymentDate')}
-            dataType="date" />
-        </DataGrid>
-        <div className="options">
-          <div className="caption">Options</div>
-          <div className="option">
-            <label htmlFor="selectInput">Language</label>
-            &nbsp;
-            <SelectBox items={this.locales}
-              valueExpr="Value"
-              displayExpr="Name"
-              value={this.state.locale}
-              onValueChanged={this.changeLocale}
-              inputAttr={selectBoxInputAttr} />
-          </div>
+  React.useEffect(() => {
+    initGlobalize();
+  }, [locale, initGlobalize]);
+
+  return (
+    <div>
+      <DataGrid
+        dataSource={payments}
+        keyExpr="PaymentId"
+      >
+        <Editing
+          mode="popup"
+          allowUpdating={true}
+          popup={editPopupOptions}
+        />
+        <FilterRow
+          visible={true}
+          applyFilter="auto"
+        />
+        <Column
+          dataField="PaymentId"
+          caption={Globalize.formatMessage('Number')}
+          allowEditing={false}
+          width={100}
+        />
+        <Column
+          dataField="ContactName"
+          caption={Globalize.formatMessage('Contact')}
+        />
+        <Column
+          dataField="CompanyName"
+          caption={Globalize.formatMessage('Company')}
+        />
+        <Column
+          dataField="Amount"
+          caption={Globalize.formatMessage('Amount')}
+          dataType="number"
+          format="currency"
+          editorOptions={amountEditorOptions}
+        />
+        <Column
+          dataField="PaymentDate"
+          caption={Globalize.formatMessage('PaymentDate')}
+          dataType="date"
+        />
+      </DataGrid>
+      <div className="options">
+        <div className="caption">Options</div>
+        <div className="option">
+          <label htmlFor="selectInput">Language</label>
+          &nbsp;
+          <SelectBox
+            items={locales}
+            valueExpr="Value"
+            displayExpr="Name"
+            value={locale}
+            onValueChanged={changeLocale}
+            inputAttr={selectBoxInputAttr}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- JSDemos/Demos/Localization/UsingIntl/React/App.js
+++ JSDemos/Demos/Localization/UsingIntl/React/App.js
@@ -15,91 +15,103 @@ const editPopupOptions = {
   width: 700,
   height: 345,
 };
+
 const amountEditorOptions = {
   format: 'currency',
   showClearButton: true,
 };
+
 const selectBoxInputAttr = { id: 'selectInput' };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locale: this.getLocale(),
-    };
-    this.locales = service.getLocales();
-    this.payments = service.getPayments();
-    this.initMessages();
-    locale(this.state.locale);
-    this.changeLocale = this.changeLocale.bind(this);
-  }
-
-  getLocale() {
+const App = () => {
+  const [localeState, setLocaleState] = React.useState(() => {
     const storageLocale = sessionStorage.getItem('locale');
     return storageLocale != null ? storageLocale : 'en';
-  }
+  });
 
-  setLocale(savingLocale) {
+  const locales = service.getLocales();
+  const payments = service.getPayments();
+
+  const setLocale = (savingLocale) => {
     sessionStorage.setItem('locale', savingLocale);
-  }
+  };
 
-  initMessages() {
+  const initMessages = () => {
     loadMessages(deMessages);
     loadMessages(ruMessages);
     loadMessages(service.getDictionary());
-  }
+  };
 
-  changeLocale(e) {
-    this.setState({
-      locale: e.value,
-    });
-    this.setLocale(e.value);
+  const changeLocale = (e) => {
+    setLocale(e.value);
+    setLocaleState(e.value);
     document.location.reload();
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <DataGrid dataSource={this.payments}
-          keyExpr="PaymentId">
-          <Editing mode="popup"
-            allowUpdating={true}
-            popup={editPopupOptions} />
-          <FilterRow visible={true}
-            applyFilter="auto" />
-          <Column dataField="PaymentId"
-            caption={formatMessage('Number')}
-            allowEditing={false}
-            width={100} />
-          <Column dataField="ContactName"
-            caption={formatMessage('Contact')} />
-          <Column dataField="CompanyName"
-            caption={formatMessage('Company')} />
-          <Column dataField="Amount"
-            caption={formatMessage('Amount')}
-            dataType="number"
-            format="currency"
-            editorOptions={amountEditorOptions} />
-          <Column dataField="PaymentDate"
-            caption={formatMessage('PaymentDate')}
-            dataType="date" />
-        </DataGrid>
-        <div className="options">
-          <div className="caption">Options</div>
-          <div className="option">
-            <label htmlFor="selectInput">Language</label>
-            &nbsp;
-            <SelectBox items={this.locales}
-              valueExpr="Value"
-              displayExpr="Name"
-              value={this.state.locale}
-              onValueChanged={this.changeLocale}
-              inputAttr={selectBoxInputAttr} />
-          </div>
+  React.useEffect(() => {
+    locale(localeState);
+    initMessages();
+  }, [localeState]);
+
+  return (
+    <div>
+      <DataGrid
+        dataSource={payments}
+        keyExpr="PaymentId"
+      >
+        <Editing
+          mode="popup"
+          allowUpdating={true}
+          popup={editPopupOptions}
+        />
+        <FilterRow
+          visible={true}
+          applyFilter="auto"
+        />
+        <Column
+          dataField="PaymentId"
+          caption={formatMessage('Number')}
+          allowEditing={false}
+          width={100}
+        />
+        <Column
+          dataField="ContactName"
+          caption={formatMessage('Contact')}
+        />
+        <Column
+          dataField="CompanyName"
+          caption={formatMessage('Company')}
+        />
+        <Column
+          dataField="Amount"
+          caption={formatMessage('Amount')}
+          dataType="number"
+          format="currency"
+          editorOptions={amountEditorOptions}
+        />
+        <Column
+          dataField="PaymentDate"
+          caption={formatMessage('PaymentDate')}
+          dataType="date"
+        />
+      </DataGrid>
+      <div className="options">
+        <div className="caption">Options</div>
+        <div className="option">
+          <label htmlFor="selectInput">Language</label>
+          &nbsp;
+          <SelectBox
+            items={locales}
+            valueExpr="Value"
+            displayExpr="Name"
+            value={localeState}
+            onValueChanged={changeLocale}
+            inputAttr={selectBoxInputAttr}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
