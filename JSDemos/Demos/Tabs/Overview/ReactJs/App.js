baf9dd3ff34429c8252c9901587c2f9e6e3f24ed
marker dao ® <youdontknow@marker-dao.eth>
2023-11-16 12:24:54 +0400

    Revert "refactor(tabs): Replace #tab-demo with .demo-container"
    This reverts commit b87843f564e54c1394570ea945627514e1913df8.

--- JSDemos/Demos/Tabs/Overview/ReactJs/App.js
+++ JSDemos/Demos/Tabs/Overview/ReactJs/App.js
@@ -98,7 +98,7 @@ const App = () => {
     [setRtlEnabled],
   );
   return (
-    <div className="demo-container">
+    <div id="tabs-demo">
       <div className="widget-container">
         <div className={widgetWrapperClasses}>
           <Tabs
