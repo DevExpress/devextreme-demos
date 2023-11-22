eb22571cc7dd390c480662a58ae27f310e096e83
Stanislav Klesarev <stanislav.klesarev@devexpress.com>
2023-11-22 16:50:23 +0400

    Remove azure.file.system and corresponding utils
    (cherry picked from commit 6d42863ab3907bcfa6712b18c9bb4dc7eb58d564)
    
    # Conflicts:
    #	gulpfile.js/js.js
    #	gulpfile.js/mvc.js
    #	utils/copy-shared-resources/files-js.json
    #	utils/ts-to-js-converter/converter.ts

--- gulpfile.js/js.js
+++ gulpfile.js/js.js
@@ -1,13 +1,11 @@
 const { join } = require('path');
 const { init } = require('../utils/shared/config-helper');
 const createConfig = require('../utils/internal/create-config');
-const { copyJsSharedResources } = require('../utils/copy-shared-resources/copy');
 
 const demosDir = join(__dirname, '..', 'JSDemos/Demos');
 
 function prepareJs(callback) {
   init();
-  copyJsSharedResources(callback);
   createConfig.useBundles = false;
   createConfig.run(demosDir);
   callback();
--- gulpfile.js/mvc.js
+++ gulpfile.js/mvc.js
@@ -3,10 +3,8 @@ const { series } = require('gulp');
 
 const { copyCommonAspFiles } = require('./copy-common-asp-files');
 const { copyMercurialAspFiles } = require('./copy-mercurial-asp-files');
-const { copyMvcSharedResources } = require('../utils/copy-shared-resources/copy');
 
 exports.mvc = series(
   copyCommonAspFiles,
   copyMercurialAspFiles,
-  copyMvcSharedResources,
 );
