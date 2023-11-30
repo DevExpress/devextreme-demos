4aa81c2289313bf93e57fcf040da570b26eacec6
Andrew Makarov <andrewmakarov@inbox.ru>
2023-12-01 02:15:06 +0400

    Remove disable animation

--- testing/widgets/scheduler/resources.test.js
+++ testing/widgets/scheduler/resources.test.js
@@ -1,9 +1,5 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { ClientFunction } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
-
-// eslint-disable-next-line no-undef
-const disableAnimation = ClientFunction(() => { DevExpress.fx.off = true; });
 
 fixture('Scheduler.Templates')
   .page('http://localhost:8080/')
@@ -16,7 +12,6 @@ runManualTest('Scheduler', 'Resources', ['jQuery', 'React', 'Vue', 'Angular'], (
   test('Resource fields shouldn\'t empty', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await disableAnimation();
     await t.doubleClick('.dx-scheduler-appointment');
 
     await takeScreenshot('scheduler_resources_appointment_popup.png');
