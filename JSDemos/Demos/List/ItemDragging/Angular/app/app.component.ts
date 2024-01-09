91e36254b0365a4d4885ed3ce7ba8c257f4b79af
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-01-09 12:43:44 +0400

    refactor Types

--- JSDemos/Demos/List/ItemDragging/Angular/app/app.component.ts
+++ JSDemos/Demos/List/ItemDragging/Angular/app/app.component.ts
@@ -1,14 +1,14 @@
 import { NgModule, Component, enableProdMode } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { DxListComponent, DxListModule } from 'devextreme-angular/ui/list';
+import { DxListModule, DxListTypes } from 'devextreme-angular/ui/list';
 import { Task, Service } from './app.service';
 
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
 }
 
-type ListItemDragging = DxListComponent['itemDragging'];
+type DxoListItemDragging = DxListTypes.Properties['itemDragging'];
 type FirstArgument<T> = T extends (firstArg: infer A, ...args: any) => any ? A : never;
 
 @Component({
@@ -28,19 +28,19 @@ export class AppComponent {
     this.plannedTasks = service.getPlannedTasks();
   }
 
-  onDragStart: ListItemDragging['onDragStart'] = (e) => {
+  onDragStart: DxoListItemDragging['onDragStart'] = (e) => {
     e.itemData = e.fromData[e.fromIndex];
   };
 
-  onAdd: ListItemDragging['onAdd'] = (e) => {
+  onAdd: DxoListItemDragging['onAdd'] = (e) => {
     e.toData.splice(e.toIndex, 0, e.itemData);
   };
 
-  onRemove: ListItemDragging['onRemove'] = (e) => {
+  onRemove: DxoListItemDragging['onRemove'] = (e) => {
     e.fromData.splice(e.fromIndex, 1);
   };
 
-  onReorder: ListItemDragging['onReorder'] = (e) => {
+  onReorder: DxoListItemDragging['onReorder'] = (e) => {
     this.onRemove(e as FirstArgument<typeof this.onRemove>);
     this.onAdd(e as FirstArgument<typeof this.onAdd>);
   };
