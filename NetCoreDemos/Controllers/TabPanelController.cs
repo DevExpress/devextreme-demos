447a9a5de8681dbfacb8a0b536d809262365cd71
marker dao ® <youdontknow@marker-dao.eth>
2024-01-16 19:25:40 +0700

    fix(tabPanel: Templates): Add ActionResult Templates

--- NetCoreDemos/Controllers/TabPanelController.cs
+++ NetCoreDemos/Controllers/TabPanelController.cs
@@ -6,8 +6,11 @@ using Newtonsoft.Json;
 
 namespace DevExtreme.NETCore.Demos.Controllers {
     public class TabPanelController : Controller {
-        #region Overview
         public ActionResult Overview() {
+            return View();
+        }
+
+        public ActionResult Templates() {
             return View();
         }
 
@@ -24,6 +27,5 @@ namespace DevExtreme.NETCore.Demos.Controllers {
         public ActionResult GetTabPanelItems(DataSourceLoadOptions loadOptions) {
             return Content(JsonConvert.SerializeObject(DataSourceLoader.Load(TabPanelData.TabPanelItems, loadOptions)), "application/json");
         }
-        #endregion
     }
 }
