299d7b18db26da5016bed243cb7dea265f4c0dc1
Marker DAO ® <mark_nikolsky@outlook.com>
2024-01-23 14:57:45 +0400

    Gallery: Add img alt into the data source

--- NetCoreDemos/Controllers/GalleryController.cs
+++ NetCoreDemos/Controllers/GalleryController.cs
@@ -8,7 +8,13 @@ using System.Linq;
 namespace DevExtreme.NETCore.Demos.Controllers {
     public class GalleryController : Controller {
         public ActionResult Overview() {
-            var images = Enumerable.Range(1, 9).Select(i => Url.Content(String.Format("~/images/Gallery/{0}.jpg", i)));
+            var images = Enumerable.Range(1, 9).Select(i =>
+                new ImageItem {
+                    imageSrc = Url.Content(String.Format("~/Content/Images/Gallery/{0}.jpg", i)),
+                    imageAlt = String.Format("Image {0}", i)
+                }
+            ).ToList();
+
             return View(new GalleryViewModel { Images = images });
         }
 
