e4adbe51a21a3c38db1d02045c3194bd39f2cf13
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2024-02-01 01:27:53 +0400

    rb changes

--- testing/widgets/gauges/GaugeVariableNumberBars.test.js
+++ testing/widgets/gauges/GaugeVariableNumberBars.test.js
@@ -1,82 +1,32 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { Selector as $, ClientFunction } from 'testcafe';
-import themes from 'devextreme/ui/themes';
-
-const createTestCafe = require('testcafe');
-
-// 1) this approach doent work
-// 2) await t.navigateTo('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.material.blue.light.css'); - works
-
-const changeTheme = async (themeName) => createTestCafe.ClientFunction((args) => new Promise((resolve) => {
-  // eslint-disable-next-line no-undef
-  const currentTheme = window.localStorage.getItem('dx-theme');
-
-  debugger;
-  // if (currentTheme !== themeName) {
-  window.localStorage.setItem('dx-theme', themeName);
-
-  // window.location.reload();
-  // }
-
-  // window.DevExpress.ui.themes.ready(resolve);
-  // eslint-disable-next-line no-undef
-}),
-{ dependencies: { themeName } })();
+import { Selector as $ } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
 fixture('Gauges.VariableNumberOfBars')
-// .page('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.light.css');
-  .page('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.light.css')
+  .page('http://localhost:8080/')
   .beforeEach(async (t) => {
-    // await changeTheme('fluent.blue.light');
-
-    // await t.navigateTo('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.material.blue.light.css');
+    await t
+      .resizeWindow(900, 600);
   });
-const getLocalStorageItem = ClientFunction((key) => localStorage.getItem(key));
 
-test('qqqq', async (t) => {
-  // await t.eval(() => {
-  //   const linkRel = document.getElementsByTagName('link')[0];
-  //   linkRel.href = 'http://localhost:3000/node_modules/devextreme-dist/css/dx.material.blue.dark.css';
-  // });
+runManualTest('Gauges', 'VariableNumberOfBars', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('Bars visibility changing', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-  await t.wait(2000);
+    const checkBoxes = $('.dx-checkbox-icon');
 
-  await t.expect(t.ctx.someProp).eql('1234');
+    await t.click(checkBoxes.nth(2));
+    await t.click(checkBoxes.nth(3));
+    await t.wait(1000);
+    await takeScreenshot(`bargauge_hide_bars(${t.ctx.theme}).png`, '#gauge');
 
-  // await t.eval(() => {
-  //   const currentLocation = location.href;
-  //   const newLocation =
-  //   // location.replace('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.material.blue.dark.css');
-  //   location.replace('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.greenmist.css');
-  //   location.reload();
-  // });
+    await t.click(checkBoxes.nth(2));
+    await t.click(checkBoxes.nth(3));
+    await t.wait(1000);
+    await takeScreenshot(`bargauge_show_bars(${t.ctx.theme}).png`, '#gauge');
 
-  // await t.eval(() => location.reload());
-
-  // await t.debug();
-
-  // await t.eval(() => window.localStorage.setItem('dx-theme', 'material.blue.dark'));
-  // await t.wait(2000);
-  // await t.eval(() => location.reload());
-  // await t.eval(() => window.DevExpress.ui.themes.current('material.blue.dark'));
-
-  // await t.expect(getLocalStorageItem('dx-theme')).eql('fluent.blue.light22');
-  // await t.expect(getLocalStorageItem(themeKey)).eql('value');
-}).before(async (t) => {
-  t.ctx.someProp = 123;
-
-  await t.eval(() => {
-    console.log('start before test');
-    const urlSegments = location.href.split('/');
-
-    const newUrl = urlSegments.slice(0, -1).join('/');
-    const newLocation = `${newUrl}/dx.greenmist.css`;
-
-    console.log('newLocation', newLocation);
-
-    location.replace(newLocation);
-    // location.replace('http://localhost:3000/#JSDemos/Demos/Gauges/VariableNumberOfBars/jQuery/dx.greenmist.css');
-    location.reload();
-    console.log('end before test');
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
   });
 });
