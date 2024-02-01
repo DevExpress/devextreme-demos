847889e7411143af8dd0b61ee661f8a61cca6110
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2024-02-01 13:36:38 +0400

    test name fixed

--- testing/widgets/list/ListSelection.test.js
+++ testing/widgets/list/ListSelection.test.js
@@ -24,24 +24,24 @@ runManualTest('List', 'ListSelection', ['jQuery', 'React', 'Vue', 'Angular'], (t
 
     await t.click(Selector(`.${LIST_ITEM_CONTENT_CLASS}`).nth(3));
 
-    await takeScreenshot(`List after click on item, selectionMode=all-${t.ctx.theme}.png`);
+    await takeScreenshot(`List after click on item, selectionMode=all(${t.ctx.theme}).png`);
 
     await t.click(Selector(`.${SELECTBOX_CLASS}`).nth(0));
     await t.click(Selector(`.${POPUP_WRAPPER_CLASS} .${LIST_ITEM_CONTENT_CLASS}`).nth(2));
 
     await t.wait(2000);
 
-    await takeScreenshot(`List after change selectionMode to multiple-${t.ctx.theme}.png`);
+    await takeScreenshot(`List after change selectionMode to multiple(${t.ctx.theme}).png`);
 
     await t.click(Selector(`.${SELECTBOX_CLASS}`).nth(0));
     await t.click(Selector(`.${POPUP_WRAPPER_CLASS} .${LIST_ITEM_CONTENT_CLASS}`).nth(1));
 
-    await takeScreenshot(`List after change selectionMode to single-${t.ctx.theme}.png`);
+    await takeScreenshot(`List after change selectionMode to single(${t.ctx.theme}).png`);
 
     await t.click(Selector(`.${SELECTBOX_CLASS}`).nth(0));
     await t.click(Selector(`.${POPUP_WRAPPER_CLASS} .${LIST_ITEM_CONTENT_CLASS}`).nth(0));
 
-    await takeScreenshot(`List after change selectionMode to none-${t.ctx.theme}.png`);
+    await takeScreenshot(`List after change selectionMode to none(${t.ctx.theme}).png`);
 
     await t
       .expect(compareResults.isValid())
