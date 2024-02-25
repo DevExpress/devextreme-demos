d0b3449960691eeca1a77186effbd5677ffec4bf
ivanblinov2k17 <ivanblinov2015@mail.ru>
2024-02-26 02:01:33 +0400

    Add a temporary WA for calculating last stable minor

--- utils/create-bundles/helper/metadata/scripts/init-theme.js
+++ utils/create-bundles/helper/metadata/scripts/init-theme.js
@@ -1,11 +1,21 @@
+// Temporary WA, need to do this in yaml
+function findLastStableVersion(version) {
+  // eslint-disable-next-line prefer-const
+  let [year, major, minor] = version.split('.');
+  if (minor.includes('build')) {
+    minor = Number(minor.split('-')[0]) - 1 || 1;
+  }
+  return `${year}.${major}.${minor}`;
+}
 // eslint-disable-next-line no-unused-vars
 function DXDemoTheme(version) {
+  const stableVersion = findLastStableVersion(version);
   const searchParams = new URLSearchParams(window.location.search);
   const theme = searchParams.get('theme') ?? 'dx.material.blue.light';
 
   const themeLink = document.createElement('link');
   themeLink.rel = 'stylesheet';
-  themeLink.href = `https://cdnjs.cloudflare.com/ajax/libs/devextreme-dist/23.2.4/css/${theme}.min.css`;
+  themeLink.href = `https://cdnjs.cloudflare.com/ajax/libs/devextreme-dist/${stableVersion}/css/${theme}.min.css`;
 
   document.head.appendChild(themeLink);
 }
