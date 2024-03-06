70f84f111fb22a5f05771087dc986d5686c6f444
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-03-06 20:13:31 +0800

    Add TestCafe test for DropDownButton componentAdd TestCafe test for DropDownButton component

---
+++ testing/widgets/dropdownbutton/DropDownButton.test.js
@@ -0,0 +1,24 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+fixture('DropDownButton.Overview')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t.resizeWindow(900, 800);
+  });
+
+runManualTest('DropDownButton', 'Overview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('Custom Overview Appearance', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await takeScreenshot('dropdown_button_overview_custom_appearance.png');
+
+    await t.click('#one-section');
+
+    await takeScreenshot('dropdown_button_overview_button_action_appearance.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
