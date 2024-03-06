69aabf5c8606d14ee97dd7c9ef1febc4c1d34f70
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-03-06 22:15:01 +0800

    Revert to runManualTest

--- testing/widgets/dropdownbutton/DropDownButton.test.js
+++ testing/widgets/dropdownbutton/DropDownButton.test.js
@@ -1,30 +1,27 @@
-import { Selector as $, Selector } from "testcafe";
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
 fixture('DropDownButton.Overview')
-  .page('http://localhost:3000/#JSDemos/Demos/DropDownButton/Overview/jQuery/dx.light.css')
+  .page('http://localhost:8080/')
   .beforeEach(async (t) => {
     await t.resizeWindow(900, 800);
   });
 
-test('Custom Overview Appearance', async (t) => {
-  const DROP_DOWN_BUTTON = 'dx-dropdownbutton';
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+runManualTest('DropDownButton', 'Overview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('Custom Overview Appearance', async (t) => {
+    const DROP_DOWN_BUTTON = 'dx-dropdownbutton';
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-  await t.switchToIframe('#frame');
+    await takeScreenshot('dropdown_button_overview_custom_appearance.png');
 
-  await t.debug();
+    await t
+      .click($(`.${DROP_DOWN_BUTTON}`).nth(0))
+      .wait(200);
 
-  await takeScreenshot('dropdown_button_overview_custom_appearance.png');
+    await takeScreenshot('dropdown_button_overview_button_action_appearance.png');
 
-  await t
-    .click($(`.${DROP_DOWN_BUTTON}`).nth(0))
-    .wait(200);
-
-  await takeScreenshot('dropdown_button_overview_button_action_appearance.png');
-
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
 });
