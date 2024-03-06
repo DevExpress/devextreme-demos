7b7bafb5fcd0d281d99c510fb6fa3afe2d801d6e
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-03-06 22:13:42 +0800

    Change Selector ID

--- testing/widgets/dropdownbutton/DropDownButton.test.js
+++ testing/widgets/dropdownbutton/DropDownButton.test.js
@@ -1,26 +1,30 @@
-import { Selector } from 'testcafe';
+import { Selector as $, Selector } from "testcafe";
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 
 fixture('DropDownButton.Overview')
-  .page('http://localhost:8080/')
+  .page('http://localhost:3000/#JSDemos/Demos/DropDownButton/Overview/jQuery/dx.light.css')
   .beforeEach(async (t) => {
     await t.resizeWindow(900, 800);
   });
 
-runManualTest('DropDownButton', 'Overview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
-  test('Custom Overview Appearance', async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+test('Custom Overview Appearance', async (t) => {
+  const DROP_DOWN_BUTTON = 'dx-dropdownbutton';
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await takeScreenshot('dropdown_button_overview_custom_appearance.png');
+  await t.switchToIframe('#frame');
 
-    const oneSectionButton = Selector('#one-section');
-    await t.click(oneSectionButton);
+  await t.debug();
 
-    await takeScreenshot('dropdown_button_overview_button_action_appearance.png');
+  await takeScreenshot('dropdown_button_overview_custom_appearance.png');
 
-    await t
-      .expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  });
+  await t
+    .click($(`.${DROP_DOWN_BUTTON}`).nth(0))
+    .wait(200);
+
+  await takeScreenshot('dropdown_button_overview_button_action_appearance.png');
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
 });
