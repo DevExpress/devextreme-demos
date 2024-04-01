d8ad13a629a9538758816eff0cdfe6976b7d5b55
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-04-01 21:23:39 +0800

    Splitter Demo: create Splitter demo (#3182)
    * Splitter Demo: applied in mvc demo first
    
    * Splitter Demo: add css style to MVC demo
    
    * Splitter demo: refactor code
    
    * Splitter Demo: fix css identations
    
    * Splitter Demo: fix error in regards using Orientation in MVC and .Net also fixed spacing and indentation on files
    
    * Splitter Demo: fix template option implementation in mvc and .net demos
    
    * Splitter Demo: cleanup for mvc and .net demo
    
    * Splitter Demo: refactor code based from recommendations
    
    * change order
    
    * Splitter Demo: refactor state text construction in mvc and .net
    
    ---------
    
    Co-authored-by: EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>

---
+++ NetCoreDemos/Controllers/SplitterController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevExtreme.NETCore.Demos.Controllers {
+    public class SplitterController : Controller {
+        public ActionResult Overview() {
+            return View();
+        }
+    }
+}
